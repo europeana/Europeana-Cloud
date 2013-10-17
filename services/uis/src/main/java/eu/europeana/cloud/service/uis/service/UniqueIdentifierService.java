b67e13744a58fa6c3bf9ca911efd801474a77c56
1nf1n1ty08 <markus.muhr@kb.nl>
2013-10-17 14:37:16 +0200

    refactoring identifier service;

---
+++ services/uis/src/main/java/eu/europeana/cloud/service/uis/service/UniqueIdentifierService.java
@@ -0,0 +1,130 @@
+package eu.europeana.cloud.service.uis.service;
+
+import java.util.List;
+
+import eu.europeana.cloud.common.model.GlobalId;
+import eu.europeana.cloud.common.model.LocalId;
+import eu.europeana.cloud.exceptions.DatabaseConnectionException;
+import eu.europeana.cloud.exceptions.GlobalIdDoesNotExistException;
+import eu.europeana.cloud.exceptions.IdHasBeenMappedException;
+import eu.europeana.cloud.exceptions.ProviderDoesNotExistException;
+import eu.europeana.cloud.exceptions.RecordDatasetEmptyException;
+import eu.europeana.cloud.exceptions.RecordDoesNotExistException;
+import eu.europeana.cloud.exceptions.RecordExistsException;
+import eu.europeana.cloud.exceptions.RecordIdDoesNotExistException;
+
+/**
+ * Unique Identifier Service Interface This service specifies the available methods for the
+ * successful generation and linking of records with a Global eCloud Identifier
+ * 
+ * @author Yorgos.Mamakis@ kb.nl
+ * @since Oct 17, 2013
+ */
+public interface UniqueIdentifierService {
+    /**
+     * Create a Unique Identifier from the given providerId and recordId
+     * 
+     * @param providerId
+     * @param recordId
+     * @return The unique identifier for this record
+     * @throws DatabaseConnectionException
+     * @throws RecordExistsException
+     */
+    GlobalId createGlobalId(String providerId, String recordId) throws DatabaseConnectionException,
+            RecordExistsException;
+
+    /**
+     * Search for a unique identifier based on the providerId and recordId
+     * 
+     * @param providerId
+     * @param recordId
+     * @return The unique identifier of the record
+     * @throws DatabaseConnectionException
+     * @throws RecordDoesNotExistException
+     */
+    GlobalId getGlobalId(String providerId, String recordId) throws DatabaseConnectionException,
+            RecordDoesNotExistException;
+
+    /**
+     * Search all the records that are linked to a unique identifier
+     * 
+     * @param globalId
+     * @return A list of providerIds with the records that have been linked to the unique identifier
+     *         provided
+     * @throws DatabaseConnectionException
+     * @throws GlobalIdDoesNotExistException
+     */
+    List<LocalId> getLocalIdsByGlobalId(String globalId) throws DatabaseConnectionException,
+            GlobalIdDoesNotExistException;
+
+    /**
+     * Retrieve the recordIds for a given provider, supporting pagination. If no pagination is
+     * provided then the recordIds retrieved are 10000 starting from record 0
+     * 
+     * @param providerId
+     * @param start
+     * @param end
+     * @return A list of recordIds for a provider bound to 10000 results
+     * @throws DatabaseConnectionException
+     * @throws ProviderDoesNotExistException
+     * @throws RecordDatasetEmptyException
+     */
+    List<LocalId> getLocalIdsByProvider(String providerId, int start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException;
+
+    /**
+     * Retrieve the globalIds for a given provider, supporting pagination. If no pagination is
+     * provided then the globalIds retrieved are 10000 starting from record 0
+     * 
+     * @param providerId
+     * @param start
+     * @param end
+     * @return A list of globalIds for a provider bound to 10000 results
+     * @throws DatabaseConnectionException
+     * @throws ProviderDoesNotExistException
+     * @throws RecordDatasetEmptyException
+     */
+    List<GlobalId> getGlobalIdsByProvider(String providerId, int start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException;
+
+    /**
+     * Create a mapping between a new providerId and recordId and an existing global identifier
+     * 
+     * @param globalId
+     * @param providerId
+     * @param recordId
+     * @throws DatabaseConnectionException
+     * @throws ProviderDoesNotExistException
+     * @throws GlobalIdDoesNotExistException
+     * @throws RecordIdDoesNotExistException
+     * @throws IdHasBeenMappedException
+     */
+    void createIdMapping(String globalId, String providerId, String recordId)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            GlobalIdDoesNotExistException, RecordIdDoesNotExistException, IdHasBeenMappedException;
+
+    /**
+     * Remove the mapping between the providerId/recordId and the global identifier The mapping is
+     * soft-deleted
+     * 
+     * @param providerId
+     * @param recordId
+     * @throws DatabaseConnectionException
+     * @throws ProviderDoesNotExistException
+     * @throws RecordIdDoesNotExistException
+     */
+    void removeIdMapping(String providerId, String recordId) throws DatabaseConnectionException,
+            ProviderDoesNotExistException, RecordIdDoesNotExistException;
+
+    /**
+     * Delete a global Identifier and all of its relevant mappings. Everything is soft-deleted
+     * 
+     * @param globalId
+     * @throws DatabaseConnectionException
+     * @throws GlobalIdDoesNotExistException
+     */
+    void deleteGlobalId(String globalId) throws DatabaseConnectionException,
+            GlobalIdDoesNotExistException;
+}
---
+++ services/uis/src/main/java/eu/europeana/cloud/service/uis/service/inmemory/InMemoryUniqueIdentifierService.java
@@ -0,0 +1,207 @@
+package eu.europeana.cloud.service.uis.service.inmemory;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.stereotype.Service;
+
+import eu.europeana.cloud.common.model.GlobalId;
+import eu.europeana.cloud.common.model.LocalId;
+import eu.europeana.cloud.common.model.Record;
+import eu.europeana.cloud.common.model.Representation;
+import eu.europeana.cloud.exceptions.DatabaseConnectionException;
+import eu.europeana.cloud.exceptions.GlobalIdDoesNotExistException;
+import eu.europeana.cloud.exceptions.IdHasBeenMappedException;
+import eu.europeana.cloud.exceptions.ProviderDoesNotExistException;
+import eu.europeana.cloud.exceptions.RecordDatasetEmptyException;
+import eu.europeana.cloud.exceptions.RecordDoesNotExistException;
+import eu.europeana.cloud.exceptions.RecordExistsException;
+import eu.europeana.cloud.exceptions.RecordIdDoesNotExistException;
+import eu.europeana.cloud.service.uis.service.UniqueIdentifierService;
+
+/**
+ * In-memory mockup of the unique identifier service
+ * 
+ * @see eu.europeana.cloud.service.uis.service.UniqueIdentifierService.java
+ * @author Yorgos.Mamakis@kb.nl
+ * @since Oct 17, 2013
+ */
+@Service
+public class InMemoryUniqueIdentifierService implements UniqueIdentifierService {
+    private List<Record>              records           = new ArrayList<>();
+    private Map<String, List<String>> providerLocalIds  = new HashMap<>();
+    private Map<String, List<String>> providerGlobalIds = new HashMap<>();
+
+    @Override
+    public GlobalId createGlobalId(String providerId, String recordId)
+            throws DatabaseConnectionException, RecordExistsException {
+        String globalId = String.format("/%s/%s", providerId, recordId);
+        for (Record record : records) {
+            if (StringUtils.equals(record.getId(), globalId)) { throw new RecordExistsException(); }
+        }
+        Record record = new Record();
+        Representation rep = new Representation();
+        rep.setDataProvider(providerId);
+        rep.setRecordId(recordId);
+        List<Representation> reps = new ArrayList<>();
+        reps.add(rep);
+        record.setRepresentations(reps);
+        record.setId(globalId);
+        records.add(record);
+        List<String> recordList = providerLocalIds.get(providerId) != null
+                ? providerLocalIds.get(providerId) : new ArrayList<String>();
+        if (!recordList.contains(recordId)) {
+            recordList.add(recordId);
+        }
+        providerLocalIds.put(providerId, recordList);
+
+        List<String> globalList = providerGlobalIds.get(providerId) != null
+                ? providerGlobalIds.get(providerId) : new ArrayList<String>();
+        if (!globalList.contains(globalId)) {
+            globalList.add(globalId);
+        }
+        providerGlobalIds.put(providerId, globalList);
+
+        LocalId localId = new LocalId();
+        localId.setProviderId(providerId);
+        localId.setRecordId(recordId);
+
+        GlobalId gId = new GlobalId();
+        gId.setLocalId(localId);
+        gId.setId(globalId);
+        return gId;
+    }
+
+    @Override
+    public GlobalId getGlobalId(String providerId, String recordId)
+            throws DatabaseConnectionException, RecordDoesNotExistException {
+        for (Record rec : records) {
+
+            for (Representation representation : rec.getRepresentations()) {
+                if (StringUtils.equals(representation.getDataProvider(), providerId) &&
+                    StringUtils.equals(representation.getRecordId(), recordId)) {
+                    LocalId localId = new LocalId();
+                    localId.setProviderId(providerId);
+                    localId.setRecordId(recordId);
+
+                    GlobalId gId = new GlobalId();
+                    gId.setLocalId(localId);
+                    gId.setId(rec.getId());
+                    return gId;
+                }
+            }
+        }
+        throw new RecordDoesNotExistException();
+    }
+
+    @Override
+    public List<LocalId> getLocalIdsByGlobalId(String globalId) throws DatabaseConnectionException,
+            GlobalIdDoesNotExistException {
+        for (Record record : records) {
+            if (StringUtils.equals(record.getId(), globalId)) {
+                List<LocalId> localIds = new ArrayList<>();
+                for (Representation rep : record.getRepresentations()) {
+                    LocalId localId = new LocalId();
+                    localId.setProviderId(rep.getDataProvider());
+                    localId.setRecordId(rep.getRecordId());
+                    localIds.add(localId);
+                }
+                return localIds;
+            }
+        }
+        throw new GlobalIdDoesNotExistException();
+    }
+
+    @Override
+    public List<LocalId> getLocalIdsByProvider(String providerId, int start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException {
+        if (providerLocalIds.containsKey(providerId)) {
+            if (providerLocalIds.get(providerId).isEmpty() ||
+                providerLocalIds.get(providerId).size() < start) { throw new RecordDatasetEmptyException(); }
+            List<LocalId> providers = new ArrayList<>();
+            for (String localId : providerLocalIds.get(providerId).subList(start,
+                    Math.min(providerLocalIds.get(providerId).size(), start + end))) {
+                LocalId provider = new LocalId();
+                provider.setProviderId(providerId);
+                provider.setRecordId(localId);
+                providers.add(provider);
+            }
+            return providers;
+        }
+        throw new ProviderDoesNotExistException();
+    }
+
+    @Override
+    public List<GlobalId> getGlobalIdsByProvider(String providerId, int start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException {
+        if (providerGlobalIds.containsKey(providerId)) {
+            if (providerLocalIds.get(providerId).isEmpty() ||
+                providerLocalIds.get(providerId).size() < start) { throw new RecordDatasetEmptyException(); }
+
+            List<GlobalId> globalIds = new ArrayList<>();
+            for (String globalId : providerGlobalIds.get(providerId).subList(start,
+                    Math.min(providerGlobalIds.get(providerId).size(), start + end))) {
+                LocalId provider = new LocalId();
+                provider.setProviderId(providerId);
+
+                GlobalId gId = new GlobalId();
+                gId.setLocalId(provider);
+                gId.setId(globalId);
+                globalIds.add(gId);
+            }
+            return globalIds;
+
+        }
+        throw new ProviderDoesNotExistException();
+    }
+
+    @Override
+    public void createIdMapping(String globalId, String providerId, String recordId)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            GlobalIdDoesNotExistException, RecordIdDoesNotExistException, IdHasBeenMappedException {
+        if (!providerLocalIds.containsKey(providerId)) { throw new ProviderDoesNotExistException(); }
+        if (!providerLocalIds.get(providerId).contains(recordId)) { throw new RecordIdDoesNotExistException(); }
+        if (providerGlobalIds.get(providerId).contains(globalId)) { throw new IdHasBeenMappedException(); }
+        for (Record record : records) {
+            if (StringUtils.equals(record.getId(), globalId)) {
+                Representation rep = new Representation();
+                rep.setRecordId(recordId);
+                rep.setDataProvider(providerId);
+                record.getRepresentations().add(rep);
+            }
+        }
+        throw new GlobalIdDoesNotExistException();
+    }
+
+    @Override
+    public void removeIdMapping(String providerId, String recordId)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordIdDoesNotExistException {
+        // Mockup it will be soft delete in reality
+        for (Record record : records) {
+            for (Representation representation : record.getRepresentations()) {
+                if (StringUtils.equals(representation.getDataProvider(), providerId) &&
+                    StringUtils.equals(recordId, representation.getRecordId())) {
+                    records.remove(record);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void deleteGlobalId(String globalId) throws DatabaseConnectionException,
+            GlobalIdDoesNotExistException {
+        // Mockup it will be soft delete in reality
+        for (Record record : records) {
+            if (StringUtils.equals(record.getId(), globalId)) {
+                records.remove(record);
+            }
+        }
+        throw new GlobalIdDoesNotExistException();
+    }
+}
