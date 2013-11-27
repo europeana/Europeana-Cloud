5dabd16719ac7b37c989ce95d14f70b5268d2da3
Krzysztof Sielski <sielski@man.poznan.pl>
2013-11-27 17:55:58 +0100

    comment to solrdao

--- services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/persistent/SolrDAO.java
+++ services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/persistent/SolrDAO.java
@@ -15,7 +15,8 @@ public abstract class SolrDAO {
 	/**
 	 * Inserts a new representation or updates a previously added (update might mean only setting persistent to true). 
 	 * Sometimes such insertion involves dataset assignment changes - to indicate the most recent version in data set.
-	 * If this is the case, the list of data sets to which the representation should be assigned to is provided into this method.
+	 * If this is the case, the list of data sets to which the representation should be assigned to is provided into 
+	 * this method (if this is update, previously assigned data sets will remain).
 	 * Moreover, those datasets should be removed from previous persistent version of this representation.
 	 *
 	 * @param rep
