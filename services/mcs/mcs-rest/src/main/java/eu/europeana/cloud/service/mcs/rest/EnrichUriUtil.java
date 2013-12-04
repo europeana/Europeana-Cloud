db19d5fa92195ec82e50e1d69751da35ec3bece0
Marcin Mielnicki <marcinm@man.poznan.pl>
2013-12-04 10:49:40 +0100

    Serving URIs with a context path

--- services/mcs/mcs-rest/src/main/java/eu/europeana/cloud/service/mcs/rest/EnrichUriUtil.java
+++ services/mcs/mcs-rest/src/main/java/eu/europeana/cloud/service/mcs/rest/EnrichUriUtil.java
@@ -1,20 +1,23 @@
 package eu.europeana.cloud.service.mcs.rest;
 
-import com.google.common.collect.ImmutableMap;
-import eu.europeana.cloud.common.model.DataProvider;
-import eu.europeana.cloud.common.model.DataSet;
-import eu.europeana.cloud.common.model.File;
-import eu.europeana.cloud.common.model.Record;
-import eu.europeana.cloud.common.model.Representation;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_DATASET;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_FILE;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_GID;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_PROVIDER;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_SCHEMA;
 import static eu.europeana.cloud.service.mcs.rest.ParamConstants.P_VER;
+
 import java.net.URI;
-import javax.ws.rs.core.UriBuilder;
+
 import javax.ws.rs.core.UriInfo;
+
+import com.google.common.collect.ImmutableMap;
+
+import eu.europeana.cloud.common.model.DataProvider;
+import eu.europeana.cloud.common.model.DataSet;
+import eu.europeana.cloud.common.model.File;
+import eu.europeana.cloud.common.model.Record;
+import eu.europeana.cloud.common.model.Representation;
 
 /**
  * Utility class that inserts absolute uris into classes that will be used as REST responses.
@@ -29,13 +32,13 @@ final class EnrichUriUtil {
 
 
     static void enrich(UriInfo uriInfo, Representation representation) {
-        URI allVersionsUri = UriBuilder.fromResource(RepresentationVersionsResource.class).buildFromMap(ImmutableMap.of(
+        URI allVersionsUri = uriInfo.getBaseUriBuilder().path(RepresentationVersionsResource.class).buildFromMap(ImmutableMap.of(
                 P_GID, representation.getRecordId(),
                 P_SCHEMA, representation.getSchema()));
         representation.setAllVersionsUri(uriInfo.resolve(allVersionsUri));
 
         if (representation.getVersion() != null) {
-            URI latestVersionUri = UriBuilder.fromResource(RepresentationVersionResource.class).buildFromMap(ImmutableMap.of(
+            URI latestVersionUri = uriInfo.getBaseUriBuilder().path(RepresentationVersionResource.class).buildFromMap(ImmutableMap.of(
                     P_GID, representation.getRecordId(),
                     P_SCHEMA, representation.getSchema(),
                     P_VER, representation.getVersion()));
@@ -55,7 +58,7 @@ final class EnrichUriUtil {
 
 
     static void enrich(UriInfo uriInfo, String recordId, String schema, String version, File file) {
-        URI fileUri = UriBuilder.fromResource(FileResource.class).buildFromMap(ImmutableMap.of(
+        URI fileUri = uriInfo.getBaseUriBuilder().path(FileResource.class).buildFromMap(ImmutableMap.of(
                 P_GID, recordId,
                 P_SCHEMA, schema,
                 P_VER, version,
@@ -65,14 +68,14 @@ final class EnrichUriUtil {
 
 
     static void enrich(UriInfo uriInfo, DataProvider provider) {
-        URI providerUri = UriBuilder.fromResource(DataProviderResource.class).buildFromMap(ImmutableMap.of(
+        URI providerUri = uriInfo.getBaseUriBuilder().path(DataProviderResource.class).buildFromMap(ImmutableMap.of(
                 P_PROVIDER, provider.getId()));
         provider.setUri(uriInfo.resolve(providerUri));
     }
 
 
     static void enrich(UriInfo uriInfo, DataSet dataSet) {
-        URI datasetUri = UriBuilder.fromResource(DataSetResource.class).buildFromMap(ImmutableMap.of(
+        URI datasetUri = uriInfo.getBaseUriBuilder().path(DataSetResource.class).buildFromMap(ImmutableMap.of(
                 P_PROVIDER, dataSet.getProviderId(),
                 P_DATASET, dataSet.getId()));
         dataSet.setUri(uriInfo.resolve(datasetUri));
