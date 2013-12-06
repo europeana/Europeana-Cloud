9bc642b8789e47ca5a8b58b531eb058823bbf700
Aleksandra Nowak <anowak@man.poznan.pl>
2013-12-06 10:51:13 +0100

    added constructor for connections without authentication

--- services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/persistent/CassandraConnectionProvider.java
+++ services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/persistent/CassandraConnectionProvider.java
@@ -22,7 +22,28 @@ public class CassandraConnectionProvider {
 
 
     /**
-     * Constructor.
+     * Constructor. Use it when your Cassandra cluster does not support authentication.
+     * 
+     * @param host
+     *            cassandra node host
+     * @param port
+     *            cassandra node cql service port
+     * @param keyspaceName
+     *            name of keyspace
+     */
+    public CassandraConnectionProvider(String host, int port, String keyspaceName) {
+        cluster = Cluster.builder().addContactPoint(host).withPort(port).build();
+        Metadata metadata = cluster.getMetadata();
+        log.info("Connected to cluster: {}", metadata.getClusterName());
+        for (Host h : metadata.getAllHosts()) {
+            log.info("Datatacenter: {}; Host: {}; Rack: {}", h.getDatacenter(), h.getAddress(), h.getRack());
+        }
+        session = cluster.connect(keyspaceName);
+    }
+
+
+    /**
+     * Constructor. Use it when your Cassandra cluster does support authentication.
      * 
      * @param host
      *            cassandra node host
