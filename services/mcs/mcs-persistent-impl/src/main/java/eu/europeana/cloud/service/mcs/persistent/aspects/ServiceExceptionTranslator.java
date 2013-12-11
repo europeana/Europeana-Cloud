867c76f69467f4108892a8347372bdde4a3a91f9
Krzysztof Sielski <sielski@man.poznan.pl>
2013-12-11 19:13:05 +0100

    Update to previous commit. (Aspect for wrapping runtimeexceptions thrown by DAOs into SystemException thrown by services.)

---
+++ services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/persistent/aspects/ServiceExceptionTranslator.java
@@ -0,0 +1,37 @@
+package eu.europeana.cloud.service.mcs.persistent.aspects;
+
+import eu.europeana.cloud.service.mcs.persistent.exception.SystemException;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+
+/**
+ * Aspect that translates every runtime exception thrown in services into {@link SystemException} that indicates any
+ * problem with another server (Cassandra, Swift, Solr).
+ */
+@Aspect
+public class ServiceExceptionTranslator {
+
+    /**
+     * Pointcut for class annotated with spring Service annotation
+     */
+    @Pointcut("@within(org.springframework.stereotype.Service)")
+    private void isService() {
+    }
+
+
+    /**
+     * Pointcut for our service implementation package
+     */
+    @Pointcut("within(eu.europeana.cloud.service.mcs.persistent..*)")
+    private void inMCSPersistentPackage() {
+    }
+
+
+    @AfterThrowing(pointcut = "isService() && inMCSPersistentPackage()", throwing = "ex")
+    public void wrapException(RuntimeException ex) {
+        SystemException wrappedException = new SystemException(ex.getMessage(), ex);
+        wrappedException.setStackTrace(ex.getStackTrace());
+        throw wrappedException;
+    }
+}
