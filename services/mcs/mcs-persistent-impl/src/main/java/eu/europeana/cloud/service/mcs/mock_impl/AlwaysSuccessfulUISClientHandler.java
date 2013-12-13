f56ac18d68d1756641c1b79e9f0cc107b85a5b5e
Aleksandra Nowak <anowak@man.poznan.pl>
2013-12-13 14:56:28 +0100

    ECL-96 AlwaysSuccessfulUISClientHandler moved to package for mocks

--- services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/mock_impl/AlwaysSuccessfulUISClientHandler.java
+++ services/mcs/mcs-persistent-impl/src/main/java/eu/europeana/cloud/service/mcs/mock_impl/AlwaysSuccessfulUISClientHandler.java
@@ -1,9 +1,15 @@
-package eu.europeana.cloud.service.mcs.rest.persistent;
+package eu.europeana.cloud.service.mcs.mock_impl;
 
 import eu.europeana.cloud.service.mcs.persistent.UISClientHandler;
 
 public class AlwaysSuccessfulUISClientHandler implements UISClientHandler {
 
+    /**
+     * Mock of UISClientHandler which always returns true.
+     * 
+     * @param cloudId
+     * @return true
+     */
     @Override
     public boolean recordExistInUIS(String cloudId) {
         return true;
