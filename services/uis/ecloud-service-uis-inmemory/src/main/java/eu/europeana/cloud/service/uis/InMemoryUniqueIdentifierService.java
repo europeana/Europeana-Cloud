071bfc1cf29bed26dfbbbb6b89d0f33ac0e26f8f
1nf1n1ty08 <markus.muhr@kb.nl>
2014-01-07 10:09:03 +0100

    added default constructor that initializes the daos and a constructor to allow providing of daos;

--- services/uis/ecloud-service-uis-inmemory/src/main/java/eu/europeana/cloud/service/uis/InMemoryUniqueIdentifierService.java
+++ services/uis/ecloud-service-uis-inmemory/src/main/java/eu/europeana/cloud/service/uis/InMemoryUniqueIdentifierService.java
@@ -30,151 +30,171 @@ import eu.europeana.cloud.service.uis.status.IdentifierErrorTemplate;
  */
 @Service
 public class InMemoryUniqueIdentifierService implements UniqueIdentifierService {
+    private final InMemoryCloudIdDao cloudIdDao;
+    private final InMemoryLocalIdDao localIdDao;
 
-	private InMemoryCloudIdDao cloudIdDao = new InMemoryCloudIdDao();
-	private InMemoryLocalIdDao localIdDao = new InMemoryLocalIdDao();
+    /**
+     * Creates a new instance of this class and intializes the service with given data access
+     * objects.
+     * 
+     * @param cloudIdDao
+     *            data access for cloud identifiers
+     * @param localIdDao
+     *            data access for local identifiers
+     */
+    public InMemoryUniqueIdentifierService(InMemoryCloudIdDao cloudIdDao,
+                                           InMemoryLocalIdDao localIdDao) {
+        this.cloudIdDao = cloudIdDao;
+        this.localIdDao = localIdDao;
+    }
 
-	@Override
-	public CloudId createCloudId(String... recordInfo) throws DatabaseConnectionException, RecordExistsException, ProviderDoesNotExistException {
-		String providerId = recordInfo[0];
-		String recordId = recordInfo.length > 1 ? recordInfo[1] : Base36.timeEncode(providerId);
-		String cloudId = Base36.encode(String.format("/%s/%s", providerId, recordId));
-		if (localIdDao.searchActive(providerId, recordId).size() > 0) {
-			throw new RecordExistsException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.RECORD_EXISTS.getHttpCode(),
-					IdentifierErrorTemplate.RECORD_EXISTS.getErrorInfo(providerId, recordId)));
-		}
-		localIdDao.insert(cloudId, providerId, recordId);
-		return cloudIdDao.insert(cloudId, providerId, recordId).get(0);
-	}
+    /**
+     * Creates a new instance of this class and initializes default data access objects.
+     */
+    public InMemoryUniqueIdentifierService() {
+        cloudIdDao = new InMemoryCloudIdDao();
+        localIdDao = new InMemoryLocalIdDao();
+    }
 
-	@Override
-	public CloudId getCloudId(String providerId, String recordId) throws DatabaseConnectionException,
-			RecordDoesNotExistException, ProviderDoesNotExistException {
-		List<CloudId> cloudIds = localIdDao.searchActive(providerId, recordId);
-		if (cloudIds.size() == 0) {
-			throw new RecordDoesNotExistException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.RECORD_DOES_NOT_EXIST.getHttpCode(),
-					IdentifierErrorTemplate.RECORD_DOES_NOT_EXIST.getErrorInfo(providerId, recordId)));
-		}
-		return cloudIds.get(0);
-	}
+    @Override
+    public CloudId createCloudId(String... recordInfo) throws DatabaseConnectionException,
+            RecordExistsException, ProviderDoesNotExistException {
+        String providerId = recordInfo[0];
+        String recordId = recordInfo.length > 1 ? recordInfo[1] : Base36.timeEncode(providerId);
+        String cloudId = Base36.encode(String.format("/%s/%s", providerId, recordId));
+        if (localIdDao.searchActive(providerId, recordId).size() > 0) { throw new RecordExistsException(
+                new IdentifierErrorInfo(IdentifierErrorTemplate.RECORD_EXISTS.getHttpCode(),
+                        IdentifierErrorTemplate.RECORD_EXISTS.getErrorInfo(providerId, recordId))); }
+        localIdDao.insert(cloudId, providerId, recordId);
+        return cloudIdDao.insert(cloudId, providerId, recordId).get(0);
+    }
 
-	@Override
-	public List<LocalId> getLocalIdsByCloudId(String cloudId) throws DatabaseConnectionException,
-			CloudIdDoesNotExistException, ProviderDoesNotExistException {
-		List<CloudId> cloudIds = cloudIdDao.searchActive(cloudId);
-		if (cloudIds.size() == 0) {
-			throw new CloudIdDoesNotExistException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId)));
-		}
-		List<LocalId> localIds = new ArrayList<>();
-		for (CloudId cId : cloudIds) {
-			if (localIdDao.searchActive(cId.getLocalId().getProviderId(), cId.getLocalId().getRecordId()).size() > 0) {
-				localIds.add(cId.getLocalId());
-			}
-		}
-		return localIds;
-	}
+    @Override
+    public CloudId getCloudId(String providerId, String recordId)
+            throws DatabaseConnectionException, RecordDoesNotExistException,
+            ProviderDoesNotExistException {
+        List<CloudId> cloudIds = localIdDao.searchActive(providerId, recordId);
+        if (cloudIds.size() == 0) { throw new RecordDoesNotExistException(new IdentifierErrorInfo(
+                IdentifierErrorTemplate.RECORD_DOES_NOT_EXIST.getHttpCode(),
+                IdentifierErrorTemplate.RECORD_DOES_NOT_EXIST.getErrorInfo(providerId, recordId))); }
+        return cloudIds.get(0);
+    }
 
-	@Override
-	public List<LocalId> getLocalIdsByProvider(String providerId, String start, int end)
-			throws DatabaseConnectionException, ProviderDoesNotExistException, RecordDatasetEmptyException {
-		List<CloudId> cloudIds = null;
-		if (start == null) {
-			cloudIds = localIdDao.searchActive(providerId);
-		} else {
-			cloudIds = localIdDao.searchActiveWithPagination(start, end, providerId);
-		}
-		List<LocalId> localIds = new ArrayList<>();
-		for (CloudId cloudId : cloudIds) {
-			localIds.add(cloudId.getLocalId());
-		}
-		return localIds;
-	}
+    @Override
+    public List<LocalId> getLocalIdsByCloudId(String cloudId) throws DatabaseConnectionException,
+            CloudIdDoesNotExistException, ProviderDoesNotExistException {
+        List<CloudId> cloudIds = cloudIdDao.searchActive(cloudId);
+        if (cloudIds.size() == 0) { throw new CloudIdDoesNotExistException(new IdentifierErrorInfo(
+                IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
+                IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId))); }
+        List<LocalId> localIds = new ArrayList<>();
+        for (CloudId cId : cloudIds) {
+            if (localIdDao.searchActive(cId.getLocalId().getProviderId(),
+                    cId.getLocalId().getRecordId()).size() > 0) {
+                localIds.add(cId.getLocalId());
+            }
+        }
+        return localIds;
+    }
 
-	@Override
-	public List<CloudId> getCloudIdsByProvider(String providerId, String start, int end)
-			throws DatabaseConnectionException, ProviderDoesNotExistException, RecordDatasetEmptyException {
-		List<CloudId> cloudIds;
+    @Override
+    public List<LocalId> getLocalIdsByProvider(String providerId, String start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException {
+        List<CloudId> cloudIds = null;
+        if (start == null) {
+            cloudIds = localIdDao.searchActive(providerId);
+        } else {
+            cloudIds = localIdDao.searchActiveWithPagination(start, end, providerId);
+        }
+        List<LocalId> localIds = new ArrayList<>();
+        for (CloudId cloudId : cloudIds) {
+            localIds.add(cloudId.getLocalId());
+        }
+        return localIds;
+    }
 
-		if (start == null) {
-			cloudIds = localIdDao.searchActive(providerId);
-		} else {
-			cloudIds = localIdDao.searchActiveWithPagination(start, end, providerId);
-		}
+    @Override
+    public List<CloudId> getCloudIdsByProvider(String providerId, String start, int end)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordDatasetEmptyException {
+        List<CloudId> cloudIds;
 
-		if (cloudIds.size() == 0) {
-			throw new RecordDatasetEmptyException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.RECORDSET_EMPTY.getHttpCode(),
-					IdentifierErrorTemplate.RECORDSET_EMPTY.getErrorInfo(providerId)));
-		}
-		return cloudIds;
-	}
+        if (start == null) {
+            cloudIds = localIdDao.searchActive(providerId);
+        } else {
+            cloudIds = localIdDao.searchActiveWithPagination(start, end, providerId);
+        }
 
-	@Override
-	public void createIdMapping(String cloudId, String providerId, String recordId) throws DatabaseConnectionException,
-			CloudIdDoesNotExistException, IdHasBeenMappedException, ProviderDoesNotExistException {
-		List<CloudId> localIds = localIdDao.searchActive(providerId, recordId);
-		if (localIds.size() != 0) {
-			throw new IdHasBeenMappedException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.ID_HAS_BEEN_MAPPED.getHttpCode(),
-					IdentifierErrorTemplate.ID_HAS_BEEN_MAPPED.getErrorInfo(providerId, recordId,cloudId)));
-		}
-		List<CloudId> cloudIds = cloudIdDao.searchActive(cloudId);
-		if (cloudIds.size() == 0) {
-			throw new CloudIdDoesNotExistException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId)));
-		}
+        if (cloudIds.size() == 0) { throw new RecordDatasetEmptyException(new IdentifierErrorInfo(
+                IdentifierErrorTemplate.RECORDSET_EMPTY.getHttpCode(),
+                IdentifierErrorTemplate.RECORDSET_EMPTY.getErrorInfo(providerId))); }
+        return cloudIds;
+    }
 
-		localIdDao.insert(cloudId, providerId, recordId);
-		cloudIdDao.insert(cloudId, providerId, recordId);
-	}
+    @Override
+    public void createIdMapping(String cloudId, String providerId, String recordId)
+            throws DatabaseConnectionException, CloudIdDoesNotExistException,
+            IdHasBeenMappedException, ProviderDoesNotExistException {
+        List<CloudId> localIds = localIdDao.searchActive(providerId, recordId);
+        if (localIds.size() != 0) { throw new IdHasBeenMappedException(new IdentifierErrorInfo(
+                IdentifierErrorTemplate.ID_HAS_BEEN_MAPPED.getHttpCode(),
+                IdentifierErrorTemplate.ID_HAS_BEEN_MAPPED.getErrorInfo(providerId, recordId,
+                        cloudId))); }
+        List<CloudId> cloudIds = cloudIdDao.searchActive(cloudId);
+        if (cloudIds.size() == 0) { throw new CloudIdDoesNotExistException(new IdentifierErrorInfo(
+                IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
+                IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId))); }
 
-	@Override
-	public void removeIdMapping(String providerId, String recordId) throws DatabaseConnectionException,
-			ProviderDoesNotExistException, RecordIdDoesNotExistException {
+        localIdDao.insert(cloudId, providerId, recordId);
+        cloudIdDao.insert(cloudId, providerId, recordId);
+    }
 
-		localIdDao.delete(providerId, recordId);
+    @Override
+    public void removeIdMapping(String providerId, String recordId)
+            throws DatabaseConnectionException, ProviderDoesNotExistException,
+            RecordIdDoesNotExistException {
 
-	}
+        localIdDao.delete(providerId, recordId);
 
-	@Override
-	public void deleteCloudId(String cloudId) throws DatabaseConnectionException, CloudIdDoesNotExistException, ProviderDoesNotExistException, RecordIdDoesNotExistException {
-		if (!(cloudIdDao.searchActive(cloudId).size() > 0)) {
-			throw new CloudIdDoesNotExistException(new IdentifierErrorInfo(
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
-					IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId)));
-		}
-		List<CloudId> localIds = cloudIdDao.searchActive(cloudId);
-		for (CloudId cId : localIds) {
-			localIdDao.delete(cId.getLocalId().getProviderId(), cId.getLocalId().getRecordId());
-			cloudIdDao.delete(cloudId, cId.getLocalId().getProviderId(), cId.getLocalId().getRecordId());
-		}
-	}
+    }
 
-	@Override
-	public String getHost() {
-		return "testhost";
-	}
+    @Override
+    public void deleteCloudId(String cloudId) throws DatabaseConnectionException,
+            CloudIdDoesNotExistException, ProviderDoesNotExistException,
+            RecordIdDoesNotExistException {
+        if (!(cloudIdDao.searchActive(cloudId).size() > 0)) { throw new CloudIdDoesNotExistException(
+                new IdentifierErrorInfo(
+                        IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getHttpCode(),
+                        IdentifierErrorTemplate.CLOUDID_DOES_NOT_EXIST.getErrorInfo(cloudId))); }
+        List<CloudId> localIds = cloudIdDao.searchActive(cloudId);
+        for (CloudId cId : localIds) {
+            localIdDao.delete(cId.getLocalId().getProviderId(), cId.getLocalId().getRecordId());
+            cloudIdDao.delete(cloudId, cId.getLocalId().getProviderId(),
+                    cId.getLocalId().getRecordId());
+        }
+    }
 
-	@Override
-	public String getKeyspace() {
-		return "testkeyspace";
-	}
+    @Override
+    public String getHost() {
+        return "testhost";
+    }
 
-	@Override
-	public String getPort() {
-		return "testport";
-	}
+    @Override
+    public String getKeyspace() {
+        return "testkeyspace";
+    }
 
-	/**
-	 * Empty daos
-	 */
-	public void reset() {
-		localIdDao.reset();
-		cloudIdDao.reset();
-	}
+    @Override
+    public String getPort() {
+        return "testport";
+    }
+
+    /**
+     * Empty daos
+     */
+    public void reset() {
+        localIdDao.reset();
+        cloudIdDao.reset();
+    }
 }
