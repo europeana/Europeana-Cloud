1195b3a91a1836466474466304f2dd2632269e8e
1nf1n1ty08 <markus.muhr@kb.nl>
2014-01-21 10:26:27 +0100

    formatting;

--- common/src/main/java/eu/europeana/cloud/common/exceptions/GenericException.java
+++ common/src/main/java/eu/europeana/cloud/common/exceptions/GenericException.java
@@ -3,7 +3,6 @@ package eu.europeana.cloud.common.exceptions;
 import eu.europeana.cloud.common.model.IdentifierErrorInfo;
 import eu.europeana.cloud.common.response.ErrorInfo;
 
-
 /**
  * Generic Exception
  * 
@@ -11,31 +10,35 @@ import eu.europeana.cloud.common.response.ErrorInfo;
  * @since Dec 17, 2013
  */
 public class GenericException extends Exception {
-	
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = -6146223626718871100L;
-	private IdentifierErrorInfo errorInfo;
-	
-	public GenericException(ErrorInfo e){
-		super(e.getDetails());
-	}
-	
-	/**
-	 * Creates a new instance of this class.
-	 * @param errorInfo
-	 */
-	public GenericException (IdentifierErrorInfo errorInfo){
-		super(errorInfo.getErrorInfo().getDetails());
-		this.errorInfo = errorInfo;
-	}
-	
-	/**
-	 * Retrieve the error information that caused the exception happen
-	 * @return The error information
-	 */
-	public IdentifierErrorInfo getErrorInfo() {
-		return this.errorInfo;
-	}
+    private static final long   serialVersionUID = -6146223626718871100L;
+    
+    private IdentifierErrorInfo errorInfo;
+
+    /**
+     * Creates a new instance of this class.
+     * 
+     * @param e
+     */
+    public GenericException(ErrorInfo e) {
+        super(e.getDetails());
+    }
+
+    /**
+     * Creates a new instance of this class.
+     * 
+     * @param errorInfo
+     */
+    public GenericException(IdentifierErrorInfo errorInfo) {
+        super(errorInfo.getErrorInfo().getDetails());
+        this.errorInfo = errorInfo;
+    }
+
+    /**
+     * Retrieve the error information that caused the exception happen
+     * 
+     * @return The error information
+     */
+    public IdentifierErrorInfo getErrorInfo() {
+        return this.errorInfo;
+    }
 }
--- common/src/main/java/eu/europeana/cloud/common/exceptions/ProviderDoesNotExistException.java
+++ common/src/main/java/eu/europeana/cloud/common/exceptions/ProviderDoesNotExistException.java
@@ -5,27 +5,28 @@ import eu.europeana.cloud.common.response.ErrorInfo;
 
 /**
  * Exception thrown when the provider Id does not exist
+ * 
  * @author Yorgos.Mamakis@ kb.nl
- *
+ * @since Dec 17, 2013
  */
 public class ProviderDoesNotExistException extends GenericException {
+    private static final long serialVersionUID = 468350985177621312L;
+    
+    /**
+     * Creates a new instance of this class.
+     * 
+     * @param e
+     */
+    public ProviderDoesNotExistException(ErrorInfo e) {
+        super(e);
+    }
 
-
-	public ProviderDoesNotExistException(ErrorInfo e){
-		super(e);
-	}
-	
-	/**
-	 * Creates a new instance of this class.
-	 * @param errorInfo
-	 */
-	public ProviderDoesNotExistException(IdentifierErrorInfo errorInfo) {
-		super(errorInfo);
-	}
-
-	/**
-	 * 
-	 */
-	private static final long serialVersionUID = 468350985177621312L;
-
+    /**
+     * Creates a new instance of this class.
+     * 
+     * @param errorInfo
+     */
+    public ProviderDoesNotExistException(IdentifierErrorInfo errorInfo) {
+        super(errorInfo);
+    }
 }
