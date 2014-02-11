0cd9cfe5afb6d47548881fea8272ecb9684a3926
Aleksandra Nowak <anowak@man.poznan.pl>
2014-02-11 14:24:33 +0100

    MCS drivers calls corrected

--- tools/ecloud-dlf-ingestion-tool/src/main/java/eu/europeana/cloud/dlf/ingestion/tool/DLFMigrator.java
+++ tools/ecloud-dlf-ingestion-tool/src/main/java/eu/europeana/cloud/dlf/ingestion/tool/DLFMigrator.java
@@ -8,6 +8,8 @@ import eu.europeana.cloud.mcs.driver.FileServiceClient;
 import eu.europeana.cloud.mcs.driver.RepresentationServiceClient;
 import eu.europeana.cloud.mcs.driver.SearchParams;
 import eu.europeana.cloud.mcs.driver.SearchServiceClient;
+import eu.europeana.cloud.service.mcs.exception.DataSetAlreadyExistsException;
+import eu.europeana.cloud.service.mcs.exception.ProviderNotExistsException;
 import java.io.File;
 import java.io.InputStream;
 import java.util.ArrayList;
@@ -19,6 +21,7 @@ import java.util.List;
 public class DLFMigrator {
 
     String uisUrl = "";
+    String mcsUrl = "";
     String providerId = "providerId";
 
     private final UISClient uisClient;
@@ -32,7 +35,7 @@ public class DLFMigrator {
         uisClient = new UISClient(uisUrl);
         representationClient = new RepresentationServiceClient();
         filesClient = new FileServiceClient();
-        dataSetClient = new DataSetServiceClient();
+        dataSetClient = new DataSetServiceClient(mcsUrl);
         searchClient = new SearchServiceClient();
     }
 
@@ -90,9 +93,14 @@ public class DLFMigrator {
 
 
     private void createDataSet() {
-        dataSetClient.createDataSet("dataSetId", providerId);
-        //for all created files
-        dataSetClient.assignRepresentationToDataSet("dataSetId", "providerId", "cloudId", "schemaId", "versionId");
+        try {
+            dataSetClient.createDataSet("dataSetId", providerId, "description");
+            //for all created files
+            dataSetClient.assignRepresentationToDataSet("dataSetId", "providerId", "cloudId", "schemaId", "versionId");
+        } catch (DataSetAlreadyExistsException ex) {
+
+        } catch (ProviderNotExistsException ex) {
+        }
     }
 
 
