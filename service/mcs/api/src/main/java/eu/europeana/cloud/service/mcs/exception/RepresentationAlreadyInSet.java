48a74d65399afe8eada79d65466efc29399040e3
EwaKowalczuk <freyja@man.poznan.pl>
2014-02-13 13:19:30 +0100

    Removed unused McsErrorCodes and added new exception for code.

---
+++ service/mcs/api/src/main/java/eu/europeana/cloud/service/mcs/exception/RepresentationAlreadyInSet.java
@@ -0,0 +1,37 @@
+package eu.europeana.cloud.service.mcs.exception;
+
+/**
+ * Thrown if there was attempt to use representation that does not exist (or representation in version that does not
+ * exist).
+ */
+public class RepresentationAlreadyInSet extends BaseMCSException {
+
+    /**
+     * Constructs a RepresentationNotExistsException with no specified detail message.
+     */
+    public RepresentationAlreadyInSet() {
+    }
+
+
+    /**
+     * Constructs a RepresentationNotExistsException with the specified detail message.
+     * 
+     * @param message
+     *            the detail message
+     */
+    public RepresentationAlreadyInSet(String message) {
+        super(message);
+    }
+
+
+    /**
+     * Constructs a RepresentationNotExistsException with the specified Throwable.
+     * 
+     * @param cause
+     *            the cause
+     */
+    public RepresentationAlreadyInSet(Throwable cause) {
+        super(cause);
+    }
+
+}
