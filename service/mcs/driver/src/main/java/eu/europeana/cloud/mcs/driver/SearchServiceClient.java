065bd68b5169e959c6e064f72b96a4563452410d
Aleksandra Nowak <anowak@man.poznan.pl>
2014-02-14 12:54:24 +0100

    bugfix

--- service/mcs/driver/src/main/java/eu/europeana/cloud/mcs/driver/SearchServiceClient.java
+++ service/mcs/driver/src/main/java/eu/europeana/cloud/mcs/driver/SearchServiceClient.java
@@ -1,11 +1,13 @@
 package eu.europeana.cloud.mcs.driver;
 
+import eu.europeana.cloud.service.mcs.RepresentationSearchParams;
+
 /**
  *
  */
 public class SearchServiceClient {
 
-    public void search(SearchParams params) {
+    public void search(RepresentationSearchParams params) {
 
     }
 }
