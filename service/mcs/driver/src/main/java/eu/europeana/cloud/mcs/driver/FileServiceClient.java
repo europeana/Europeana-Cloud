17e8036067ea8b64ffa4f8afce08eb50cd42fdee
akrystian <akrystian@gmail.com>
2014-02-28 15:12:04 +0100

    ECL-134 - MCS Wrapper: file related part

--- service/mcs/driver/src/main/java/eu/europeana/cloud/mcs/driver/FileServiceClient.java
+++ service/mcs/driver/src/main/java/eu/europeana/cloud/mcs/driver/FileServiceClient.java
@@ -1,16 +1,17 @@
 package eu.europeana.cloud.mcs.driver;
 
-import com.google.common.hash.Hashing;
-import com.google.common.io.ByteStreams;
 import eu.europeana.cloud.common.response.ErrorInfo;
 import eu.europeana.cloud.common.web.ParamConstants;
+import static eu.europeana.cloud.common.web.ParamConstants.H_RANGE;
 import eu.europeana.cloud.service.mcs.exception.FileNotExistsException;
 import eu.europeana.cloud.service.mcs.exception.CannotModifyPersistentRepresentationException;
 import eu.europeana.cloud.service.mcs.exception.MCSException;
 import eu.europeana.cloud.service.mcs.exception.RepresentationNotExistsException;
 import eu.europeana.cloud.service.mcs.exception.WrongContentRangeException;
+
 import java.io.IOException;
 import java.io.InputStream;
+
 import java.net.URI;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.Entity;
@@ -19,6 +20,7 @@ import javax.ws.rs.client.WebTarget;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.Status;
+
 import org.glassfish.jersey.client.JerseyClientBuilder;
 import org.glassfish.jersey.media.multipart.FormDataMultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
@@ -30,6 +32,11 @@ public class FileServiceClient {
     private final Client client;
     private final String baseUrl;
     private static final Logger logger = LoggerFactory.getLogger(FileServiceClient.class);
+    //records/CLOUDID/representations/REPRESENTATIONNAME/versions/VERSION/files/
+    private static final String filesPath = "records/{" + ParamConstants.P_CLOUDID + "}/representations/{"
+            + ParamConstants.P_REPRESENTATIONNAME + "}/versions/{" + ParamConstants.P_VER + "}/files";
+    //records/CLOUDID/representations/REPRESENTATIONNAME/versions/VERSION/files/FILENAME/
+    private static final String filePath = filesPath + "/" + ParamConstants.P_FILENAME;
 
 
     /**
@@ -58,7 +65,7 @@ public class FileServiceClient {
      * @param range
      *            range of bytes to return (optional). Range header can be found in Hypertext Transfer Protocol
      *            HTTP/1.1, section 14.35 Range).
-     * @return InputStream
+     * @return InputStream returned content.
      * @throws RepresentationNotExistsException
      *             when requested representation (or representation version) does not exist.
      * @throws FileNotExistsException
@@ -70,16 +77,16 @@ public class FileServiceClient {
      */
     public InputStream getFile(String fileName, String cloudId, String schema, String version, String range)
             throws RepresentationNotExistsException, FileNotExistsException, WrongContentRangeException, MCSException {
-        WebTarget target = client.target(this.baseUrl)
-                .path("records/{ID}/representations/{SCHEMA}/versions/{VERSION}/files/{FILE_NAME}")
-                .resolveTemplate("MCS_SERVICE_ADDRES", baseUrl).resolveTemplate("ID", cloudId)
-                .resolveTemplate("SCHEMA", schema).resolveTemplate("VERSION", version)
-                .resolveTemplate("FILE_NAME", fileName);
+        WebTarget target = client.target(baseUrl).path(filePath).resolveTemplate(ParamConstants.P_CLOUDID, cloudId)
+                .resolveTemplate(ParamConstants.P_REPRESENTATIONNAME, schema)
+                .resolveTemplate(ParamConstants.P_VER, version).resolveTemplate(ParamConstants.P_FILENAME, fileName);
         Builder requset;
+
         if (range != null)
-            requset = target.request().header("Range", range);
+            requset = target.request().header(H_RANGE, range);
         else
             requset = target.request();
+
         Response response = requset.get();
         if (response.getStatus() == Response.Status.OK.getStatusCode()) {
             InputStream contentResponse = response.readEntity(InputStream.class);
@@ -92,6 +99,55 @@ public class FileServiceClient {
 
 
     /**
+     * Returns file content with checking checksum.
+     * 
+     * @param cloudId
+     *            id of uploaded file.
+     * @param schema
+     *            schema of uploaded file.
+     * @param version
+     *            version of uploaded file.
+     * @param data
+     *            InputStream of uploaded file.
+     * @param mediaType
+     *            mediaType of uploaded file.
+     * @param expectedMd5
+     *            expected MD5 checksum.
+     * @return URI of uploaded file.
+     * @throws IOException
+     * @throws RepresentationNotExistsException
+     *             when representation does not exist in specified version.
+     * @throws CannotModifyPersistentRepresentationException
+     *             when specified representation version is persistent and modifying its files is not allowed.
+     * @throws MCSException
+     *             on unexpected situations.
+     */
+    public URI uploadFile(String cloudId, String schema, String version, InputStream data, String mediaType,
+            String expectedMd5)
+            throws IOException, RepresentationNotExistsException, CannotModifyPersistentRepresentationException,
+            MCSException {
+        WebTarget target = client.target(baseUrl).path(filesPath).resolveTemplate(ParamConstants.P_CLOUDID, cloudId)
+                .resolveTemplate(ParamConstants.P_REPRESENTATIONNAME, schema)
+                .resolveTemplate(ParamConstants.P_VER, version);
+        FormDataMultiPart multipart = new FormDataMultiPart().field(ParamConstants.F_FILE_MIME, mediaType).field(
+            ParamConstants.F_FILE_DATA, data, MediaType.APPLICATION_OCTET_STREAM_TYPE);
+        Builder request = target.request();
+
+        Response response = request.post(Entity.entity(multipart, multipart.getMediaType()));
+
+        if (response.getStatus() == Status.CREATED.getStatusCode()) {
+            if (expectedMd5.equals(response.getEntityTag().getValue()))
+                throw new IOException("Incorrect MD5 checksum");
+            return response.getLocation();
+        } else {
+            ErrorInfo errorInfo = response.readEntity(ErrorInfo.class);
+            throw MCSExceptionProvider.generateException(errorInfo);
+        }
+    }
+
+
+    /**
+     * Returns file content without checking checksum.
      * 
      * @param cloudId
      *            id of uploaded file.
@@ -108,31 +164,24 @@ public class FileServiceClient {
      * @throws RepresentationNotExistsException
      *             when representation does not exist in specified version.
      * @throws CannotModifyPersistentRepresentationException
-     *             when specified representation version is persistent and modyfying its files is not allowed.
+     *             when specified representation version is persistent and modifying its files is not allowed.
      * @throws MCSException
      *             on unexpected situations.
      */
     public URI uploadFile(String cloudId, String schema, String version, InputStream data, String mediaType)
             throws IOException, RepresentationNotExistsException, CannotModifyPersistentRepresentationException,
             MCSException {
-        WebTarget target = client.target(this.baseUrl)
-                .path("records/{ID}/representations/{SCHEMA}/versions/{VERSION}/files/")
-                .resolveTemplate("MCS_SERVICE_ADDRES", baseUrl).resolveTemplate("ID", cloudId)
-                .resolveTemplate("SCHEMA", schema).resolveTemplate("VERSION", version);
+        WebTarget target = client.target(baseUrl).path(filesPath).resolveTemplate(ParamConstants.P_CLOUDID, cloudId)
+                .resolveTemplate(ParamConstants.P_REPRESENTATIONNAME, schema)
+                .resolveTemplate(ParamConstants.P_VER, version);
         FormDataMultiPart multipart = new FormDataMultiPart().field(ParamConstants.F_FILE_MIME, mediaType).field(
             ParamConstants.F_FILE_DATA, data, MediaType.APPLICATION_OCTET_STREAM_TYPE);
         Builder request = target.request();
+
         Response response = request.post(Entity.entity(multipart, multipart.getMediaType()));
+
         if (response.getStatus() == Status.CREATED.getStatusCode()) {
-            String resultMd5 = response.getEntityTag().getValue();
-            byte[] contentByte = ByteStreams.toByteArray(data);
-            String contentMd5 = Hashing.md5().hashBytes(contentByte).toString();
-            if (contentMd5.equals(resultMd5)) {
-                return response.getLocation();
-            } else {
-                ErrorInfo errorInfo = new ErrorInfo("000", "Incorrect md5 chcecsum");
-                throw MCSExceptionProvider.generateException(errorInfo);
-            }
+            return response.getLocation();
         } else {
             ErrorInfo errorInfo = response.readEntity(ErrorInfo.class);
             throw MCSExceptionProvider.generateException(errorInfo);
@@ -141,6 +190,7 @@ public class FileServiceClient {
 
 
     /**
+     * Modifies existed file with checking checksum.
      * 
      * @param cloudId
      *            id of modifying file.
@@ -153,44 +203,72 @@ public class FileServiceClient {
      * @param mediaType
      *            mediaType of modifying file.
      * @param fileName
+     *            name of modifying file.
+     * @param expectedMd5
+     *            expected MD5 checksum.
      * @return URI to modified file.
      * @throws IOException
+     *             when checksum is incorrect.
      * @throws RepresentationNotExistsException
+     *             when representation does not exist in specified version.
      * @throws CannotModifyPersistentRepresentationException
+     *             when specified representation version is persistent and modifying its files is not allowed.
      * @throws MCSException
+     *             on unexpected situations.
      */
     public URI modyfiyFile(String cloudId, String schema, String version, InputStream data, String mediaType,
-            String fileName)
+            String fileName, String expectedMd5)
             throws IOException, RepresentationNotExistsException, CannotModifyPersistentRepresentationException,
             MCSException {
-        WebTarget target = client.target(this.baseUrl)
-                .path("records/{ID}/representations/{SCHEMA}/versions/{VERSION}/files/{FILE}/")
-                .resolveTemplate("MCS_SERVICE_ADDRES", baseUrl).resolveTemplate("ID", cloudId)
-                .resolveTemplate("SCHEMA", schema).resolveTemplate("VERSION", version)
-                .resolveTemplate("FILE", fileName);
+        WebTarget target = client.target(baseUrl).path(filePath).resolveTemplate(ParamConstants.P_CLOUDID, cloudId)
+                .resolveTemplate(ParamConstants.P_REPRESENTATIONNAME, schema)
+                .resolveTemplate(ParamConstants.P_VER, version).resolveTemplate(ParamConstants.P_FILENAME, fileName);
         FormDataMultiPart multipart = new FormDataMultiPart().field(ParamConstants.F_FILE_MIME, mediaType).field(
             ParamConstants.F_FILE_DATA, data, MediaType.APPLICATION_OCTET_STREAM_TYPE);
         Response response = target.request().put(Entity.entity(multipart, multipart.getMediaType()));
         if (response.getStatus() == Status.NO_CONTENT.getStatusCode()) {
-            String resultMd5 = response.getEntityTag().getValue();
-            byte[] contentByte = ByteStreams.toByteArray(data);
-            String contentMd5 = Hashing.md5().hashBytes(contentByte).toString();
-            if (contentMd5.equals(resultMd5)) {
-                return response.getLocation();
-            } else {
-                ErrorInfo errorInfo = new ErrorInfo("000", "Incorrect md5 chcecsum");
-                throw MCSExceptionProvider.generateException(errorInfo);
-            }
+            if (expectedMd5.equals(response.getEntityTag().getValue()))
+                throw new IOException("Incorrect MD5 checksum");
+            return response.getLocation();
+
         } else {
             ErrorInfo errorInfo = response.readEntity(ErrorInfo.class);
             throw MCSExceptionProvider.generateException(errorInfo);
         }
-
     }
 
 
-    public void deleteFile(String cloudId, String schema, String versionId, String fileName) {
-
+    /**
+     * Deletes existed file.
+     * 
+     * @param cloudId
+     *            id of deleting file.
+     * @param schema
+     *            schema of deleting file.
+     * @param version
+     *            version of deleting file.
+     * @param fileName
+     *            name of deleting file.
+     * @throws RepresentationNotExistsException
+     *             when representation does not exist in specified version.
+     * @throws FileNotExistsException
+     *             when requested file does not exist.
+     * @throws CannotModifyPersistentRepresentationException
+     *             when specified representation version is persistent and modifying its files is not allowed.
+     * @throws MCSException
+     *             on unexpected situations.
+     */
+    public void deleteFile(String cloudId, String schema, String version, String fileName)
+            throws RepresentationNotExistsException, FileNotExistsException,
+            CannotModifyPersistentRepresentationException, MCSException {
+        WebTarget target = client.target(baseUrl).path(filePath).resolveTemplate(ParamConstants.P_CLOUDID, cloudId)
+                .resolveTemplate(ParamConstants.P_REPRESENTATIONNAME, schema)
+                .resolveTemplate(ParamConstants.P_VER, version).resolveTemplate(ParamConstants.P_FILENAME, fileName);
+        Response response = target.request().delete();
+        if (response.getStatus() != Response.Status.NO_CONTENT.getStatusCode()) {
+            ErrorInfo errorInfo = response.readEntity(ErrorInfo.class);
+            throw MCSExceptionProvider.generateException(errorInfo);
+        }
     }
 
 }
