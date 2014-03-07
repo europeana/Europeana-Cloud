d14612eadc4ea7b3b9805eecdb57d553d404fb70
koufakis <emmanouil.Koufakis@theeuropeanlibrary.org>
2014-03-07 11:31:47 +0100

    ECL-144 UIS api refactoring UISParamConstants contains shared query params between the Wrapper and the Rest API.

---
+++ common/src/main/java/eu/europeana/cloud/common/web/UISParamConstants.java
@@ -0,0 +1,23 @@
+package eu.europeana.cloud.common.web;
+
+/**
+ * Constants for the UIS.
+ * 
+ * Shared between the Wrapper and the Rest API.
+ */
+public final class UISParamConstants {
+	
+	private UISParamConstants() {
+	}
+
+    // Query parameter names
+    public static final String Q_PROVIDER = "providerId";
+
+    public static final String Q_PROVIDER_ID = "providerId";
+	
+    public static final String Q_RECORD_ID = "recordId";
+	
+    public static final String Q_FROM = "from";
+
+    public static final String Q_TO = "to";
+}
