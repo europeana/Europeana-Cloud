a7b2ec3a8d3fb3dc50e1e0ffb0cb4e88b73ed82a
1nf1n1ty08 <drakash08>
2014-08-07 15:54:50 +0200

    setup for authorization unit tests

--- service/uis/api/src/main/java/eu/europeana/cloud/service/uis/DataProviderService.java
+++ service/uis/api/src/main/java/eu/europeana/cloud/service/uis/DataProviderService.java
@@ -8,65 +8,53 @@ import eu.europeana.cloud.service.uis.exception.ProviderAlreadyExistsException;
 
 /**
  * Service for data provider operations.
- * 
+ *
  */
 public interface DataProviderService {
 
     /**
      * Returns all providers (in slices).
-     * 
-     * @param thresholdProviderId
-     *            if null - will return first result slice. Result slices contain token for next pages, which should be
-     *            provided in this parameter.
-     * @param limit
-     *            max number of results in one slice.
+     *
+     * @param thresholdProviderId if null - will return first result slice.
+     * Result slices contain token for next pages, which should be provided in
+     * this parameter.
+     * @param limit max number of results in one slice.
      * @return list of providers as a result slice.
      */
     ResultSlice<DataProvider> getProviders(String thresholdProviderId, int limit);
 
-
     /**
      * Returns data provider for its id.
-     * 
-     * @param providerId
-     *            provider id.
+     *
+     * @param providerId provider id.
      * @return data provider with given id.
-     * @throws ProviderDoesNotExistException
-     *             if threre is no provider with such id.
+     * @throws ProviderDoesNotExistException if threre is no provider with such
+     * id.
      */
     DataProvider getProvider(String providerId)
             throws ProviderDoesNotExistException;
 
-
     /**
      * Creates a new data provider.
-     * 
-     * @param providerId
-     *            id of provider.
-     * @param properties
-     *            properties of provider.
+     *
+     * @param providerId id of provider.
+     * @param properties properties of provider.
      * @return created provider.
-     * @throws ProviderAlreadyExistsException
-     *             provider with this id already exists
+     * @throws ProviderAlreadyExistsException provider with this id already
+     * exists
      */
     DataProvider createProvider(String providerId, DataProviderProperties properties)
             throws ProviderAlreadyExistsException;
 
-
     /**
      * Updates properties for a data provider.
-     * 
-     * @param providerId
-     *            id of provider.
-     * @param properties
-     *            new properties of provider.
+     *
+     * @param providerId id of provider.
+     * @param properties new properties of provider.
      * @return updated provider.
-     * @throws ProviderDoesNotExistException
-     *             if threre is no provider with such id.
+     * @throws ProviderDoesNotExistException if threre is no provider with such
+     * id.
      */
     DataProvider updateProvider(String providerId, DataProviderProperties properties)
             throws ProviderDoesNotExistException;
-
-
-  
 }
