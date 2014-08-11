21d309aff855e9d6ec3802758eb66a6f4052aaf5
Aleksandra Nowak <anowak@man.poznan.pl>
2014-08-11 11:30:23 +0200

    ECL-272 javadoc added

--- common/src/main/java/eu/europeana/cloud/service/mcs/messages/RemoveRepresentationMessage.java
+++ common/src/main/java/eu/europeana/cloud/service/mcs/messages/RemoveRepresentationMessage.java
@@ -1,15 +1,19 @@
 package eu.europeana.cloud.service.mcs.messages;
 
 /**
- * Message about removing a
- * {@link eu.europeana.cloud.common.model.Representation representation} of
- * certain representation name in all versions from a
- * {@link eu.europeana.cloud.common.model.Record record} of certain cloudId.
+ * Message about removing a {@link eu.europeana.cloud.common.model.Representation representation} of certain
+ * representation name in all versions from a {@link eu.europeana.cloud.common.model.Record record} of certain cloudId.
  */
 public class RemoveRepresentationMessage extends AbstractMessage {
 
+    /**
+     * Constructs RemoveRepresentationMessage with given payload
+     * 
+     * @param payload
+     *            message payload
+     */
     public RemoveRepresentationMessage(String payload) {
-	super(payload);
+        super(payload);
     }
 
 }
--- common/src/main/java/eu/europeana/cloud/service/mcs/messages/RemoveRepresentationVersionMessage.java
+++ common/src/main/java/eu/europeana/cloud/service/mcs/messages/RemoveRepresentationVersionMessage.java
@@ -1,14 +1,19 @@
 package eu.europeana.cloud.service.mcs.messages;
 
 /**
- * Message about removing a
- * {@link eu.europeana.cloud.common.model.Representation representation} version
- * from a certain {@link eu.europeana.cloud.common.model.Record record}.
+ * Message about removing a {@link eu.europeana.cloud.common.model.Representation representation} version from a certain
+ * {@link eu.europeana.cloud.common.model.Record record}.
  */
 public class RemoveRepresentationVersionMessage extends AbstractMessage {
 
+    /**
+     * Constructs RemoveRepresentationVersionMessage with given payload
+     * 
+     * @param payload
+     *            message payload
+     */
     public RemoveRepresentationVersionMessage(String payload) {
-	super(payload);
+        super(payload);
     }
 
 }
