778a3704d3b5f3eed38c3a48256fa0e409fb32c0
1nf1n1ty08 <markus.muhr@kb.nl>
2014-08-19 16:44:00 +0200

    utilities for getting creators and remove cassandra tests

---
+++ service/aas/authentication/src/main/java/eu/europeana/cloud/service/aas/authentication/SpringUserUtils.java
@@ -0,0 +1,28 @@
+package eu.europeana.cloud.service.aas.authentication;
+
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+
+/**
+ * Utility class containing function for spring security usage.
+ * 
+ * @author Markus Muhr (markus.muhr@theeuropeanlibrary.org)
+ */
+public final class SpringUserUtils {
+    /**
+     * @return Name of the currently logged in user
+     */
+    public static String getUsername() {
+        String username = null;
+        Authentication auth = SecurityContextHolder.getContext().getAuthentication();
+        if (auth != null) {
+            if (auth.getPrincipal() instanceof UserDetails) {
+                username = ((UserDetails) auth.getPrincipal()).getUsername();
+            } else {
+                username = auth.getPrincipal().toString();
+            }
+        }
+        return username;
+    }
+}
