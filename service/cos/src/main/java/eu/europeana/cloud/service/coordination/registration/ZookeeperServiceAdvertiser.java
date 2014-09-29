81db3f7074d17cd36c2465d45d2861fc6e35c9c2
koufakis <emmanouil.koufakis@kb.nl>
2014-09-29 18:33:20 +0200

    ECL-194 cos can now autodetect the address of each service

--- service/cos/src/main/java/eu/europeana/cloud/service/coordination/registration/ZookeeperServiceAdvertiser.java
+++ service/cos/src/main/java/eu/europeana/cloud/service/coordination/registration/ZookeeperServiceAdvertiser.java
@@ -1,14 +1,26 @@
 package eu.europeana.cloud.service.coordination.registration;
 
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.SocketException;
+import java.net.URL;
+import java.net.UnknownHostException;
+import java.util.Collection;
+import java.util.Iterator;
+
 import javax.annotation.PostConstruct;
+import javax.servlet.ServletContext;
 
 import org.apache.curator.x.discovery.ServiceDiscovery;
 import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
 import org.apache.curator.x.discovery.ServiceInstance;
+import org.apache.curator.x.discovery.ServiceInstanceBuilder;
 import org.apache.curator.x.discovery.details.InstanceSerializer;
 import org.apache.curator.x.discovery.details.JsonInstanceSerializer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.context.ServletContextAware;
 
 import com.google.common.base.Throwables;
 
@@ -27,6 +39,9 @@ import eu.europeana.cloud.service.coordination.ZookeeperService;
  */
 public final class ZookeeperServiceAdvertiser implements
 		EcloudServiceAdvertiser {
+
+	@Autowired
+	ServletContext servletContext;
 
 	/** Logging */
 	private static final Logger LOGGER = LoggerFactory
@@ -55,6 +70,7 @@ public final class ZookeeperServiceAdvertiser implements
 	private final ServiceProperties serviceProperties;
 
 	ZookeeperServiceAdvertiser(final ZookeeperService zookeeper,
+			final String discoveryPath,
 			final ServiceProperties serviceProperties) {
 
 		LOGGER.info("ZookeeperServiceAdvertiser starting...");
@@ -62,7 +78,7 @@ public final class ZookeeperServiceAdvertiser implements
 		this.serviceProperties = serviceProperties;
 
 		discovery = ServiceDiscoveryBuilder.builder(ServiceProperties.class)
-				.basePath(zookeeper.getZookeeperPath())
+				.basePath(zookeeper.getZookeeperPath() + discoveryPath)
 				.client(zookeeper.getClient()).serializer(serializer).build();
 
 		try {
@@ -86,15 +102,19 @@ public final class ZookeeperServiceAdvertiser implements
 	@Override
 	public void startAdvertising(final ServiceProperties serviceProperties) {
 
-		LOGGER.info("ZookeeperServiceAdvertiser startAdvertising...");
+		LOGGER.info("ZookeeperServiceAdvertiser starting advertising process '{}' ...",
+				serviceProperties);
+
 		try {
 			discovery.registerService(convert(serviceProperties));
+			this.currentlyAdvertisedServiceProperties = serviceProperties;
+			LOGGER.info("ZookeeperServiceAdvertiser has advertised the service successfully.");
+			
 		} catch (final Exception e) {
 			this.currentlyAdvertisedServiceProperties = null;
 			LOGGER.error(e.getMessage());
 			throw Throwables.propagate(e);
 		}
-		LOGGER.info("ZookeeperServiceAdvertiser has advertised the service successfully.");
 	}
 
 	@Override
@@ -112,17 +132,83 @@ public final class ZookeeperServiceAdvertiser implements
 			final ServiceProperties p) throws Exception {
 		return ServiceInstance.<ServiceProperties> builder()
 				.name(p.getServiceName()).payload(p)
-				.address(p.getListenAddress()).id(p.getServiceId()).build();
+				.address(p.getListenAddress()).build();
+	}
+
+	/**
+	 * Used if no IP address is specified by the user.
+	 * 
+	 * @return tries to autodetect server's ip address using ServiceInstanceBuilder.getAllLocalIPs()
+	 */
+	private String getAutoListenAddress() throws UnknownHostException, IOException {
+
+		Collection<InetAddress> localIps = ServiceInstanceBuilder.getAllLocalIPs();
+		Iterator<InetAddress> localIpsIter = localIps.iterator();
+		LOGGER.info(
+				"ZookeeperServiceAdvertiser: autodetecting ip address.. found {} localIpAddresses",
+				localIps.size());
+
+		while (localIpsIter.hasNext()) {
+			InetAddress localIp = localIpsIter.next();
+			if (localIp.isReachable(5000)) {
+				String address = localIp.getLocalHost().getHostAddress();
+				LOGGER.info("ZookeeperServiceAdvertiser: autodetected {} as listen address",
+						address);
+				return address;
+			}
+		}
+		return null;
+	}
+
+	/**
+	 * @return Servlet's context path. 
+	 * 
+	 * (Appended to the ip address, to construct the listen address for this service)
+	 */
+	private String getServletContextPath() {
+
+		String servletPath = servletContext.getContextPath();
+		LOGGER.info("ZookeeperServiceAdvertiser detecting ContextPath: {}", servletPath);
+		return servletPath;
+	}
+	
+	/**
+	 * TODO
+	 * 
+	 * Used if no port is specified by the user.
+	 * 
+	 * @return Currently hardcoded port number.
+	 * 
+	 * How this port is configured will depend
+	 * on how the services are going to be deployed in the future.
+	 */
+	private String getPort() {
+		return "8080";
 	}
 
 	@PostConstruct
-	public void postConstruct()  {
+	public void postConstruct() {
 
-		LOGGER.info("ZookeeperService starting advertising process '{}' ...",
-				serviceProperties);
+		try {
+			if (this.serviceProperties.getListenAddress().isEmpty()) {
+				String autodectedIpaddress = getAutoListenAddress();
+				if (autodectedIpaddress != null) {
+					this.serviceProperties.setListenAddress("http://"
+							+ getAutoListenAddress() + ":" + getPort()
+							+ getServletContextPath());
+				}
+			}
+			else {
+				this.serviceProperties.setListenAddress(serviceProperties.getListenAddress()
+						+ getServletContextPath());
+			}
+		}
+		catch (Exception e) {
+			LOGGER.warn("ZookeeperServiceAdvertiser: Error while setting service address.. {}",
+					e.getMessage());
+		}
 
 		this.startAdvertising(serviceProperties);
-
-		LOGGER.info("ZookeeperService advertising process successfull.");
+		LOGGER.info("ZookeeperServiceAdvertiser advertising process successfull.");
 	}
 }
