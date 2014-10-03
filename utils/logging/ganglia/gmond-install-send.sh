e80552416da411f5746046c37a57b460c7ba8dcc
EwaKowalczuk <freyja@man.poznan.pl>
2014-10-03 20:07:24 +0200

    Installing gmond scripts - finished script.

--- utils/logging/ganglia/gmond-install-send.sh
+++ utils/logging/ganglia/gmond-install-send.sh
@@ -36,6 +36,7 @@ aggregators=($@)
 #but we don't want to change them from installation to installation
 sendIterval=60
 gridName=ecloud
+owner=ecloud
 
 
 ###prepare files for sending - fill parameters in installation files and target host script
@@ -47,18 +48,25 @@ cp -r $sourceDir/* $tempDir/install
 ##configure gmond.conf
 sed -i "s/user\ =\ nobody/user\ =\ $user/g" $tempDir/install/etc/gmond.conf
 sed -i "s/send_metadata_interval\ =\ 0/send_metadata_interval\ =\ 60/g" $tempDir/install/etc/gmond.conf
-sed -i "s/# override_hostname\ =\ \"mywebserver.domain.com\"/override_hostname\ =\ $hostnameShort/g" $tempDir/install/etc/gmond.conf
+sed -i "s/# override_hostname\ =\ \"mywebserver.domain.com\"/override_hostname\ =\ \"$hostnameShort\"/g" $tempDir/install/etc/gmond.conf
+sed -i "s/name\ = \"unspecified\"/name\ =\ \"$clusterName cluster\"/g" $tempDir/install/etc/gmond.conf
+sed -i "s/owner\ = \"unspecified\"/owner\ =\ \"$owner\"/g" $tempDir/install/etc/gmond.conf
 
 #remove udp_send_channel, mark where it was
 perl -0777 -pi -e "s/\nudp_send_channel\ {\n([^}]*)}/\nXXX/g" $tempDir/install/etc/gmond.conf
-#for all agregators, set one udp_send_channel:
+#for each agregators set one udp_send_channel:
 #first, prepare text
-###udp_send_channel {
-###  host = $a
-###  port = 8649
-###  ttl = 1
-###}
+channelText=""
+for ((i=0; i < ${#aggregators[@]}; i++))
+do
+	channelText="$channelText\nudp_send_channel {"
+	channelText="$channelText\n  host = ${aggregators[$i]}"
+	channelText="$channelText\n  port = 8649"
+	channelText="$channelText\n  ttl = 1"
+	channelText="$channelText\n}"
+done
 #then replace XXX with it
+perl -0777 -pi -e "s/\nXXX/$channelText/g" $tempDir/install/etc/gmond.conf
 
 #remove some setting from udp_recv
 perl -0777 -pi -e "s/\n\ \ bind\ =\ 239.2.11.71//g" $tempDir/install/etc/gmond.conf
@@ -68,3 +76,14 @@ then
 	perl -0777 -pi -e "s/\nudp_recv_channel\ {\n([^}]*)}/\n/g" $tempDir/install/etc/gmond.conf
 fi
 
+#add script for running gmond to files for sending
+mkdir -p $tempDir/tools
+echo 'INSTALL_ROOT=$HOME/install' >> $tempDir/tools/gmond-run.sh
+echo '' >> $tempDir/tools/gmond-run.sh
+echo 'gmond -c $INSTALL_ROOT/etc/gmond.conf' >> $tempDir/tools/gmond-run.sh
+chmod u+x $tempDir/tools/gmond-run.sh
+
+#copy everything to the remote server
+scp -r $tempDir/* $sshAddress:
+#run gmond deamon
+ssh $sshAddress tools/gmond-run.sh
