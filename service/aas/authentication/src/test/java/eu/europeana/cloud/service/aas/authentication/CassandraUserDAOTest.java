1818e4c021c6a891587f5e16963a1d990aad0b00
akrystian <kadamski@man.poznan.pl>
2014-11-27 11:07:54 +0100

    Change in aas CassandraConnector

--- service/aas/authentication/src/test/java/eu/europeana/cloud/service/aas/authentication/CassandraUserDAOTest.java
+++ service/aas/authentication/src/test/java/eu/europeana/cloud/service/aas/authentication/CassandraUserDAOTest.java
@@ -12,16 +12,16 @@ import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
-import eu.europeana.cloud.service.aas.authentication.repository.CassandraConnectionProvider;
+import eu.europeana.cloud.cassandra.CassandraConnectionProvider;
 import eu.europeana.cloud.service.aas.authentication.repository.CassandraUserDAO;
 
 @RunWith(SpringJUnit4ClassRunner.class)
-@ContextConfiguration(locations = {"classpath:/default-context.xml"})
+@ContextConfiguration(locations = { "classpath:/default-context.xml" })
 public class CassandraUserDAOTest extends CassandraTestBase {
 
     @Autowired
     private CassandraConnectionProvider provider;
-    
+
     @Autowired
     private CassandraUserDAO dao;
 
@@ -30,23 +30,25 @@ public class CassandraUserDAOTest extends CassandraTestBase {
      */
     @Before
     public void prepare() {
-        @SuppressWarnings("resource")
-        ApplicationContext context = new ClassPathXmlApplicationContext("default-context.xml");
-        provider = (CassandraConnectionProvider) context.getBean("provider");
-        dao = (CassandraUserDAO) context.getBean("dao");
+	@SuppressWarnings("resource")
+	ApplicationContext context = new ClassPathXmlApplicationContext(
+		"default-context.xml");
+	provider = (CassandraConnectionProvider) context.getBean("provider");
+	dao = (CassandraUserDAO) context.getBean("dao");
     }
 
     @Test
     public void testUserWithRoles() throws Exception {
-    	
-    	SpringUser robinVanPersie = dao.getUser("Robin_Van_Persie");
-    	assertTrue(!isAdmin(robinVanPersie));
-    	
-    	SpringUser admin = dao.getUser("admin");
-    	assertTrue(isAdmin(admin));
+
+	SpringUser robinVanPersie = dao.getUser("Robin_Van_Persie");
+	assertTrue(!isAdmin(robinVanPersie));
+
+	SpringUser admin = dao.getUser("admin");
+	assertTrue(isAdmin(admin));
     }
-    
+
     private boolean isAdmin(final SpringUser u) {
-    	return u.getAuthorities().contains(new SimpleGrantedAuthority("ROLE_ADMIN"));
+	return u.getAuthorities().contains(
+		new SimpleGrantedAuthority("ROLE_ADMIN"));
     }
 }
