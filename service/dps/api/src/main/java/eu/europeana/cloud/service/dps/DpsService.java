6c9bd57f94157c3a32f3d241baa42cc201535c68
koufakis <emmanouil.koufakis@theeuropeanlibrary.org>
2014-12-02 18:42:34 +0100

    ECL-353: added rest-call getTask() that returns a DpsTask (dummy implementation, should be changed to kafka impl)

--- service/dps/api/src/main/java/eu/europeana/cloud/service/dps/DpsService.java
+++ service/dps/api/src/main/java/eu/europeana/cloud/service/dps/DpsService.java
@@ -2,9 +2,20 @@ package eu.europeana.cloud.service.dps;
 
 
 /**
- * Service to submit tasks 
+ * Service to fetch / submit tasks
  */
 public interface DpsService {
 
+	/**
+	 * Submits a task for execution.
+	 * 
+	 * Depending on the task-type and the task-owner,
+	 * the {@link DpsTask} will be submitted to a different Storm topology
+	 */
     void submitTask(DpsTask task);
+    
+    /**
+     * @return Pops a task from the list
+     */
+    DpsTask fetchAndRemove();
 }
--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/DpsResource.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/DpsResource.java
@@ -1,8 +1,14 @@
 package eu.europeana.cloud.service.dps.rest;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.POST;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
+import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 
@@ -10,17 +16,21 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
+import com.google.common.collect.ImmutableList;
+
+import eu.europeana.cloud.service.dps.DpsKeys;
 import eu.europeana.cloud.service.dps.DpsTask;
 
 /**
- * Resource to manage data sets.
+ * Resource to fetch / submit Tasks to the DPS service
+ * 
  */
 @Path("/tasks")
 @Component
 @Scope("request")
 public class DpsResource {
 	
-    @Autowired
+	//    @Autowired
     // TODO: not currently used. 
     // This is only needed if DPS tasks are assigned permissions
     //private MutableAclService mutableAclService;
@@ -32,6 +42,43 @@ public class DpsResource {
     @Consumes({MediaType.APPLICATION_JSON})
     public Response submitTask(DpsTask task)  {
     	
-    	return Response.ok().build();
+    	if (task != null) {
+        	return Response.ok().build();
+    	}
+    	return Response.notModified().build();
     }
+    
+    /**
+     * Submits a Task
+     */
+    @GET
+    @Path("/{type}")
+    @Produces({MediaType.APPLICATION_XML, MediaType.APPLICATION_JSON})
+    public DpsTask getTask(@PathParam("type") String providerId)  {
+    	
+    	DpsTask task = generateDpsTask();
+    	if (task != null) {
+        	return task;
+    	}
+    	return null;
+    }
+    
+    /**
+	 * @return Dummy Implementation, always returns the same hard-coded Task
+	 */
+	private static DpsTask generateDpsTask() {
+
+		DpsTask task = new DpsTask();
+		
+		final String fileUrl = "http://ecloud.eanadev.org:8080/ecloud-service-mcs-rest-0.3-SNAPSHOT/records/"
+				+ "L9WSPSMVQ85/representations/edm/versions/b17c4f60-70d0-11e4-8fe1-00163eefc9c8/files/ef9322a1-5416-4109-a727-2bdfecbf352d";
+		
+		final String xsltUrl = "http://myxslt.url.com";
+		
+		task.addDataEntry(DpsTask.FILE_URLS, ImmutableList.of(fileUrl));
+		task.addParameter(DpsKeys.XSLT_URL, xsltUrl);
+		task.addParameter(DpsKeys.OUTPUT_URL, fileUrl);
+		
+		return task;
+	}
 }
