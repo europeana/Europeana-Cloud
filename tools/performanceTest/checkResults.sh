e6dd34405e71c532dd1a662447f5253ae642c12a
Aleksandra Nowak <anowak@man.poznan.pl>
2015-01-08 10:57:23 +0100

    ECL-336 script should succeed in case of no errors

--- tools/performanceTest/checkResults.sh
+++ tools/performanceTest/checkResults.sh
@@ -10,10 +10,9 @@ sh  ~/performanceTest/performanceTestScript.sh --host localhost --port 9090 --HT
 timestamp=( $(cat ~/performanceTest/results.txt |  sed -nr 's/results have timestamp (.+)/\1/p'))
 cmd=( $(cat ~/performanceTest/results.txt | grep Err | sed -e 's/.\+Err:[^0-9]\+\([0-9]\+\).\+/\1/g'))
 
-
 errors=0
 for ((i=0; i < ${#cmd[@]}; i++));
-        do if [ ${cmd[$i]} -eq 0 ]; then
+        do if [ ${cmd[$i]} -nq 0 ]; then
                 ((errors+=1))
         fi;
 done;
@@ -21,11 +20,10 @@ done;
 rm ~/performanceTest/results.txt
 
 echo -e  "\n#############################\n"
-echo Test cases with errors output:
-tail -n +1 -- ~/pTest${timestamp}/err*
-
-echo -e  "\n#############################\n"
 if [ $errors -gt 0 ] ; then
         echo "tests failed; there were errors"
+        echo -e  "\n#############################\n"
+        echo Test cases with errors output:
+        tail -n +1 -- ~/pTest${timestamp}/err*
         exit 1;
 fi
