54c6b526593e6fd307aaeb75a2b8764bdfc84066
koufakis <emmanouil.koufakis@theeuropeanlibrary.org>
2015-03-04 11:25:54 +0100

    ECL-406: added Task_ID storm-pararameters to bolts

--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/StormTupleKeys.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/StormTupleKeys.java
@@ -9,11 +9,17 @@ public final class StormTupleKeys {
 	private StormTupleKeys() {
 	}
 
-	public static final String PARAMETERS_TUPLE_KEY = "PARAMETERS";
+	// e.g: "54698435894"
+	public static final String TASK_ID_TUPLE_KEY = "TASK_ID";
+	
+	//e.g: "xslt-transformation-task"
+	public static final String TASK_NAME_TUPLE_KEY = "TASK_NAME";
 	
 	public static final String DATASETS_TUPLE_KEY = "DATASETS";
 	
 	public static final String INPUT_FILES_TUPLE_KEY = "INPUT_FILES";
-	
+
 	public static final String FILE_CONTENT_TUPLE_KEY = "FILE_CONTENT";
+	
+	public static final String PARAMETERS_TUPLE_KEY = "PARAMETERS";
 }
