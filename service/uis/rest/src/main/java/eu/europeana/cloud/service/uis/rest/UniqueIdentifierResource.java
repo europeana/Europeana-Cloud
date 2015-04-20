2752d212caf01c043499dc32b7389558b665f6cd
koufakis <emmanouil.koufakis@theeuropeanlibrary.org>
2015-04-20 14:31:03 +0200

    ECL-441: revision of CloudId permissions + tests

--- service/uis/rest/src/main/java/eu/europeana/cloud/service/uis/rest/UniqueIdentifierResource.java
+++ service/uis/rest/src/main/java/eu/europeana/cloud/service/uis/rest/UniqueIdentifierResource.java
@@ -48,139 +48,134 @@ import eu.europeana.cloud.service.uis.exception.RecordIdDoesNotExistException;
 @Scope("request")
 public class UniqueIdentifierResource {
 
-    @Autowired
-    private UniqueIdentifierService uniqueIdentifierService;
+	@Autowired
+	private UniqueIdentifierService uniqueIdentifierService;
 
-    private static final String CLOUDID = "cloudId";
+	private static final String CLOUDID = "cloudId";
 
-    @Autowired
-    private MutableAclService mutableAclService;
+	@Autowired
+	private MutableAclService mutableAclService;
 
-    private final String CLOUD_ID_CLASS_NAME = CloudId.class.getName();
+	private final String CLOUD_ID_CLASS_NAME = CloudId.class.getName();
 
-    /**
-     * Invoke the generation of a cloud identifier using the provider identifier
-     * and a record identifier
-     * 
-     * @param providerId
-     * @param localId
-     * @return The newly created CloudId
-     * @throws DatabaseConnectionException
-     * @throws RecordExistsException
-     * @throws ProviderDoesNotExistException
-     * @throws RecordDatasetEmptyException
-     * @throws CloudIdDoesNotExistException
-     * @throws CloudIdAlreadyExistException
-     */
-    @POST
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @ReturnType("eu.europeana.cloud.common.model.CloudId")
-    @PreAuthorize("isAuthenticated()")
-    public Response createCloudId(
-	    @QueryParam(UISParamConstants.Q_PROVIDER) String providerId,
-	    @QueryParam(UISParamConstants.Q_RECORD_ID) String localId)
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, CloudIdAlreadyExistException {
+	/**
+	 * Invoke the generation of a cloud identifier using the provider identifier
+	 * and a record identifier
+	 * 
+	 * @param providerId
+	 * @param localId
+	 * @return The newly created CloudId
+	 * @throws DatabaseConnectionException
+	 * @throws RecordExistsException
+	 * @throws ProviderDoesNotExistException
+	 * @throws RecordDatasetEmptyException
+	 * @throws CloudIdDoesNotExistException
+	 * @throws CloudIdAlreadyExistException
+	 */
+	@POST
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ReturnType("eu.europeana.cloud.common.model.CloudId")
+	@PreAuthorize("isAuthenticated()")
+	public Response createCloudId(
+			@QueryParam(UISParamConstants.Q_PROVIDER) String providerId,
+			@QueryParam(UISParamConstants.Q_RECORD_ID) String localId)
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, CloudIdAlreadyExistException {
 
-	final CloudId cId = (localId != null) ? (uniqueIdentifierService
-		.createCloudId(providerId, localId)) : (uniqueIdentifierService
-		.createCloudId(providerId));
+		final CloudId cId = (localId != null) ? (uniqueIdentifierService
+				.createCloudId(providerId, localId)) : (uniqueIdentifierService
+				.createCloudId(providerId));
 
-	final Response response = Response.ok().entity(cId).build();
+		final Response response = Response.ok().entity(cId).build();
 
-	// CloudId created => let's assign permissions to the owner
-	String creatorName = SpringUserUtils.getUsername();
-	if (creatorName != null) {
-	    ObjectIdentity cloudIdIdentity = new ObjectIdentityImpl(
-		    CLOUD_ID_CLASS_NAME, cId.getId());
+		// CloudId created => let's assign permissions to the owner
+		String creatorName = SpringUserUtils.getUsername();
 
-	    MutableAcl cloudIdAcl = mutableAclService
-		    .createAcl(cloudIdIdentity);
+		if (creatorName != null) {
+			
+			ObjectIdentity cloudIdIdentity = new ObjectIdentityImpl(CLOUD_ID_CLASS_NAME, cId.getId());
 
-	    cloudIdAcl.insertAce(0, BasePermission.READ, new PrincipalSid(
-		    creatorName), true);
-	    cloudIdAcl.insertAce(1, BasePermission.WRITE, new PrincipalSid(
-		    creatorName), true);
-	    cloudIdAcl.insertAce(2, BasePermission.DELETE, new PrincipalSid(
-		    creatorName), true);
-	    cloudIdAcl.insertAce(3, BasePermission.ADMINISTRATION,
-		    new PrincipalSid(creatorName), true);
+			MutableAcl cloudIdAcl = mutableAclService.createAcl(cloudIdIdentity);
 
-	    mutableAclService.updateAcl(cloudIdAcl);
+			cloudIdAcl.insertAce(0, BasePermission.READ, new PrincipalSid(creatorName), true);
+			cloudIdAcl.insertAce(1, BasePermission.WRITE, new PrincipalSid(creatorName), true);
+			cloudIdAcl.insertAce(2, BasePermission.DELETE, new PrincipalSid(creatorName), true);
+			cloudIdAcl.insertAce(3, BasePermission.ADMINISTRATION, new PrincipalSid(creatorName), true);
+
+			mutableAclService.updateAcl(cloudIdAcl);
+		}
+
+		return response;
 	}
 
-	return response;
-    }
+	/**
+	 * Invoke the generation of a cloud identifier using the provider identifier
+	 * 
+	 * @param providerId
+	 * @param recordId
+	 * @return The newly created CloudId
+	 * @throws DatabaseConnectionException
+	 * @throws RecordDoesNotExistException
+	 * @throws ProviderDoesNotExistException
+	 * @throws RecordDatasetEmptyException
+	 */
+	@GET
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ReturnType("eu.europeana.cloud.common.model.CloudId")
+	public Response getCloudId(
+			@QueryParam(UISParamConstants.Q_PROVIDER) String providerId,
+			@QueryParam(UISParamConstants.Q_RECORD_ID) String recordId)
+			throws DatabaseConnectionException, RecordDoesNotExistException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException {
+		return Response.ok(
+				uniqueIdentifierService.getCloudId(providerId, recordId))
+				.build();
+	}
 
-    /**
-     * Invoke the generation of a cloud identifier using the provider identifier
-     * 
-     * @param providerId
-     * @param recordId
-     * @return The newly created CloudId
-     * @throws DatabaseConnectionException
-     * @throws RecordDoesNotExistException
-     * @throws ProviderDoesNotExistException
-     * @throws RecordDatasetEmptyException
-     */
-    @GET
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @ReturnType("eu.europeana.cloud.common.model.CloudId")
-    public Response getCloudId(
-	    @QueryParam(UISParamConstants.Q_PROVIDER) String providerId,
-	    @QueryParam(UISParamConstants.Q_RECORD_ID) String recordId)
-	    throws DatabaseConnectionException, RecordDoesNotExistException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException {
-	return Response.ok(
-		uniqueIdentifierService.getCloudId(providerId, recordId))
-		.build();
-    }
+	/**
+	 * Retrieve a list of record Identifiers associated with a cloud identifier
+	 * 
+	 * @return A list of record identifiers
+	 * @throws DatabaseConnectionException
+	 * @throws CloudIdDoesNotExistException
+	 * @throws ProviderDoesNotExistException
+	 * @throws RecordDatasetEmptyException
+	 */
+	@GET
+	@Path("{" + CLOUDID + "}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+	@ReturnType("eu.europeana.cloud.common.response.ResultSlice")
+	public Response getLocalIds(@PathParam(CLOUDID) String cloudId)
+			throws DatabaseConnectionException, CloudIdDoesNotExistException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException {
+		ResultSlice<CloudId> pList = new ResultSlice<>();
+		pList.setResults(uniqueIdentifierService.getLocalIdsByCloudId(cloudId));
+		return Response.ok(pList).build();
+	}
 
-    /**
-     * Retrieve a list of record Identifiers associated with a cloud identifier
-     * 
-     * @return A list of record identifiers
-     * @throws DatabaseConnectionException
-     * @throws CloudIdDoesNotExistException
-     * @throws ProviderDoesNotExistException
-     * @throws RecordDatasetEmptyException
-     */
-    @GET
-    @Path("{" + CLOUDID + "}")
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @ReturnType("eu.europeana.cloud.common.response.ResultSlice")
-    public Response getLocalIds(@PathParam(CLOUDID) String cloudId)
-            throws DatabaseConnectionException, CloudIdDoesNotExistException, ProviderDoesNotExistException,
-            RecordDatasetEmptyException {
-        ResultSlice<CloudId> pList = new ResultSlice<>();
-        pList.setResults(uniqueIdentifierService.getLocalIdsByCloudId(cloudId));
-        return Response.ok(pList).build();
-    }
+	/**
+	 * Remove a cloud identifier and all the associations to its record
+	 * identifiers
+	 * 
+	 * @return Confirmation that the selected cloud identifier is removed
+	 * @throws DatabaseConnectionException
+	 * @throws CloudIdDoesNotExistException
+	 * @throws ProviderDoesNotExistException
+	 * @throws RecordIdDoesNotExistException
+	 */
+	@DELETE
+	@Path("{" + CLOUDID + "}")
+	@Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+	public Response deleteCloudId(@PathParam(CLOUDID) String cloudId)
+			throws DatabaseConnectionException, CloudIdDoesNotExistException,
+			ProviderDoesNotExistException, RecordIdDoesNotExistException {
 
-    /**
-     * Remove a cloud identifier and all the associations to its record
-     * identifiers
-     * 
-     * @return Confirmation that the selected cloud identifier is removed
-     * @throws DatabaseConnectionException
-     * @throws CloudIdDoesNotExistException
-     * @throws ProviderDoesNotExistException
-     * @throws RecordIdDoesNotExistException
-     */
-    @DELETE
-    @Path("{" + CLOUDID + "}")
-    @Produces({ MediaType.APPLICATION_JSON, MediaType.APPLICATION_XML })
-    @PreAuthorize("hasPermission(#cloudId, 'eu.europeana.cloud.common.model.CloudId', delete)")
-    public Response deleteCloudId(@PathParam(CLOUDID) String cloudId)
-	    throws DatabaseConnectionException, CloudIdDoesNotExistException,
-	    ProviderDoesNotExistException, RecordIdDoesNotExistException {
-
-	uniqueIdentifierService.deleteCloudId(cloudId);
-	// let's delete the permissions as well
-	ObjectIdentity dataSetIdentity = new ObjectIdentityImpl(
-		CLOUD_ID_CLASS_NAME, cloudId);
-	mutableAclService.deleteAcl(dataSetIdentity, false);
-	return Response.ok("CloudId marked as deleted").build();
-    }
+		uniqueIdentifierService.deleteCloudId(cloudId);
+		// let's delete the permissions as well
+		ObjectIdentity cloudIdentity = new ObjectIdentityImpl(CLOUD_ID_CLASS_NAME, cloudId);
+		mutableAclService.deleteAcl(cloudIdentity, false);
+		return Response.ok("CloudId marked as deleted").build();
+	}
 }
--- service/uis/rest/src/test/java/eu/europeana/cloud/service/uis/security/UisAATest.java
+++ service/uis/rest/src/test/java/eu/europeana/cloud/service/uis/security/UisAATest.java
@@ -35,177 +35,184 @@ import eu.europeana.cloud.service.uis.rest.UniqueIdentifierResource;
 @RunWith(SpringJUnit4ClassRunner.class)
 public class UisAATest extends AbstractSecurityTest {
 
-    @Autowired
-    private UniqueIdentifierService uniqueIdentifierService;
+	@Autowired
+	private UniqueIdentifierService uniqueIdentifierService;
 
-    @Autowired
-    private UniqueIdentifierResource uisResource;
+	@Autowired
+	private UniqueIdentifierResource uisResource;
 
-    private final static String PROVIDER_ID = "Russell_Stringer_Bell";
-    private final static String LOCAL_ID = "LOCAL_ID";
-    private final static String RECORD_ID = "RECORD_ID";
-    private final static String CLOUD_ID = "CLOUD_ID";
+	private final static String PROVIDER_ID = "Russell_Stringer_Bell";
+	private final static String LOCAL_ID = "LOCAL_ID";
+	private final static String RECORD_ID = "RECORD_ID";
+	private final static String CLOUD_ID = "CLOUD_ID";
 
-    /**
-     * Pre-defined users
-     */
-    private final static String RANDOM_PERSON = "Cristiano";
-    private final static String RANDOM_PASSWORD = "Ronaldo";
+	/**
+	 * Pre-defined users
+	 */
+	private final static String RANDOM_PERSON = "Cristiano";
+	private final static String RANDOM_PASSWORD = "Ronaldo";
 
-    private final static String VAN_PERSIE = "Robin_Van_Persie";
-    private final static String VAN_PERSIE_PASSWORD = "Feyenoord";
+	private final static String VAN_PERSIE = "Robin_Van_Persie";
+	private final static String VAN_PERSIE_PASSWORD = "Feyenoord";
 
-    private final static String RONALDO = "Cristiano";
-    private final static String RONALD_PASSWORD = "Ronaldo";
+	private final static String RONALDO = "Cristiano";
+	private final static String RONALD_PASSWORD = "Ronaldo";
 
-    private final static String ADMIN = "admin";
-    private final static String ADMIN_PASSWORD = "admin";
+	private final static String ADMIN = "admin";
+	private final static String ADMIN_PASSWORD = "admin";
 
-    /**
-     * Prepare the unit tests
-     */
-    @Before
-    public void prepare() {
-    }
+	/**
+	 * Prepare the unit tests
+	 */
+	@Before
+	public void prepare() {
+	}
 
-    /**
-     * Makes sure these methods can run even if noone is logged in. No special
-     * permissions are required.
-     */
-    @Test
-    public void testMethodsThatDontNeedAnyAuthentication()
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, RecordDoesNotExistException {
+	/**
+	 * Makes sure these methods can run even if noone is logged in. No special
+	 * permissions are required.
+	 */
+	@Test
+	public void testMethodsThatDontNeedAnyAuthentication()
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, RecordDoesNotExistException {
 
-	uisResource.getCloudId(PROVIDER_ID, RECORD_ID);
-	uisResource.getLocalIds(CLOUD_ID);
-    }
+		uisResource.getCloudId(PROVIDER_ID, RECORD_ID);
+		uisResource.getLocalIds(CLOUD_ID);
+	}
 
-    /**
-     * Makes sure any random person can just call these methods. No special
-     * permissions are required.
-     */
-    @Test
-    public void shouldBeAbleToCallMethodsThatDontNeedAnyAuthenticationWithSomeRandomPersonLoggedIn()
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, RecordDoesNotExistException {
+	/**
+	 * Makes sure any random person can just call these methods. No special
+	 * permissions are required.
+	 */
+	@Test
+	public void shouldBeAbleToCallMethodsThatDontNeedAnyAuthenticationWithSomeRandomPersonLoggedIn()
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, RecordDoesNotExistException {
 
-	login(RANDOM_PERSON, RANDOM_PASSWORD);
-	uisResource.getCloudId(PROVIDER_ID, RECORD_ID);
-	uisResource.getLocalIds(CLOUD_ID);
-    }
+		login(RANDOM_PERSON, RANDOM_PASSWORD);
+		uisResource.getCloudId(PROVIDER_ID, RECORD_ID);
+		uisResource.getLocalIds(CLOUD_ID);
+	}
 
-    @Test
-    public void shouldBeAbleToCallMethodsThatNeedBasicAuthenticationWithSomeRandomPersonLoggedIn()
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, CloudIdAlreadyExistException {
+	@Test
+	public void shouldBeAbleToCallMethodsThatNeedBasicAuthenticationWithSomeRandomPersonLoggedIn()
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, CloudIdAlreadyExistException {
 
-	CloudId cloudId = new CloudId();
-	cloudId.setId(CLOUD_ID);
+		CloudId cloudId = new CloudId();
+		cloudId.setId(CLOUD_ID);
 
-	Mockito.when(
-		uniqueIdentifierService.createCloudId(Mockito.anyString(),
-			Mockito.anyString())).thenReturn(cloudId);
+		Mockito.when(
+				uniqueIdentifierService.createCloudId(Mockito.anyString(),
+						Mockito.anyString())).thenReturn(cloudId);
 
-	login(RANDOM_PERSON, RANDOM_PASSWORD);
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-    }
+		login(RANDOM_PERSON, RANDOM_PASSWORD);
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+	}
 
-    @Test(expected = AuthenticationCredentialsNotFoundException.class)
-    public void shouldThrowExceptionWhenUnknowUserTriesToCreateCloudID()
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, CloudIdAlreadyExistException {
+	@Test(expected = AuthenticationCredentialsNotFoundException.class)
+	public void shouldThrowExceptionWhenUnknowUserTriesToCreateCloudID()
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, CloudIdAlreadyExistException {
 
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-    }
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+	}
 
-    /**
-     * Makes sure that a random person cannot just delete a cloud id. Simple
-     * authentication test to make sure spring security annotations are in
-     * place.
-     */
-    @Test(expected = AccessDeniedException.class)
-    public void shouldThrowAccessDeniedExceptionWhenRandomPersonTriesToDeleteCloudId()
-	    throws DatabaseConnectionException, CloudIdDoesNotExistException,
-	    ProviderDoesNotExistException, RecordIdDoesNotExistException {
+	/**
+	 * Makes sure that a random person cannot just delete a cloud id. Simple
+	 * authentication test to make sure spring security annotations are in
+	 * place.
+	 */
+	@Test(expected = AccessDeniedException.class)
+	public void shouldThrowAccessDeniedExceptionWhenRandomPersonTriesToDeleteCloudId()
+			throws DatabaseConnectionException, CloudIdDoesNotExistException,
+			ProviderDoesNotExistException, RecordIdDoesNotExistException {
 
-	login(RANDOM_PERSON, RANDOM_PASSWORD);
-	uisResource.deleteCloudId(CLOUD_ID);
-    }
+		login(RANDOM_PERSON, RANDOM_PASSWORD);
+		uisResource.deleteCloudId(CLOUD_ID);
+	}
+	
 
-    /**
-     * Makes sure the person who created a cloudId has delete permissions as
-     * well.
-     */
-    @Test
-    public void shouldBeAbleToDeleteCloudIdIfHeIsTheOwner()
-	    throws ProviderDoesNotExistException,
-	    ProviderAlreadyExistsException, URISyntaxException,
-	    DatabaseConnectionException, RecordExistsException,
-	    CloudIdAlreadyExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, RecordIdDoesNotExistException {
+	@Test(expected = AccessDeniedException.class)
+	public void shouldThrowAccessDeniedExceptionEvenWhenOwnerTriesToDeleteCloudId() throws DatabaseConnectionException, 
+		RecordExistsException, ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, CloudIdAlreadyExistException, 
+				RecordIdDoesNotExistException {
 
-	CloudId cloudId = new CloudId();
-	cloudId.setId(CLOUD_ID);
+		CloudId cloudId = new CloudId();
+		cloudId.setId(CLOUD_ID);
 
-	Mockito.when(
-		uniqueIdentifierService.createCloudId(Mockito.anyString(),
-			Mockito.anyString())).thenReturn(cloudId);
+		Mockito.when(
+				uniqueIdentifierService.createCloudId(Mockito.anyString(),
+						Mockito.anyString())).thenReturn(cloudId);
 
-	login(VAN_PERSIE, VAN_PERSIE_PASSWORD);
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-	uisResource.deleteCloudId(CLOUD_ID);
-    }
+		login(VAN_PERSIE, VAN_PERSIE_PASSWORD);
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+		uisResource.deleteCloudId(CLOUD_ID);
+	}
 
-    /**
-     * Makes sure the person who a deleted cloudId can be recreated.
-     */
-    @Test
-    public void shouldBeAbleToRecreateDeletedCloudID()
-	    throws ProviderDoesNotExistException,
-	    ProviderAlreadyExistsException, URISyntaxException,
-	    DatabaseConnectionException, RecordExistsException,
-	    CloudIdAlreadyExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, RecordIdDoesNotExistException {
+	@Test
+	public void shouldBeAbleToDeleteCloudIdIfHeIsAdmin()
+			throws ProviderDoesNotExistException,
+			ProviderAlreadyExistsException, URISyntaxException,
+			DatabaseConnectionException, RecordExistsException,
+			CloudIdAlreadyExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, RecordIdDoesNotExistException {
 
-	CloudId cloudId = new CloudId();
-	cloudId.setId(CLOUD_ID);
+		login(ADMIN, ADMIN_PASSWORD);
+		uisResource.deleteCloudId(CLOUD_ID);
+	}
 
-	Mockito.when(
-		uniqueIdentifierService.createCloudId(Mockito.anyString(),
-			Mockito.anyString())).thenReturn(cloudId);
+	/**
+	 * Makes sure the person who deleted cloudId can be recreated.
+	 */
+	@Test
+	public void shouldBeAbleToRecreateDeletedCloudID()
+			throws ProviderDoesNotExistException,
+			ProviderAlreadyExistsException, URISyntaxException,
+			DatabaseConnectionException, RecordExistsException,
+			CloudIdAlreadyExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, RecordIdDoesNotExistException {
 
-	login(VAN_PERSIE, VAN_PERSIE_PASSWORD);
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-	uisResource.deleteCloudId(CLOUD_ID);
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-    }
+		CloudId cloudId = new CloudId();
+		cloudId.setId(CLOUD_ID);
 
-    /**
-     * Makes sure Van Persie cannot delete cloud id's that belong to Christiano
-     * Ronaldo.
-     */
-    @Test(expected = AccessDeniedException.class)
-    public void shouldThrowExceptionWhenVanPersieTriesToDeleteRonaldosCloudIds()
-	    throws DatabaseConnectionException, RecordExistsException,
-	    ProviderDoesNotExistException, RecordDatasetEmptyException,
-	    CloudIdDoesNotExistException, CloudIdAlreadyExistException,
-	    URISyntaxException, RecordIdDoesNotExistException {
+		Mockito.when(
+				uniqueIdentifierService.createCloudId(Mockito.anyString(),
+						Mockito.anyString())).thenReturn(cloudId);
 
-	CloudId cloudId = new CloudId();
-	cloudId.setId(CLOUD_ID);
+		login(ADMIN, ADMIN_PASSWORD);
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+		uisResource.deleteCloudId(CLOUD_ID);
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+	}
 
-	Mockito.when(
-		uniqueIdentifierService.createCloudId(Mockito.anyString(),
-			Mockito.anyString())).thenReturn(cloudId);
+	/**
+	 * Makes sure Van Persie cannot delete cloud id's that belong to Christiano
+	 * Ronaldo.
+	 */
+	@Test(expected = AccessDeniedException.class)
+	public void shouldThrowExceptionWhenVanPersieTriesToDeleteRonaldosCloudIds()
+			throws DatabaseConnectionException, RecordExistsException,
+			ProviderDoesNotExistException, RecordDatasetEmptyException,
+			CloudIdDoesNotExistException, CloudIdAlreadyExistException,
+			URISyntaxException, RecordIdDoesNotExistException {
 
-	login(RONALDO, RONALD_PASSWORD);
-	uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
-	login(VAN_PERSIE, VAN_PERSIE_PASSWORD);
-	uisResource.deleteCloudId(CLOUD_ID);
-    }
+		CloudId cloudId = new CloudId();
+		cloudId.setId(CLOUD_ID);
+
+		Mockito.when(
+				uniqueIdentifierService.createCloudId(Mockito.anyString(),
+						Mockito.anyString())).thenReturn(cloudId);
+
+		login(RONALDO, RONALD_PASSWORD);
+		uisResource.createCloudId(PROVIDER_ID, LOCAL_ID);
+		login(VAN_PERSIE, VAN_PERSIE_PASSWORD);
+		uisResource.deleteCloudId(CLOUD_ID);
+	}
 }
