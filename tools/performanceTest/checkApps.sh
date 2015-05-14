c7af8213fbbcc6d794651f13f703fc6837637a36
Aleksandra Nowak <anowak@man.poznan.pl>
2015-05-14 16:47:45 +0200

    small fix

--- tools/performanceTest/checkApps.sh
+++ tools/performanceTest/checkApps.sh
@@ -10,7 +10,6 @@ solr
 
 success=0
 pass=`sed -nr 's/.*tomcat-admin.*password="(.*)" roles.*/\1/p' ~/tomcat/conf/tomcat-users.xml`
-echo $pass
 cmd=`curl http://tomcat-admin:$pass@localhost:9090/manager/text/list`
 echo $cmd
 for i in "${array[@]}"
