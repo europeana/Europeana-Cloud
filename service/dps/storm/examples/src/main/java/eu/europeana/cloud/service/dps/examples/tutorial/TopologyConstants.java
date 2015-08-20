2ebe930b36449e189f0235cd068a4f43fc7b5eca
Lucas Anastasiou <anastluc@gmail.com>
2015-08-20 10:36:33 +0100

    ECL-578 Adding the tutorial code about the convert topology

---
+++ service/dps/storm/examples/src/main/java/eu/europeana/cloud/service/dps/examples/tutorial/TopologyConstants.java
@@ -0,0 +1,21 @@
+package eu.europeana.cloud.service.dps.examples.tutorial;
+
+/**
+ *
+ * @author lucasanastasiou
+ */
+public class TopologyConstants {
+
+    public static final String ZOOKEEPER_LOCATION = "";
+    public static final String KAFKA_TOPIC = "";
+    public static final String ZK_ROOT = "";
+    public static final String ID = "";
+    public static final String ECLOUD_MCS_ADDRESS = "";
+    public static final String ECLOUD_MCS_USERNAME = "admin";
+    public static final String ECLOUD_MCS_PASSWORD = "admin";
+    public static final String CASSANDRA_HOSTS = "";
+    public static final int CASSANDRA_PORT = 9042;
+    public static final String CASSANDRA_KEYSPACE_NAME = "";    
+    public static final String CASSANDRA_USERNAME = "";
+    public static final String CASSANDRA_PASSWORD = "";
+}
