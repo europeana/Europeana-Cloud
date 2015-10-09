337b0fc87d7e4fb3f6b82df2193652e34e2bc7e5
akrystian <kadamski@man.poznan.pl>
2015-10-09 07:55:20 +0200

    ECL-515 - Method Not Allowed - change in to client side timestamps.

--- cassandra-connector/src/main/java/eu/europeana/cloud/cassandra/CassandraConnectionProvider.java
+++ cassandra-connector/src/main/java/eu/europeana/cloud/cassandra/CassandraConnectionProvider.java
@@ -1,11 +1,15 @@
 package eu.europeana.cloud.cassandra;
 
+import com.datastax.driver.core.AtomicMonotonicTimestampGenerator;
 import com.datastax.driver.core.Cluster;
 import com.datastax.driver.core.ConsistencyLevel;
 import com.datastax.driver.core.Host;
 import com.datastax.driver.core.Metadata;
+import com.datastax.driver.core.ProtocolVersion;
 import com.datastax.driver.core.Session;
+
 import javax.annotation.PreDestroy;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
@@ -25,66 +29,80 @@ public class CassandraConnectionProvider {
     private final ConsistencyLevel consistencyLevel = ConsistencyLevel.QUORUM;
 
     private String hosts;
-        
+
     private String port;
 
     private String keyspaceName;
+
+
     /**
-     * Constructor. Use it when your Cassandra cluster does not support authentication.
-     * 
-     * @param hosts
-     *            cassandra node hosts, comma separated
-     * @param port
-     *            cassandra node cql service port
-     * @param keyspaceName
-     *            name of keyspace
+     * Constructor. Use it when your Cassandra cluster does not support
+     * authentication.
+     *
+     * @param hosts        cassandra node hosts, comma separated
+     * @param port         cassandra node cql service port
+     * @param keyspaceName name of keyspace
      */
     public CassandraConnectionProvider(String hosts, int port, String keyspaceName) {
         this.hosts = hosts;
         this.port = String.valueOf(port);
         this.keyspaceName = keyspaceName;
-        
+
         String[] contactPoints = hosts.split(",");
-        cluster = Cluster.builder().addContactPoints(contactPoints).withPort(port).build();
-        Metadata metadata = cluster.getMetadata();
-        LOGGER.info("Connected to cluster: {}", metadata.getClusterName());
-        for (Host h : metadata.getAllHosts()) {
-            LOGGER.info("Datatacenter: {}; Hosts: {}; Rack: {}", h.getDatacenter(), h.getAddress(), h.getRack());
-        }
+        cluster = getClusterBuilder(port, contactPoints)
+                .build();
+        init();
         session = cluster.connect(keyspaceName);
     }
 
-    
+
     /**
-     * Constructor. Use it when your Cassandra cluster does support authentication.
-     * 
-     * @param hosts
-     *            cassandra node hosts, comma separated
-     * @param port
-     *            cassandra node cql service port
-     * @param keyspaceName
-     *            name of keyspace
-     * @param userName
-     *            user name
-     * @param password
-     *            password
+     * Constructor. Use it when your Cassandra cluster does support
+     * authentication.
+     *
+     * @param hosts        cassandra node hosts, comma separated
+     * @param port         cassandra node cql service port
+     * @param keyspaceName name of keyspace
+     * @param userName     user name
+     * @param password     password
      */
     public CassandraConnectionProvider(String hosts, int port, String keyspaceName, String userName, String password) {
         this.hosts = hosts;
         this.port = String.valueOf(port);
         this.keyspaceName = keyspaceName;
-        
+
         String[] contactPoints = hosts.split(",");
-        cluster = Cluster.builder().addContactPoints(contactPoints).withCredentials(userName, password).withPort(port).build();
+        cluster = getClusterBuilder(port, contactPoints)
+                .withCredentials(userName, password).build();
+        init();
+        session = cluster.connect(keyspaceName);
+    }
+
+    /**
+     * Common initializer.
+     */
+    private void init() {
         Metadata metadata = cluster.getMetadata();
         LOGGER.info("Connected to cluster: {}", metadata.getClusterName());
         for (Host h : metadata.getAllHosts()) {
-            LOGGER.info("Datatacenter: {}; Hosts: {}; Rack: {}", h.getDatacenter(), h.getAddress(), h.getRack());
+            LOGGER.info("Data center: {}; Hosts: {}; Rack: {}", h.getDatacenter(), h.getAddress(), h.getRack());
         }
-        session = cluster.connect(keyspaceName);
     }
-        
-        
+
+    /**
+     * Obtain common cassandra Cluster Builder.
+     *
+     * @param port port of endpoints
+     * @param contactPoints list of cassandra ip addresses
+     * @return {@link com.datastax.driver.core.Cluster.Builder}
+     */
+    private Cluster.Builder getClusterBuilder(int port, String[] contactPoints) {
+        return Cluster.builder().addContactPoints(contactPoints).withPort(port)
+                .withProtocolVersion(ProtocolVersion.V3)
+                .withTimestampGenerator(new AtomicMonotonicTimestampGenerator());
+    }
+
+
     @PreDestroy
     private void closeConnections() {
         LOGGER.info("Cluster is shutting down.");
@@ -92,7 +110,7 @@ public class CassandraConnectionProvider {
     }
 
 
-     /**
+    /**
      * Expose a singleton instance connection to a database on the requested
      * host and keyspace
      *
@@ -105,14 +123,15 @@ public class CassandraConnectionProvider {
 
     /**
      * Returns the default consistency level;
-     * 
+     *
      * @return the consistencyLevel
      */
     public ConsistencyLevel getConsistencyLevel() {
         return consistencyLevel;
     }
-    
-     /**
+
+
+    /**
      * Expose the contact server IP address
      *
      * @return The host name
@@ -120,6 +139,7 @@ public class CassandraConnectionProvider {
     public String getHosts() {
         return hosts;
     }
+
 
     /**
      * Expose the contact server port
@@ -129,6 +149,7 @@ public class CassandraConnectionProvider {
     public String getPort() {
         return port;
     }
+
 
     /**
      * Expose the keyspace
