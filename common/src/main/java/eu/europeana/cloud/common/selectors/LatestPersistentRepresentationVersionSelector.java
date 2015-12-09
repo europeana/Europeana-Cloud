7ac2b33e25c217454fca491db7a55cc0ffa6875c
pWoz <pwozniak@man.poznan.pl>
2015-12-09 15:33:11 +0100

    Representation selection from list of representations.

---
+++ common/src/main/java/eu/europeana/cloud/common/selectors/LatestPersistentRepresentationVersionSelector.java
@@ -0,0 +1,33 @@
+package eu.europeana.cloud.common.selectors;
+
+import com.eaio.uuid.UUID;
+import eu.europeana.cloud.common.model.Representation;
+
+import java.util.List;
+
+/**
+ * Selects latest persistent representation from list of provided representations
+ */
+public class LatestPersistentRepresentationVersionSelector implements RepresentationSelector {
+
+    @Override
+    public Representation select(List<Representation> representations) {
+
+        Representation representationToBeReturned = null;
+        
+        for (Representation representation : representations) {
+            if (representation.isPersistent()) {
+                if (representationToBeReturned != null) {
+                    UUID uuid = new UUID(representation.getVersion());
+                    UUID uuid_1 = new UUID(representationToBeReturned.getVersion());
+                    if (uuid.compareTo(uuid_1) > 0) {
+                        representationToBeReturned = representation;
+                    }
+                } else {
+                    representationToBeReturned = representation;
+                }
+            }
+        }
+        return representationToBeReturned;
+    }
+}
