b1db2f9934b702cb92964f25bc81b0dfbfcb14dd
akrystian <kadamski@man.poznan.pl>
2015-12-10 12:57:54 +0100

    ECL-681 - Storm bolt for deleting permissions to file - refactor

--- service/dps/storm/io/src/test/java/eu/europeana/cloud/service/dps/storm/io/RemovePermissionsToFileBoltTest.java
+++ service/dps/storm/io/src/test/java/eu/europeana/cloud/service/dps/storm/io/RemovePermissionsToFileBoltTest.java
@@ -1,42 +1,34 @@
 package eu.europeana.cloud.service.dps.storm.io;
 
 import backtype.storm.Config;
-import backtype.storm.ILocalCluster;
-import backtype.storm.Testing;
-import backtype.storm.generated.AlreadyAliveException;
-import backtype.storm.generated.InvalidTopologyException;
-import backtype.storm.testing.AckTracker;
-import backtype.storm.testing.FeederSpout;
-import backtype.storm.testing.TestJob;
-import backtype.storm.testing.TrackedTopology;
+import backtype.storm.task.GeneralTopologyContext;
+import backtype.storm.task.OutputCollector;
 import backtype.storm.topology.TopologyBuilder;
+import backtype.storm.tuple.Fields;
+import backtype.storm.tuple.Tuple;
+import backtype.storm.tuple.TupleImpl;
 import backtype.storm.tuple.Values;
 import eu.europeana.cloud.common.model.Permission;
 import eu.europeana.cloud.mcs.driver.RecordServiceClient;
 import eu.europeana.cloud.service.dps.PluginParameterKeys;
 import eu.europeana.cloud.service.dps.service.zoo.ZookeeperKillService;
+import eu.europeana.cloud.service.dps.storm.AbstractDpsBolt;
+import eu.europeana.cloud.service.dps.storm.NotificationTuple;
 import eu.europeana.cloud.service.dps.storm.StormTaskTuple;
 import eu.europeana.cloud.service.mcs.exception.MCSException;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.Mock;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
 
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.mockito.Matchers.any;
-import static org.mockito.Matchers.anyLong;
-import static org.mockito.Matchers.anyString;
+import static org.mockito.Mockito.*;
 
 @RunWith(PowerMockRunner.class)
 @PrepareForTest(RemovePermissionsToFileBolt.class)
@@ -47,93 +39,86 @@ public class RemovePermissionsToFileBoltTest {
     private static final String MALFORMED_MCS_FILE_URL = "MALFORMED_MCS_FILE_URL";
 
     private RecordServiceClient recordServiceClient;
-    private FeederSpout spout;
-    private AckTracker tracker;
+    private RemovePermissionsToFileBolt testedBolt;
+
 
     public RemovePermissionsToFileBoltTest() {
-
     }
+
+    @Mock
+    private OutputCollector collector;
+
 
 
     @Before
     public void setUp() throws Exception {
         dirtyMockZookeeperKS();
         dirtyMockRecordSC();
+        testedBolt = new RemovePermissionsToFileBolt("http://localhost:8080/mcs/", "admin", "admin");
+        Map<String,Object> boltConfig = new HashMap<>();
+        boltConfig.put(Config.STORM_ZOOKEEPER_SERVERS, Arrays.asList("",""));
+        boltConfig.put(Config.STORM_ZOOKEEPER_PORT,"");
+        boltConfig.put(Config.TOPOLOGY_NAME,"");
+        testedBolt.prepare(boltConfig, null, collector);
     }
+
 
     @Test
     public void successfullyRemovingPermissions() throws Exception {
         //given
         Mockito.doNothing().when(recordServiceClient).revokePermissionsToVersion(anyString(), anyString(), anyString(), anyString(), any(Permission.class));
-        final List<Values> data = prepareInputData(PROPER_MCS_FILE_URL);
+        final Tuple tuple = createTestTuple(PROPER_MCS_FILE_URL);
         //when
-        testOnLocalCluster(data);
+        testedBolt.execute(tuple);
         //then
-        assertThat(tracker.getNumAcks(), is(1));
-
+        verify(collector,times(1)).ack(tuple);
+        verify(collector,times(1)).emit(eq(tuple),any(Values.class));
     }
 
-
     @Test
-    public void throwsMalformedExceptionOnGrantingPermissions() throws Exception {
+    public void throwsMalformedExceptionOnRemovingPermissions() throws Exception {
         //given
         Mockito.doNothing().when(recordServiceClient).revokePermissionsToVersion(anyString(), anyString(), anyString(), anyString(), any(Permission.class));
-        final List<Values> data = prepareInputData(MALFORMED_MCS_FILE_URL);
+        final Tuple tuple = createTestTuple(MALFORMED_MCS_FILE_URL);
+        final NotificationTuple expectedNotification = NotificationTuple.prepareNotification(1,
+                "fileUrl", NotificationTuple.States.ERROR, "Url to file is malformed. Permissions will not be removed on: MALFORMED_MCS_FILE_URLno protocol: MALFORMED_MCS_FILE_URL", "{OUTPUT_URL=MALFORMED_MCS_FILE_URL, TASK_SUBMITTER_NAME=user}");
         //when
-        testOnLocalCluster(data);
+        testedBolt.execute(tuple);
         //then
-        assertThat(tracker.getNumAcks(), is(0));
+        verify(collector,times(1)).emit(eq(AbstractDpsBolt.NOTIFICATION_STREAM_NAME), eq(expectedNotification.toStormTuple()));
+        verify(collector,times(0)).ack(tuple);
     }
+
 
     @Test
-    public void throwsMCSExceptionOnGrantingPermissions() throws Exception {
+    public void throwsMCSExceptionOnRemovingPermissions() throws Exception {
         //given
         Mockito.doThrow(new MCSException()).when(recordServiceClient).revokePermissionsToVersion(anyString(), anyString(), anyString(), anyString(), any(Permission.class));
-        final List<Values> data = prepareInputData(PROPER_MCS_FILE_URL);
+        final Tuple tuple = createTestTuple(PROPER_MCS_FILE_URL);
+        final NotificationTuple expectedNotification = NotificationTuple.prepareNotification(1,
+                "fileUrl", NotificationTuple.States.ERROR, "There was exception while trying to remove permissions on: http://localhost:8080/mcs/records/RJUK2YK567SH75DW5GXVH5MXUUGVGX2QUKZC4L6HTPQPZEDE3Q5Q/representations/RepName_0000001/versions/5f860750-8c36-11e5-80b8-0a0027000001/files/cb8f8dc1-78cb-4cb3-a6ad-84e34aa6ca91null", "{OUTPUT_URL=http://localhost:8080/mcs/records/RJUK2YK567SH75DW5GXVH5MXUUGVGX2QUKZC4L6HTPQPZEDE3Q5Q/representations/RepName_0000001/versions/5f860750-8c36-11e5-80b8-0a0027000001/files/cb8f8dc1-78cb-4cb3-a6ad-84e34aa6ca91, TASK_SUBMITTER_NAME=user}");
         //when
-        testOnLocalCluster(data);
+        testedBolt.execute(tuple);
         //then
-        assertThat(tracker.getNumAcks(), is(0));
+        verify(collector,times(1)).emit(eq(AbstractDpsBolt.NOTIFICATION_STREAM_NAME), eq(expectedNotification.toStormTuple()));
+        verify(collector,times(0)).ack(tuple);
+
     }
 
-
-    private void testOnLocalCluster(final List<Values> data) {
-        Testing.withTrackedCluster(new TestJob() {
-            @Override
-            public void run(ILocalCluster cluster)
-                    throws IOException, AlreadyAliveException, InvalidTopologyException {
-                TrackedTopology topology = createTestTopology(cluster);
-                submitTopology(cluster, topology);
-                for (Values tuple : data) {
-                    spout.feed(tuple);
-                    Testing.trackedWait(topology, 1, 60000);
-                }
-
-            }
-        });
-    }
-
-
-    private TrackedTopology createTestTopology(ILocalCluster cluster) {
-        tracker = new AckTracker();
-        spout = new FeederSpout(StormTaskTuple.getFields());
-        spout.setAckFailDelegate(tracker);
-
-        //build topology
+    private Tuple createTestTuple(String fileUrl) {
+        Map<String, String> taskParams = new HashMap<>();
+        taskParams.put(PluginParameterKeys.TASK_SUBMITTER_NAME, "user");
+        taskParams.put(PluginParameterKeys.OUTPUT_URL, fileUrl);
         TopologyBuilder builder = new TopologyBuilder();
-        builder.setSpout("testSpout", spout);
-        RemovePermissionsToFileBolt bolt = new RemovePermissionsToFileBolt("http://localhost:8080/mcs/", "admin", "admin");
-        builder.setBolt("removePermissionsToFileBolt", bolt)
-                .shuffleGrouping("testSpout");
-        return Testing.mkTrackedTopology(cluster, builder.createTopology());
-    }
-
-    private void submitTopology(ILocalCluster cluster, TrackedTopology tt) throws AlreadyAliveException, InvalidTopologyException {
-        //topology config
-        Config config = new Config();
-        config.setNumWorkers(1);
-        config.setDebug(true);
-        cluster.submitTopology("testTopology", config, tt.getTopology());
+        @SuppressWarnings("unchecked")
+        GeneralTopologyContext topologyContext = new GeneralTopologyContext(builder.createTopology(), new Config(), new HashMap(), new HashMap(), new HashMap(), "") {
+            @Override
+            public Fields getComponentOutputFields(String componentId, String streamId) {
+                return StormTaskTuple.getFields();
+            }
+        };
+        Values testValue = new StormTaskTuple(1, "task", "fileUrl", null, taskParams).toStormTuple();
+        return new TupleImpl(topologyContext, testValue, 1, "");
     }
 
     private void dirtyMockZookeeperKS() throws Exception {
@@ -143,15 +128,7 @@ public class RemovePermissionsToFileBoltTest {
 
     }
 
-    private List<Values> prepareInputData(String fileUrl) {
-        int i = 1;
-        List<Values> result = new ArrayList<>();
-        Map<String, String> taskParams = new HashMap<>();
-        taskParams.put(PluginParameterKeys.TASK_SUBMITTER_NAME, "user");
-        taskParams.put(PluginParameterKeys.OUTPUT_URL, fileUrl);
-        result.add(new StormTaskTuple(i, "task", "fileUrl", null, taskParams).toStormTuple());
-        return result;
-    }
+
 
     private void dirtyMockRecordSC() throws Exception {
         recordServiceClient = Mockito.mock(RecordServiceClient.class);
