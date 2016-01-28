905d66daf5fcdbbb5527d4fb4983128e09689ef1
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2016-01-28 02:16:15 -0800

    ECL-769,DPS task is not validated before sending it

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/utils/DpsTaskValidatorFactory.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/utils/DpsTaskValidatorFactory.java
@@ -1,20 +1,27 @@
 package eu.europeana.cloud.service.dps.utils;
 
+import eu.europeana.cloud.service.dps.PluginParameterKeys;
 import eu.europeana.cloud.service.dps.service.utils.validation.DpsTaskValidator;
 import eu.europeana.cloud.service.dps.service.utils.validation.InputDataValueType;
 
 public class DpsTaskValidatorFactory {
 
     private static final DpsTaskValidator EMPTY_VALIDATOR = new DpsTaskValidator();
+    private final static String XSLT_TOPOLOGY_NAME = "xslt_topology";
+    private final static String IC_TOPOLOGY_NAME = "ic_topology";
 
     public static DpsTaskValidator createValidator(String topologyName) {
-        if (topologyName.equals("xsltTopology")) {
-            DpsTaskValidator validator = new DpsTaskValidator().withParameter("XSLT_URL");
+        if (topologyName.equals(XSLT_TOPOLOGY_NAME)) {
+            DpsTaskValidator validator = new DpsTaskValidator().withParameter(PluginParameterKeys.XSLT_URL)
+                    .withDataEntry(PluginParameterKeys.FILE_URLS, InputDataValueType.LINK_TO_FILE)
+                    .withParameter(PluginParameterKeys.TASK_SUBMITTER_NAME);
             return validator;
-        } else if (topologyName.equals("icTopology")) {
+        } else if (topologyName.equals(IC_TOPOLOGY_NAME)) {
             DpsTaskValidator validator = new DpsTaskValidator()
-                    .withDataEntry("FILE_URLS", InputDataValueType.LINK_TO_FILE)
-                    .withParameter("OUTPUT_MIME_TYPE");
+                    .withDataEntry(PluginParameterKeys.FILE_URLS, InputDataValueType.LINK_TO_FILE)
+                    .withParameter(PluginParameterKeys.MIME_TYPE)
+                    .withParameter(PluginParameterKeys.OUTPUT_MIME_TYPE)
+                    .withParameter(PluginParameterKeys.TASK_SUBMITTER_NAME);
             return validator;
         } else {
             return EMPTY_VALIDATOR;
