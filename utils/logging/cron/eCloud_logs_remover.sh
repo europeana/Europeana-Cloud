01f2f656b7ca26fd0baeddbdce80330bf93b9095
pWoz <pwozniak@man.poznan.pl>
2016-02-26 16:07:33 +0100

    ECL-789 scripts for compressing and removing log files.

---
+++ utils/logging/cron/eCloud_logs_remover.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+#
+# Will remove all old log files specified in 'fileNamesForDeletion' array.
+#
+
+#exit script of first failed command
+#set -e
+#forbid using unset variables in bash script
+set -u
+
+logsDirectory=/home/tomcat_user/logs/
+fileNamesForDeletion=( aas uis mcs dls dps iiif)
+daysBack=${1:-20}
+
+echo "Deleting log files for directory: "$logsDirectory
+echo "Days back: "$daysBack
+
+date=`date -u --date="$daysBack days ago" +"%Y-%m-%d"`
+echo "Deleting logs for date: "$date
+
+for j in "${fileNamesForDeletion[@]}"
+do
+        filePath=$logsDirectory$j.log.$date.gz
+        echo "Deleting file:"$filePath
+        rm $filePath
+done
