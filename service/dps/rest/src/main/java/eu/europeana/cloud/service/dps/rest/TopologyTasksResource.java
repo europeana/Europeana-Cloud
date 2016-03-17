f9d5b5718f7e5025bf1160012760b90fb57c719f
akrystian <kadamski@man.poznan.pl>
2016-03-17 12:12:11 +0100

    ECL-662 - add dps rest api test and change mapping

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/TopologyTasksResource.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/TopologyTasksResource.java
@@ -3,7 +3,6 @@ package eu.europeana.cloud.service.dps.rest;
 import com.qmino.miredot.annotations.ReturnType;
 import eu.europeana.cloud.common.model.Permission;
 import eu.europeana.cloud.mcs.driver.RecordServiceClient;
-import eu.europeana.cloud.service.aas.authentication.SpringUserUtils;
 import eu.europeana.cloud.service.commons.urls.UrlParser;
 import eu.europeana.cloud.service.commons.urls.UrlPart;
 import eu.europeana.cloud.service.dps.DpsTask;
@@ -17,19 +16,13 @@ import eu.europeana.cloud.service.dps.service.utils.TopologyManager;
 import eu.europeana.cloud.service.dps.service.utils.validation.DpsTaskValidationException;
 import eu.europeana.cloud.service.dps.service.utils.validation.DpsTaskValidator;
 import eu.europeana.cloud.service.dps.utils.DpsTaskValidatorFactory;
+import eu.europeana.cloud.service.dps.utils.PermissionManager;
 import eu.europeana.cloud.service.mcs.exception.MCSException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.security.access.prepost.PreAuthorize;
-import org.springframework.security.acls.domain.BasePermission;
-import org.springframework.security.acls.domain.ObjectIdentityImpl;
-import org.springframework.security.acls.domain.PrincipalSid;
-import org.springframework.security.acls.model.MutableAcl;
-import org.springframework.security.acls.model.MutableAclService;
-import org.springframework.security.acls.model.NotFoundException;
-import org.springframework.security.acls.model.ObjectIdentity;
 import org.springframework.stereotype.Component;
 
 import javax.ws.rs.Consumes;
@@ -61,22 +54,22 @@ public class TopologyTasksResource {
 
     @Autowired
     ApplicationContext context;
-    
+
     @Autowired
     private TaskExecutionReportService reportService;
 
     @Autowired
     private TaskExecutionSubmitService submitService;
-    
+
     @Autowired
     private TaskExecutionKillService killService;
-    
+
     @Autowired
     private TopologyManager topologyManager;
 
     @Autowired
-    private MutableAclService mutableAclService;
-    
+    private PermissionManager permissionManager;
+
     @Autowired
     private String mcsLocation;
 
@@ -84,24 +77,24 @@ public class TopologyTasksResource {
     private RecordServiceClient recordServiceClient;
 
     private final static String TOPOLOGY_PREFIX = "Topology";
-    private final static String TASK_PREFIX = "DPS_Task";
-    
+    public final static String TASK_PREFIX = "DPS_Task";
+
     private static final Logger LOGGER = LoggerFactory.getLogger(TopologyTasksResource.class);
-    
+
     /**
-     * Retrieves a task with the given taskId from the specified topology. 
-     * 
+     * Retrieves a task with the given taskId from the specified topology.
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Authenticated user</li> 			    
+     *     			<li>Authenticated user</li>
      *     			<li>Read permission for selected task</li>
      * 			</ul>
      * </div>
-     * 
+     *
      * @summary Task retrieval
-     * 
+     *
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
      * @return The requested task.
@@ -124,21 +117,21 @@ public class TopologyTasksResource {
     }
 
     /**
-     * Retrieves the current progress for the requested task. 
-     * 
+     * Retrieves the current progress for the requested task.
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Read permissions for selected task</li> 			    
+     *     			<li>Read permissions for selected task</li>
      * 			</ul>
      * </div>
      *
      * @summary Get Task Progress
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
-     * 
-     * @return Progress for the requested task 
+     *
+     * @return Progress for the requested task
      * (number of records of the specified task that have been fully processed).
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrObjectDoesNotExistException   if task does not exist or access to the task is denied for the user
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
@@ -159,17 +152,17 @@ public class TopologyTasksResource {
     }
 
     /**
-     * Submits a Task for execution. 
+     * Submits a Task for execution.
      * Each Task execution is associated with a specific plugin.
-     * 
+     *
      * <strong>Write permissions required</strong>.
      *
      * @summary Submit Task
      * @param task <strong>REQUIRED</strong> Task to be executed. Should contain links to input data,
-     * either in form of cloud-records or cloud-datasets. 
-     * 
+     * either in form of cloud-records or cloud-datasets.
+     *
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
-     * 
+     *
      * @return URI with information about the submitted task execution.
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
      * @summary Submit Task
@@ -186,15 +179,15 @@ public class TopologyTasksResource {
     ) throws AccessDeniedOrTopologyDoesNotExistException, DpsTaskValidationException, TaskSubmissionException {
 
         LOGGER.info("Submiting task");
-        
+
         assertContainTopology(topologyName);
         validateTask(task, topologyName);
-        
+
         if (task != null) {
             grantPermissionsToTaskResources(topologyName, authorizationHeader, task);
-            
+
             submitService.submitTask(task, topologyName);
-            grantPermissionsForTask(task.getTaskId() + "");
+            permissionManager.grantPermissionsForTask(task.getTaskId() + "");
             String createdTaskUrl = buildTaskUrl(uriInfo, task, topologyName);
             try {
                 LOGGER.info("Task submitted succesfully");
@@ -221,9 +214,9 @@ public class TopologyTasksResource {
      * </div>
      *
      * @summary Retrieve task notifications
-     * 
+     *
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
-     * 
+     *
      * @return Notification messages for the specified task.
      */
     @GET
@@ -234,24 +227,24 @@ public class TopologyTasksResource {
         String progress = reportService.getTaskNotification(taskId);
         return progress;
     }
-    
+
     /**
      * Grants read / write permissions for a task to the specified user.
-     * 
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Admin permissions</li> 			    
+     *     			<li>Admin permissions</li>
      * 			</ul>
      * </div>
-     * 
+     *
      * @summary Grant task permissions to user
-     * 
+     *
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
      * @param username <strong>REQUIRED</strong> Permissions are granted to the account with this unique username
-     * 
+     *
      * @return Status code indicating whether the operation was successful or not.
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
      * @summary Grant task permissions to user
@@ -266,31 +259,31 @@ public class TopologyTasksResource {
         assertContainTopology(topologyName);
 
         if (taskId != null) {
-            grantPermissionsForTask(taskId, username);
+            permissionManager.grantPermissionsForTask(taskId, username);
             return Response.ok().build();
         }
         return Response.notModified().build();
     }
-    
+
     /**
      * Submit kill flag to the specific task.
-     * 
+     *
      * Side effect: remove all flags older than 5 days (per topology).
-     * 
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Authenticated user</li> 			    
+     *     			<li>Authenticated user</li>
      *     			<li>Write permission for selected task</li>
      * 			</ul>
      * </div>
-     * 
+     *
      * @summary Kill task
-     * 
+     *
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
-     * 
+     *
      * @return Status code indicating whether the operation was successful or not.
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
      * @summary Kill task
@@ -311,22 +304,22 @@ public class TopologyTasksResource {
     }
 
     /**
-     * Check kill flag for the specified task. 
-     * 
+     * Check kill flag for the specified task.
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Authenticated user</li> 			    
+     *     			<li>Authenticated user</li>
      *     			<li>Read permission for selected task</li>
      * 			</ul>
      * </div>
      *
      * @summary Check kill flag
-     * 
+     *
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
-     * 
+     *
      * @return true if provided task id has kill flag, false otherwise
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
      * @summary Check kill flag
@@ -339,24 +332,24 @@ public class TopologyTasksResource {
 
         return killService.hasKillFlag(topologyName, Long.valueOf(taskId));
     }
-    
+
     /**
-     * Remove kill flag for the specified task. 
-     * 
+     * Remove kill flag for the specified task.
+     *
      * <br/><br/>
      * <div style='border-left: solid 5px #999999; border-radius: 10px; padding: 6px;'>
      * 		<strong>Required permissions:</strong>
      * 			<ul>
-     *     			<li>Authenticated user</li> 			    
+     *     			<li>Authenticated user</li>
      *     			<li>Write permission for selected task</li>
      * 			</ul>
      * </div>
      *
      * @summary Remove kill flag
-     * 
+     *
      * @param topologyName <strong>REQUIRED</strong> Name of the topology where the task is submitted.
      * @param taskId <strong>REQUIRED</strong> Unique id that identifies the task.
-     * 
+     *
      * @return Status code indicating whether the operation was successful or not.
      * @throws eu.europeana.cloud.service.dps.exception.AccessDeniedOrTopologyDoesNotExistException if topology does not exist or access to the topology is denied for the user
      * @summary Remove kill flag
@@ -374,37 +367,11 @@ public class TopologyTasksResource {
         }
         return Response.notModified().build();
     }
-    
-    /**
-     * Grants permissions to the current user for the specified task.
-     */
-    private void grantPermissionsForTask(String taskId) {
-    	
-    	grantPermissionsForTask(taskId, SpringUserUtils.getUsername());
-    }
 
-    /**
-     * Grants permissions for the specified task to the specified user.
-     */
-    private void grantPermissionsForTask(String taskId, String username) {
-        
-        MutableAcl taskAcl = null;
-        ObjectIdentity taskObjectIdentity = new ObjectIdentityImpl(TASK_PREFIX, taskId);
-        
-        try{
-            taskAcl = (MutableAcl) mutableAclService.readAclById(taskObjectIdentity);
-        }catch(NotFoundException e){
-            taskAcl = mutableAclService.createAcl(taskObjectIdentity);
-        }
-        Object obj = taskAcl.getEntries();
-        taskAcl.insertAce(taskAcl.getEntries().size(), BasePermission.WRITE, new PrincipalSid(username), true);
-        taskAcl.insertAce(taskAcl.getEntries().size(), BasePermission.READ, new PrincipalSid(username), true);
-        
-        mutableAclService.updateAcl(taskAcl);
-    }
+
 
     private String buildTaskUrl(UriInfo uriInfo, DpsTask task, String topologyName) {
-        
+
         StringBuilder taskUrl = new StringBuilder()
                 .append(uriInfo.getBaseUri().toString())
                 .append("topologies/")
@@ -450,11 +417,11 @@ public class TopologyTasksResource {
                     recordServiceClient
                             .useAuthorizationHeader(authorizationHeader)
                             .grantPermissionsToVersion(
-                                parser.getPart(UrlPart.RECORDS), 
-                                parser.getPart(UrlPart.REPRESENTATIONS), 
-                                parser.getPart(UrlPart.VERSIONS), 
-                                topologyUserName, 
-                                Permission.ALL);
+                                    parser.getPart(UrlPart.RECORDS),
+                                    parser.getPart(UrlPart.REPRESENTATIONS),
+                                    parser.getPart(UrlPart.VERSIONS),
+                                    topologyUserName,
+                                    Permission.ALL);
                     LOGGER.info("Permissions granted to: {}", fileUrl);
                 }else{
                     LOGGER.info("Permissions was not granted. Url does not point to file: {}", fileUrl);
