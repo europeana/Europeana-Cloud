a66ad420ad30f5540a338d789cf9bebec1210fb3
akrystian <kadamski@man.poznan.pl>
2016-03-31 08:42:28 +0200

    Upgrade docker scripts.

--- docker/cassandra/change_host.sh
+++ docker/cassandra/change_host.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
 
 IP=`hostname --ip-address`
-CONFIG=/opt/apache-cassandra-2.0.6/conf
+CONFIG=/opt/apache-cassandra-2.1.8/conf
 
-sed -i -e "s/^listen_address.*/listen_address: $IP/"            $CONFIG/cassandra.yaml
+sed -i -e "s/^listen_address.*/listen_address: localhost/"            $CONFIG/cassandra.yaml
 sed -i -e "s/^rpc_address.*/rpc_address: 0.0.0.0/"              $CONFIG/cassandra.yaml
-sed -i -e "s/# broadcast_address.*/broadcast_address: $IP/"              $CONFIG/cassandra.yaml
 sed -i -e "s/# broadcast_rpc_address.*/broadcast_rpc_address: $IP/"              $CONFIG/cassandra.yaml
-sed -i -e "s/127.0.0.1/ $IP/" $CONFIG/cassandra.yaml
--- docker/cassandra/change_host_and_run.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-sh /etc/cassandra/change_host.sh
-/opt/apache-cassandra-2.0.6/bin/cassandra -f 
