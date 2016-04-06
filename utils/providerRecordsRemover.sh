f65d8b7a7f2567f4c1bcbee500af171553e24af8
pWoz <pwozniak@man.poznan.pl>
2016-04-06 15:31:07 +0200

    ECL-811 remarks implemented

--- utils/providerRecordsRemover.sh
+++ utils/providerRecordsRemover.sh
@@ -4,8 +4,8 @@
 #Admin credentials are required here
 #
 #Usage:
-#./script.sh <providerName> <adminName> <adminPassword>
-#./script.sh Provider___%202016_03_24_13_50_28_4_453 admin admin
+#./script.sh <ecloudLocation> <adminName> <adminPassword> <mcsPrefix> <uisPrefix> <providerName>
+#./script.sh http://127.0.0.1:8080 admin admin mcs uis Provider___%202016_03_24_13_50_28_4_453
 #
 #
 
@@ -16,16 +16,15 @@ set -u
 #wildcard *
 shopt -s nullglob
 
-providerId=$1
+ecloudLocation=$1
 adminName=$2
 adminPass=$3
-
-ecloudLocation=http://127.0.0.1:8080
-mcsPrefix=mcs
-uisPrefix=uis
+mcsPrefix=$4
+uisPrefix=$5
+providerId=$6
 
 echo Reading cloudIds for given provider id: $providerId
-existingCloudIds=`curl -s $ecloudLocation/$uisPrefix/data-providers/$providerId/cloudIds | xmllint --xpath "/resultSlice/results/id" -`
+existingCloudIds=`curl -s --insecure $ecloudLocation/$uisPrefix/data-providers/$providerId/cloudIds | xmllint --xpath "/resultSlice/results/id" -`
 
 existingCloudIds=$(echo $existingCloudIds | sed -e 's|</id><id>|\n|g')
 existingCloudIds=$(echo $existingCloudIds | sed -e 's|<id>|''|g')
@@ -37,9 +36,9 @@ echo Found ${#array[@]} cloudIds
 for ((i=0; i < ${#array[@]}; i++))
 do
         echo Removing representations for cloudId: ${array[$i]}
-        curl --user $adminName:$adminPass -X DELETE $ecloudLocation/$mcsPrefix/records/${array[$i]}
+        curl --user $adminName:$adminPass --insecure -X DELETE $ecloudLocation/$mcsPrefix/records/${array[$i]}
         echo Representations removed
         echo Marking cloudId as deleted ${array[$i]}
-        curl --user $adminName:$adminPass -X DELETE $ecloudLocation/$uisPrefix/cloudIds/${array[$i]}
+        curl --user $adminName:$adminPass --insecure -X DELETE $ecloudLocation/$uisPrefix/cloudIds/${array[$i]}
         echo CloudId marked as deleted
 done
