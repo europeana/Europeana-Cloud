8cbdddc7dd446d0510e71f99a1fec1bfb9139761
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2016-07-06 16:20:28 +0200

    ECL-926

--- service/dps/storm/transform/xslt/src/main/java/eu/europeana/cloud/service/dps/storm/xslt/XsltBolt.java
+++ service/dps/storm/transform/xslt/src/main/java/eu/europeana/cloud/service/dps/storm/xslt/XsltBolt.java
@@ -1,7 +1,6 @@
 package eu.europeana.cloud.service.dps.storm.xslt;
 
 import java.io.ByteArrayInputStream;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.StringWriter;
 import java.net.URL;
@@ -28,77 +27,49 @@ public class XsltBolt extends AbstractDpsBolt {
     public static final Logger LOGGER = LoggerFactory.getLogger(XsltBolt.class);
 
     private LRUCache<String, Transformer> cache = new LRUCache<String, Transformer>(
-            100);
+            50);
 
     @Override
     public void execute(StormTaskTuple t) {
 
-        byte[] fileContent = null;
-        String fileUrl = null;
-        String xsltUrl = null;
-
         try {
-
-            fileUrl = t.getFileUrl();
-            fileContent = t.getFileData();
-            xsltUrl = t.getParameter(PluginParameterKeys.XSLT_URL);
-
-            LOGGER.info("processing file: {}", fileUrl);
-            LOGGER.debug("xslt schema:" + xsltUrl);
-
+            String fileUrl = t.getFileUrl();
+            byte[] fileContent = t.getFileData();
+            String xsltUrl = t.getParameter(PluginParameterKeys.XSLT_URL);
+            LOGGER.info("processing file: {} with xslt schema:{}", fileUrl, xsltUrl);
+            Transformer transformer;
+            if (cache.containsKey(xsltUrl)) {
+                transformer = cache.get(xsltUrl);
+            } else {
+                Source xslDoc = new StreamSource(new URL(xsltUrl).openStream());
+                TransformerFactory tFactory = TransformerFactory.newInstance();
+                transformer = tFactory.newTransformer(xslDoc);
+                cache.put(xsltUrl, transformer);
+            }
+            InputStream stream = new ByteArrayInputStream(fileContent);
+            Source xmlDoc = new StreamSource(stream);
+            StringWriter writer = new StringWriter();
+            transformer.transform(xmlDoc, new StreamResult(writer));
+            LOGGER.info("XsltBolt: transformation success for: {}", fileUrl);
+            t.setFileData(writer.toString().getBytes(Charset.forName("UTF-8")));
+            outputCollector.emit(inputTuple, t.toStormTuple());
+        } catch (TransformerConfigurationException e) {
+            LOGGER.error("XsltBolt error:" + e.getMessage());
+            emitDropNotification(t.getTaskId(), "", e.getMessage(), t
+                    .getParameters().toString());
+        } catch (TransformerException e) {
+            LOGGER.error("XsltBolt error:" + e.getMessage());
+            emitDropNotification(t.getTaskId(), "", e.getMessage(), t
+                    .getParameters().toString());
         } catch (Exception e) {
             LOGGER.error("XsltBolt error:" + e.getMessage());
             emitDropNotification(t.getTaskId(), "", e.getMessage(), t
                     .getParameters().toString());
         }
-
-        Source xslDoc = null;
-        Source xmlDoc = null;
-
-        try {
-            xslDoc = new StreamSource(new URL(xsltUrl).openStream());
-            InputStream stream = new ByteArrayInputStream(fileContent);
-            xmlDoc = new StreamSource(stream);
-        } catch (IOException e) {
-            LOGGER.error("XsltBolt error:" + e.getMessage());
-            emitDropNotification(t.getTaskId(), "", e.getMessage(), t
-                    .getParameters().toString());
-        }
-
-        TransformerFactory tFactory = TransformerFactory.newInstance();
-        Transformer transformer = null;
-        StringWriter writer = new StringWriter();
-
-        if (cache.containsKey(xsltUrl)) {
-            transformer = cache.get(xsltUrl);
-        } else {
-            try {
-                transformer = tFactory.newTransformer(xslDoc);
-                cache.put(xsltUrl, transformer);
-            } catch (TransformerConfigurationException e) {
-                LOGGER.error("XsltBolt error:" + e.getMessage());
-                emitDropNotification(t.getTaskId(), "", e.getMessage(), t
-                        .getParameters().toString());
-            }
-        }
-
-        try {
-            transformer.transform(xmlDoc, new StreamResult(writer));
-        } catch (TransformerException e) {
-            LOGGER.error("XsltBolt error:" + e.getMessage());
-            emitDropNotification(t.getTaskId(), "", e.getMessage(), t
-                    .getParameters().toString());
-        }
-
-        LOGGER.info("XsltBolt: transformation success for: {}", fileUrl);
-
-        // pass data to next Bolt
-        t.setFileData(writer.toString().getBytes(Charset.forName("UTF-8")));
-        // outputCollector.emit(t.toStormTuple());
-        outputCollector.emit(inputTuple, t.toStormTuple());
     }
 
     @Override
     public void prepare() {
     }
 }
+
