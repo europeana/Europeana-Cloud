2411853ee729c2b2ef2dc523ee6654cf8b537bb8
Aleksandra Nowak <anowak@man.poznan.pl>
2017-04-24 17:10:44 +0200

    changes to make it work with the new dev machine

--- tools/performanceTest/checkResults.sh
+++ tools/performanceTest/checkResults.sh
@@ -3,12 +3,12 @@
 set -e;
 set -u;
 
-sh ~/performanceTest/checkApps.sh
+sh ~/jenkins/performanceTest/checkApps.sh
 
-sh  ~/performanceTest/performanceTestScript.sh "$@" --host localhost --port 9090 --HTTP --threads 2 --loops 2 --Auth admin admin | tee ~/performanceTest/results.txt
+sh  ~/jenkins/performanceTest/performanceTestScript.sh "$@" --host localhost --port 8080 --HTTP --threads 2 --loops 2 --Auth admin ecloud_admin | tee ~/performanceTest/results.txt
 
-timestamp=( $(cat ~/performanceTest/results.txt |  sed -nr 's/results have timestamp (.+)/\1/p'))
-cmd=( $(cat ~/performanceTest/results.txt | grep Err | sed -e 's/.\+Err:[^0-9]\+\([0-9]\+\).\+/\1/g'))
+timestamp=( $(cat ~/jenkins/performanceTest/results.txt |  sed -nr 's/results have timestamp (.+)/\1/p'))
+cmd=( $(cat ~/jenkins/performanceTest/results.txt | grep Err | sed -e 's/.\+Err:[^0-9]\+\([0-9]\+\).\+/\1/g'))
 
 errors=0
 for ((i=0; i < ${#cmd[@]}; i++));
@@ -17,7 +17,7 @@ for ((i=0; i < ${#cmd[@]}; i++));
         fi;
 done;
 
-rm ~/performanceTest/results.txt
+rm ~/jenkins/performanceTest/results.txt
 
 echo -e  "\n#############################\n"
 if [ $errors -gt 0 ] ; then
