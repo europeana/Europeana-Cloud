4c3c2812258a31301491464dcf11e8169c558726
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2017-05-12 13:19:48 +0200

    ECL-1122 , small changes

--- tools/data-validator/src/main/java/data/validator/jobs/RowsValidatorJob.java
+++ tools/data-validator/src/main/java/data/validator/jobs/RowsValidatorJob.java
@@ -1,8 +1,10 @@
 package data.validator.jobs;
 
 import com.datastax.driver.core.*;
+import data.validator.constants.CassandraColumnTypes;
 
 import java.util.List;
+import java.util.UUID;
 import java.util.concurrent.Callable;
 
 /**
@@ -52,8 +54,7 @@ public class RowsValidatorJob implements Callable<Void> {
 
     private long getMatchingCount(Row row) throws Exception {
         for (int i = 0; i < primaryKeys.size(); i++) {
-            Object value = row.getObject(i);
-            matchingBoundStatement.setString(i, value.toString());
+            boundTheValues(row, i);
         }
         ResultSet resultSet = session.execute(matchingBoundStatement);
         long count = resultSet.one().getLong("count");
@@ -62,6 +63,21 @@ public class RowsValidatorJob implements Callable<Void> {
             throw new Exception("The data doesn't fully match!. The exception was thrown for this query: " + matchingBoundStatement.preparedStatement().getQueryString() + " Using these values" + message);
         }
         return count;
+    }
+
+    private void boundTheValues(Row row, int i) {
+        Object value = row.getObject(i);
+        if (value.getClass().getName().equals(CassandraColumnTypes.UUID.getClassName()))
+            matchingBoundStatement.setUUID(i, row.getUUID(i));
+        else if (value.getClass().getName().equals(CassandraColumnTypes.DATE.getClassName()))
+            matchingBoundStatement.setDate(i, row.getDate(i));
+        else if (value.getClass().getName().equals(CassandraColumnTypes.INTEGER.getClassName()))
+            matchingBoundStatement.setInt(i, row.getInt(i));
+        else if (value.getClass().getName().equals(CassandraColumnTypes.BOOLEAN.getClassName()))
+            matchingBoundStatement.setBool(i, row.getBool(i));
+        else if (value.getClass().getName().equals(CassandraColumnTypes.LONG.getClassName()))
+            matchingBoundStatement.setLong(i, row.getLong(i));
+        else matchingBoundStatement.setString(i, value.toString());
     }
 
     private String constructTheExceptionMessage(Row row) {
