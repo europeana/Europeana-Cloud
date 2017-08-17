fc7331a7043143b910aff8f9c77c13a579b8ca2f
akrystian <kadamski@man.poznan.pl>
2017-08-17 09:52:46 +0200

    ECL-1129 - fix for https://issues.apache.org/jira/browse/CASSANDRA-11850

--- docker/cassandra/scripts/initialize_tables.sh
+++ docker/cassandra/scripts/initialize_tables.sh
@@ -3,12 +3,12 @@ set -u
 set -e
 
 MAIN_DIR=$1
-BIN=$MAIN_DIR/bin
 
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/db_users.cql;
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/aas_setup.cql;
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/uis_setup.cql;
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/mcs_setup.cql;
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/dps_setup.cql;
-$BIN/cqlsh -u cassandra -p cassandra -f /etc/cassandra/aas_users.cql;
-$BIN/cqlsh -u cassandra -p cassandra -e 'select * from ecloud_aas.users;';
+
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/db_users.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/aas_setup.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/uis_setup.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/mcs_setup.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/dps_setup.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -f /etc/cassandra/aas_users.cql;
+cqlsh --cqlversion=3.2.0 -u cassandra -p cassandra -e 'select * from ecloud_aas.users;';
