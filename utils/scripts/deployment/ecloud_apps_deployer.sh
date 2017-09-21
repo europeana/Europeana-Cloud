75e85e40d024ea01be5cb5e014fb515354206873
pWoz <pwozniak@man.poznan.pl>
2017-09-21 14:52:22 +0200

    ECL-1207 deployment script updated

--- utils/scripts/deployment/ecloud_apps_deployer.sh
+++ utils/scripts/deployment/ecloud_apps_deployer.sh
@@ -10,10 +10,10 @@
 
 webAppsDirectory=~/tomcat/webapps/
 backupDirectory=~/eCloud_apps/backup/
-newAppDirectory=~/eCloud_apps/to_be_deployed/20170616/
+newAppDirectory=~/eCloud_apps/to_be_deployed/
 applicationsToBeDeployed=( aas uis mcs dps)
 
-echo "Copying apps to backup directory"
+echo "Copying old apps to backup directory"
 backupTime=`date +"%Y-%m-%d-%H:%M:%S"`
 mkdir $backupDirectory$backupTime
 
@@ -23,6 +23,16 @@ do
         cp -R $webAppsDirectory$i/ $backupDirectory$backupTime/$i/
 
 done
+
+echo "Copying new apps to backup directory"
+mkdir $newAppDirectory$backupTime
+
+for i in "${applicationsToBeDeployed[@]}"
+do
+        echo -e "\tCopying to: "$newAppDirectory$backupTime/$i
+        cp -R $newAppDirectory$i/ $newAppDirectory$backupTime/$i/
+done
+
 echo "Removing apps from tomcat server"
 for i in "${applicationsToBeDeployed[@]}"
 do
@@ -33,9 +43,14 @@ done
 echo "Copying new apps to tomcat server"
 for i in "${applicationsToBeDeployed[@]}"
 do
-        echo -e "\tCopying: "$i
-        mkdir $webAppsDirectory/$i/
+        echo -e "\tCopying: "$i" to "$webAppsDirectory
         cp -R $newAppDirectory$i $webAppsDirectory
 done
 
+echo "Removing new apps from deployment directory"
+for i in "${applicationsToBeDeployed[@]}"
+do
+        echo -e "\tRemoving: "$newAppDirectory$i
+        rm -R $newAppDirectory$i
+done
 
