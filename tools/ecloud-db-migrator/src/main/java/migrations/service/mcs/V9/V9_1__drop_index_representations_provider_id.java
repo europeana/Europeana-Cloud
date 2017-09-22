dd56b994926a7abdee233456fdca9108b8ce47ae
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2017-09-22 13:04:13 +0200

    ECL-1205 , small changes

--- tools/ecloud-db-migrator/src/main/java/migrations/service/mcs/V9/V9_1__drop_index_representations_provider_id.java
+++ tools/ecloud-db-migrator/src/main/java/migrations/service/mcs/V9/V9_1__drop_index_representations_provider_id.java
@@ -1,14 +1,16 @@
-package migrations.service.mcs.V8;
+package migrations.service.mcs.V9;
 
 import com.contrastsecurity.cassandra.migration.api.JavaMigration;
 import com.datastax.driver.core.Session;
 
-
-public class V9_1_drop_index_representations_provider_id implements JavaMigration {
-
+/**
+ * Created by Tarek on 9/22/2017.
+ */
+public class V9_1__drop_index_representations_provider_id implements JavaMigration {
     @Override
     public void migrate(Session session) throws Exception {
         session.execute(
                 "DROP INDEX IF EXISTS representations_provider_id;");
     }
 }
+
