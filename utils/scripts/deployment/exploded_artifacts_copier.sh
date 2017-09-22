41128005df2db73828c13e98d27f8ed333a83f19
pWoz <pwozniak@man.poznan.pl>
2017-09-22 11:42:51 +0200

    ECL-1208 script to copy artifacts

---
+++ utils/scripts/deployment/exploded_artifacts_copier.sh
@@ -0,0 +1,51 @@
+#!/bin/bash
+#
+# This script will:
+# 1. Copy built applications (in exploded form) to remove machines (test or production)
+
+#exit script of first failed command
+#set -e
+
+newAppDirectory=eCloud_apps/to_be_deployed/
+applicationsToBeDeployed=( aas uis mcs dps)
+testMachines=( 81 82 83)
+productionMachines=( 95 96 97)
+environment=$1
+
+
+if [ "$environment" == "TEST" ]
+then
+    echo "Copying new apps to test machines "
+
+    for i in "${testMachines[@]}"
+        do
+            for j in "${applicationsToBeDeployed[@]}"
+                do
+                    appLocation=../../../service/$j/rest/target/ecloud-service-$j-rest-*/
+                    echo -e "\tCopying "$appLocation" to "$i
+                    scp -r -q $appLocation centos@chara-$i.man.poznan.pl:$newAppDirectory/$j/
+                done
+                echo -e "\t"
+        done
+
+elif [ "$environment" == "PROD" ]
+then
+    echo "Copying new apps to prod machines "
+
+    for i in "${productionMachines[@]}"
+        do
+            for j in "${applicationsToBeDeployed[@]}"
+                do
+                    appLocation=../../../service/$j/rest/target/ecloud-service-$j-rest-*/
+                    echo -e "\tCopying "$appLocation" to "$i
+                    scp -r -q $appLocation centos@chara-$i.man.poznan.pl:$newAppDirectory/$j/
+                done
+                echo -e "\t"
+        done
+
+else
+    echo "Missing or wrong parameter"
+    echo "Available parameters"
+    echo -e "\tTEST for test environment"
+    echo -e "\tPROD for production environment"
+fi
