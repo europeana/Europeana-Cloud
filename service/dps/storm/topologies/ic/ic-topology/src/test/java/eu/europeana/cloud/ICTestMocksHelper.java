f7f7c69212930936f4be955dde14109ef4f1c32b
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2017-11-09 12:13:55 +0100

    MET-162 , add notification bolt to OAI topology

--- service/dps/storm/topologies/ic/ic-topology/src/test/java/eu/europeana/cloud/ICTestMocksHelper.java
+++ service/dps/storm/topologies/ic/ic-topology/src/test/java/eu/europeana/cloud/ICTestMocksHelper.java
@@ -1,94 +1,27 @@
 package eu.europeana.cloud;
 
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
-import eu.europeana.cloud.cassandra.CassandraConnectionProvider;
-import eu.europeana.cloud.cassandra.CassandraConnectionProviderSingleton;
+import eu.europeana.cloud.helper.TopologyTestHelper;
 import eu.europeana.cloud.mcs.driver.*;
-import eu.europeana.cloud.service.dps.service.zoo.ZookeeperKillService;
 import eu.europeana.cloud.service.dps.storm.topologies.ic.converter.converter.Converter;
 import eu.europeana.cloud.service.dps.storm.topologies.ic.converter.converter.ConverterContext;
 import eu.europeana.cloud.service.dps.storm.topologies.ic.topology.api.ImageConverterServiceImpl;
 import eu.europeana.cloud.service.dps.storm.topologies.ic.topology.util.ImageConverterUtil;
-import eu.europeana.cloud.service.dps.storm.utils.CassandraSubTaskInfoDAO;
-import eu.europeana.cloud.service.dps.storm.utils.CassandraTaskInfoDAO;
-import org.apache.storm.Config;
-import org.apache.storm.testing.MkClusterParam;
 import org.mockito.Mockito;
 import org.powermock.api.mockito.PowerMockito;
 
-import java.util.List;
 
 import static org.mockito.Matchers.*;
 import static org.mockito.Mockito.doNothing;
-import static org.mockito.Mockito.when;
+
 
 /**
  * @author krystian.
  */
-public class ICTestMocksHelper {
-    protected FileServiceClient fileServiceClient;
-    protected DataSetServiceClient dataSetClient;
-    protected RecordServiceClient recordServiceClient;
-    protected CassandraTaskInfoDAO taskInfoDAO;
-    protected CassandraSubTaskInfoDAO subTaskInfoDAO;
+public class ICTestMocksHelper extends TopologyTestHelper {
+
     protected RepresentationIterator representationIterator;
-    protected RevisionServiceClient revisionServiceClient;
 
-
-    private static final Gson prettyGson = new GsonBuilder().setPrettyPrinting().create();
-    private static Gson gson = new GsonBuilder().create();
-
-    static void prettyPrintJSON(List printIn, String input) {
-        String json = prettyGson.toJson(printIn);
-        System.out.println("=============================" + input + "=============================\n" + json);
-    }
-
-    static String parse(List input) {
-        return gson.toJson(input);
-    }
-
-    static MkClusterParam prepareMKClusterParm() {
-        MkClusterParam mkClusterParam = new MkClusterParam();
-        int SUPERVISORS = 4;
-        mkClusterParam.setSupervisors(SUPERVISORS);
-        Config daemonConf = new Config();
-        daemonConf.put(Config.STORM_LOCAL_MODE_ZMQ, false);
-        mkClusterParam.setDaemonConf(daemonConf);
-        return mkClusterParam;
-    }
-
-    protected void mockZookeeperKS() throws Exception {
-        ZookeeperKillService zookeeperKillService = Mockito.mock(ZookeeperKillService.class);
-        when(zookeeperKillService.hasKillFlag(anyString(), anyLong())).thenReturn(false);
-        PowerMockito.whenNew(ZookeeperKillService.class).withAnyArguments().thenReturn(zookeeperKillService);
-
-    }
-
-    protected void mockRecordSC() throws Exception {
-        recordServiceClient = Mockito.mock(RecordServiceClient.class);
-        PowerMockito.whenNew(RecordServiceClient.class).withAnyArguments().thenReturn(recordServiceClient);
-
-    }
-
-    protected void mockFileSC() throws Exception {
-        fileServiceClient = Mockito.mock(FileServiceClient.class);
-        PowerMockito.whenNew(FileServiceClient.class).withAnyArguments().thenReturn(fileServiceClient);
-
-    }
-
-    protected void mockDatSetClient() throws Exception {
-        dataSetClient = Mockito.mock(DataSetServiceClient.class);
-        PowerMockito.whenNew(DataSetServiceClient.class).withAnyArguments().thenReturn(dataSetClient);
-
-    }
-
-    protected void mockRevisionServiceClient() throws Exception {
-        revisionServiceClient = Mockito.mock(RevisionServiceClient.class);
-        PowerMockito.whenNew(RevisionServiceClient.class).withAnyArguments().thenReturn(revisionServiceClient);
-
-    }
 
     protected void mockImageCS() throws Exception {
         ConverterContext converterContext = Mockito.mock(ConverterContext.class);
@@ -103,18 +36,5 @@ public class ICTestMocksHelper {
         representationIterator = Mockito.mock(RepresentationIterator.class);
         PowerMockito.whenNew(RepresentationIterator.class).withAnyArguments().thenReturn(representationIterator);
     }
-
-
-    protected void mockDPSDAO() throws Exception {
-        taskInfoDAO = Mockito.mock(CassandraTaskInfoDAO.class);
-        PowerMockito.mockStatic(CassandraTaskInfoDAO.class);
-        when(CassandraTaskInfoDAO.getInstance(isA(CassandraConnectionProvider.class))).thenReturn(taskInfoDAO);
-        subTaskInfoDAO = Mockito.mock(CassandraSubTaskInfoDAO.class);
-        PowerMockito.mockStatic(CassandraSubTaskInfoDAO.class);
-        when(CassandraSubTaskInfoDAO.getInstance(isA(CassandraConnectionProvider.class))).thenReturn(subTaskInfoDAO);
-        PowerMockito.mockStatic(CassandraConnectionProviderSingleton.class);
-        when(CassandraConnectionProviderSingleton.getCassandraConnectionProvider(anyString(), anyInt(), anyString(), anyString(), anyString())).thenReturn(Mockito.mock(CassandraConnectionProvider.class));
-    }
-
 
 }
