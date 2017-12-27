347cb13b5e932b8340ca912955e4535726351c68
Mateusz Matela <mmatela@man.poznan.pl>
2017-12-27 16:22:04 +0100

    StatsBold doesn't depend on AbstractDpsBolt

--- service/dps/storm/topologies/media/src/main/java/eu/europeana/cloud/dps/topologies/media/StatsBolt.java
+++ service/dps/storm/topologies/media/src/main/java/eu/europeana/cloud/dps/topologies/media/StatsBolt.java
@@ -6,25 +6,41 @@ import java.util.Map;
 
 import org.apache.storm.task.OutputCollector;
 import org.apache.storm.task.TopologyContext;
+import org.apache.storm.topology.OutputFieldsDeclarer;
+import org.apache.storm.topology.base.BaseRichBolt;
+import org.apache.storm.tuple.Tuple;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import eu.europeana.cloud.common.model.dps.States;
 import eu.europeana.cloud.common.model.dps.TaskState;
-import eu.europeana.cloud.service.dps.service.zoo.ZookeeperKillService;
 import eu.europeana.cloud.service.dps.storm.AbstractDpsBolt;
 import eu.europeana.cloud.service.dps.storm.NotificationTuple;
 import eu.europeana.cloud.service.dps.storm.StormTaskTuple;
 
-public class StatsBolt extends AbstractDpsBolt {
+public class StatsBolt extends BaseRichBolt {
 	private static final Logger logger = LoggerFactory.getLogger(FileDownloadBolt.class);
+	
+	private OutputCollector outputCollector;
 	
 	private int files = 0;
 	private BigInteger size = BigInteger.valueOf(0);
 	private BigInteger time = BigInteger.valueOf(0);
 	
 	@Override
-	public void execute(StormTaskTuple t) {
+	public void prepare(Map stormConf, TopologyContext context, OutputCollector collector) {
+		this.outputCollector = collector;
+	}
+	
+	@Override
+	public void declareOutputFields(OutputFieldsDeclarer declarer) {
+		declarer.declareStream(AbstractDpsBolt.NOTIFICATION_STREAM_NAME, NotificationTuple.getFields());
+	}
+	
+	@Override
+	public void execute(Tuple tuple) {
+		StormTaskTuple t = StormTaskTuple.fromStormTuple(tuple);
+		
 		size = size.add(BigInteger.valueOf(Long.parseLong(t.getParameter("length"))));
 		time = time.add(BigInteger.valueOf(Long.parseLong(t.getParameter("time"))));
 		files++;
@@ -45,30 +61,13 @@ public class StatsBolt extends AbstractDpsBolt {
 		}
 		
 		else {
-			nt =
-					NotificationTuple.prepareUpdateTask(t.getTaskId(),
-							"{\"averageSize\":\"" + averageSize.toString() + "\",\"averageSpeed\":\""
-									+ averageSpeed.toString() + "\",\"averageTime\":\"" + averageTime + "\"}",
-							TaskState.CURRENTLY_PROCESSING,
-							startTime);
-			
+			nt = NotificationTuple.prepareUpdateTask(t.getTaskId(),
+					"{\"averageSize\":\"" + averageSize.toString() + "\",\"averageSpeed\":\""
+							+ averageSpeed.toString() + "\",\"averageTime\":\"" + averageTime + "\"}",
+					TaskState.CURRENTLY_PROCESSING,
+					startTime);
 		}
 		
-		outputCollector.emit(NOTIFICATION_STREAM_NAME, nt.toStormTuple());
-	}
-	
-	@Override
-	public void prepare() {
-		
-	}
-	
-	@Override
-	public void prepare(Map stormConfig, TopologyContext tc, OutputCollector oc) {
-		this.stormConfig = stormConfig;
-		this.topologyContext = tc;
-		this.outputCollector = oc;
-		
-		this.killService = new ZookeeperKillService("127.0.0.1:2181");
-		prepare();
+		outputCollector.emit(AbstractDpsBolt.NOTIFICATION_STREAM_NAME, nt.toStormTuple());
 	}
 }
