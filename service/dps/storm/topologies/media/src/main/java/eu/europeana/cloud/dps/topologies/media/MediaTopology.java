171cad06e915d4b082db90608af343ec25a20804
Mateusz Matela <mmatela@man.poznan.pl>
2017-12-29 13:45:39 +0100

    config in yaml file

--- service/dps/storm/topologies/media/src/main/java/eu/europeana/cloud/dps/topologies/media/MediaTopology.java
+++ service/dps/storm/topologies/media/src/main/java/eu/europeana/cloud/dps/topologies/media/MediaTopology.java
@@ -1,7 +1,9 @@
 package eu.europeana.cloud.dps.topologies.media;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.Map;
-import java.util.Properties;
 
 import org.apache.storm.Config;
 import org.apache.storm.LocalCluster;
@@ -9,60 +11,74 @@ import org.apache.storm.StormSubmitter;
 import org.apache.storm.generated.AlreadyAliveException;
 import org.apache.storm.generated.AuthorizationException;
 import org.apache.storm.generated.InvalidTopologyException;
+import org.apache.storm.shade.org.yaml.snakeyaml.Yaml;
+import org.apache.storm.shade.org.yaml.snakeyaml.constructor.SafeConstructor;
 import org.apache.storm.topology.TopologyBuilder;
 import org.apache.storm.tuple.Fields;
 import org.apache.storm.utils.Utils;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 import eu.europeana.cloud.service.dps.storm.AbstractDpsBolt;
 import eu.europeana.cloud.service.dps.storm.NotificationBolt;
 import eu.europeana.cloud.service.dps.storm.NotificationTuple;
-import eu.europeana.cloud.service.dps.storm.topologies.properties.PropertyFileLoader;
 import eu.europeana.cloud.service.dps.storm.topologies.properties.TopologyPropertyKeys;
 import eu.europeana.cloud.service.dps.storm.utils.TopologyHelper;
 
 public class MediaTopology {
 	
+	private static final Logger logger = LoggerFactory.getLogger(MediaTopology.class);
+	
 	public static void main(String[] args)
 			throws AlreadyAliveException, InvalidTopologyException, AuthorizationException {
 		
-		Config conf = new Config();
-		conf.setDebug(true);
-		conf.setNumWorkers(2);
-		
-		conf.setMessageTimeoutSecs(2000);
-		
-		Properties topologyProperties = new Properties();
-		PropertyFileLoader.loadPropertyFile("media-topology-config.properties", "media-topology-config.properties",
-				topologyProperties);
-		conf.putAll((Map) topologyProperties);
+		Config conf = loadConfig();
 		
 		TopologyBuilder builder = new TopologyBuilder();
 		builder.setSpout("fileUrlSpout", new FileUrlSpout(), 1);
-		builder.setBolt("fileDownloadBolt", new FileDownloadBolt(), 30).shuffleGrouping("fileUrlSpout");
+		builder.setBolt("fileDownloadBolt", new FileDownloadBolt(), 10).shuffleGrouping("fileUrlSpout");
 		builder.setBolt("statsBolt", new StatsBolt(), 1).shuffleGrouping("fileDownloadBolt");
 		
 		builder.setBolt(TopologyHelper.NOTIFICATION_BOLT,
-				new NotificationBolt(topologyProperties.getProperty(TopologyPropertyKeys.CASSANDRA_HOSTS),
-						Integer.parseInt(topologyProperties.getProperty(TopologyPropertyKeys.CASSANDRA_PORT)),
-						topologyProperties.getProperty(TopologyPropertyKeys.CASSANDRA_KEYSPACE_NAME),
-						topologyProperties.getProperty(TopologyPropertyKeys.CASSANDRA_USERNAME),
-						topologyProperties.getProperty(TopologyPropertyKeys.CASSANDRA_PASSWORD)),
-				Integer.parseInt(topologyProperties.getProperty(TopologyPropertyKeys.NOTIFICATION_BOLT_PARALLEL)))
-				.setNumTasks((Integer.parseInt(
-						topologyProperties.getProperty(TopologyPropertyKeys.NOTIFICATION_BOLT_NUMBER_OF_TASKS))))
+				new NotificationBolt((String) conf.get(TopologyPropertyKeys.CASSANDRA_HOSTS),
+						(Integer) conf.get(TopologyPropertyKeys.CASSANDRA_PORT),
+						(String) conf.get(TopologyPropertyKeys.CASSANDRA_KEYSPACE_NAME),
+						(String) conf.get(TopologyPropertyKeys.CASSANDRA_USERNAME),
+						(String) conf.get(TopologyPropertyKeys.CASSANDRA_PASSWORD)),
+				(Integer) conf.get(TopologyPropertyKeys.NOTIFICATION_BOLT_PARALLEL))
+				.setNumTasks((Integer) conf.get(TopologyPropertyKeys.NOTIFICATION_BOLT_NUMBER_OF_TASKS))
 				.fieldsGrouping("statsBolt", AbstractDpsBolt.NOTIFICATION_STREAM_NAME,
 						new Fields(NotificationTuple.taskIdFieldName));
 		
+		String topologyName = (String) conf.get(TopologyPropertyKeys.TOPOLOGY_NAME);
 		
 		if (args.length > 0) {
 			LocalCluster cluster = new LocalCluster();
-			cluster.submitTopology("mediaTopology", conf, builder.createTopology());
-			Utils.sleep(6000000);
-			cluster.killTopology("mediaTopology");
+			cluster.submitTopology(topologyName, conf, builder.createTopology());
+			Utils.sleep(600000);
+			cluster.killTopology(topologyName);
 			cluster.shutdown();
 		} else {
-			StormSubmitter.submitTopology("mms-experiment", conf, builder.createTopology());
+			StormSubmitter.submitTopology(topologyName, conf, builder.createTopology());
 		}
 		
+	}
+
+	private static Config loadConfig() {
+		Config conf = new Config();
+		Yaml yamlConf = new Yaml(new SafeConstructor());
+		String configFileName = "media-topology-config.yaml";
+		try (InputStream is = Thread.currentThread().getContextClassLoader().getResourceAsStream(configFileName)) {
+			conf.putAll((Map) yamlConf.load(is));
+		} catch (IOException e) {
+			throw new RuntimeException("Built in config could not be loaded: " + configFileName, e);
+		}
+		try (InputStream is = new FileInputStream(configFileName)) {
+			conf.putAll((Map) yamlConf.load(is));
+		} catch (IOException e) {
+			logger.warn("Could not load custom config file, using defaults");
+			logger.debug("Custom config load problem", e);
+		}
+		return conf;
 	}
 }
