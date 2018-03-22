2e4a2c834e7b26ea2111de12909a7f48911a325a
pWoz <pwozniak@man.poznan.pl>
2018-03-22 16:15:25 +0100

    MET-573 xslt topology is using metis transformation service now

--- service/dps/storm/transform/xslt/src/main/java/eu/europeana/cloud/service/dps/storm/xslt/XsltBolt.java
+++ service/dps/storm/transform/xslt/src/main/java/eu/europeana/cloud/service/dps/storm/xslt/XsltBolt.java
@@ -5,54 +5,29 @@ import eu.europeana.cloud.service.commons.urls.UrlPart;
 import eu.europeana.cloud.service.dps.PluginParameterKeys;
 import eu.europeana.cloud.service.dps.storm.AbstractDpsBolt;
 import eu.europeana.cloud.service.dps.storm.StormTaskTuple;
-import eu.europeana.cloud.service.dps.util.LRUCache;
+import eu.europeana.metis.transformation.service.XsltTransformer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-import javax.xml.transform.*;
-import javax.xml.transform.stream.StreamResult;
-import javax.xml.transform.stream.StreamSource;
-import java.io.ByteArrayInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.io.StringWriter;
-import java.net.URL;
 import java.nio.charset.Charset;
 
 public class XsltBolt extends AbstractDpsBolt {
 
     public static final Logger LOGGER = LoggerFactory.getLogger(XsltBolt.class);
 
-    private static LRUCache<String, Transformer> cache = new LRUCache<String, Transformer>(
-            50);
-
     @Override
     public void execute(StormTaskTuple stormTaskTuple) {
-        InputStream stream = null;
+        XsltTransformer transformer = new XsltTransformer();
+
         StringWriter writer = null;
-        InputStream xsltStream = null;
         try {
             String fileUrl = stormTaskTuple.getFileUrl();
-            byte[] fileContent = stormTaskTuple.getFileData();
             String xsltUrl = stormTaskTuple.getParameter(PluginParameterKeys.XSLT_URL);
             LOGGER.info("processing file: {} with xslt schema:{}", fileUrl, xsltUrl);
-            Transformer transformer;
-
-            if (cache.containsKey(xsltUrl)) {
-                transformer = cache.get(xsltUrl);
-            } else {
-                xsltStream = new URL(xsltUrl).openStream();
-                Source xslDoc = new StreamSource(xsltStream);
-                TransformerFactory tFactory = TransformerFactory.newInstance();
-                transformer = tFactory.newTransformer(xslDoc);
-                cache.put(xsltUrl, transformer);
-            }
-            // metis specific
-            injectMetisDatasetId(stormTaskTuple, transformer);
-            stream = new ByteArrayInputStream(fileContent);
-            Source xmlDoc = new StreamSource(stream);
-            writer = new StringWriter();
-            transformer.transform(xmlDoc, new StreamResult(writer));
+            String datasetValue = stormTaskTuple.getParameter(PluginParameterKeys.METIS_DATASET_ID);
+            writer = transformer.transform(xsltUrl, fileToTransform(stormTaskTuple), datasetValue);
             LOGGER.info("XsltBolt: transformation success for: {}", fileUrl);
             stormTaskTuple.setFileData(writer.toString().getBytes(Charset.forName("UTF-8")));
 
@@ -65,52 +40,25 @@ public class XsltBolt extends AbstractDpsBolt {
             stormTaskTuple.getParameters().remove(PluginParameterKeys.XSLT_URL);
 
             outputCollector.emit(inputTuple, stormTaskTuple.toStormTuple());
-        } catch (TransformerConfigurationException e) {
-            LOGGER.error("XsltBolt error:" + e.getMessage());
-            emitErrorNotification(stormTaskTuple.getTaskId(), "", e.getMessage(), stormTaskTuple
-                    .getParameters().toString());
-        } catch (TransformerException e) {
-            LOGGER.error("XsltBolt error:" + e.getMessage());
-            emitErrorNotification(stormTaskTuple.getTaskId(), "", e.getMessage(), stormTaskTuple
-                    .getParameters().toString());
         } catch (Exception e) {
             LOGGER.error("XsltBolt error:" + e.getMessage());
             emitErrorNotification(stormTaskTuple.getTaskId(), "", e.getMessage(), stormTaskTuple
                     .getParameters().toString());
         } finally {
-            if (stream != null) {
-                try {
-                    stream.close();
-                } catch (IOException e) {
-                    LOGGER.error("error: during closing the stream" + e.getMessage());
-                }
-            }
-            if (xsltStream != null) {
-                try {
-                    xsltStream.close();
-                } catch (IOException e) {
-                    LOGGER.error("error: during closing the stream" + e.getMessage());
-                }
-            }
-            if (writer != null) {
+            if (writer != null)
                 try {
                     writer.close();
                 } catch (IOException e) {
                     LOGGER.error("error: during closing the writter" + e.getMessage());
                 }
-            }
         }
     }
 
-    private void injectMetisDatasetId(StormTaskTuple stormTaskTuple, Transformer transformer) {
-        String value = stormTaskTuple.getParameter(PluginParameterKeys.METIS_DATASET_ID);
-        if (value != null && !value.isEmpty()) {
-            transformer.setParameter(PluginParameterKeys.METIS_DATASET_ID_PARAMETER_NAME, value);
-        }
+    private byte[] fileToTransform(StormTaskTuple stormTaskTuple) {
+        return stormTaskTuple.getFileData();
     }
 
     @Override
     public void prepare() {
     }
 }
-
