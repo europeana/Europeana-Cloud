ec71379474e741f3baf34836e101437dddefa8aa
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2018-04-06 13:09:23 +0200

    refactor http static topology after MET-450 merge

--- service/dps/storm/examples/src/main/java/eu/europeana/cloud/service/dps/examples/toplologies/StaticHttpTopology.java
+++ service/dps/storm/examples/src/main/java/eu/europeana/cloud/service/dps/examples/toplologies/StaticHttpTopology.java
@@ -3,12 +3,11 @@ package eu.europeana.cloud.service.dps.examples.toplologies;
 import eu.europeana.cloud.service.dps.examples.StaticDpsTaskSpout;
 import eu.europeana.cloud.service.dps.examples.toplologies.builder.SimpleStaticHTTPTopologyBuilder;
 import eu.europeana.cloud.service.dps.examples.util.DpsTaskUtil;
-import org.apache.storm.Config;
+import eu.europeana.cloud.service.dps.examples.util.TopologyConfigBuilder;
 import org.apache.storm.LocalCluster;
 import org.apache.storm.generated.StormTopology;
 import org.apache.storm.utils.Utils;
 
-import java.util.Arrays;
 
 import static eu.europeana.cloud.service.dps.examples.toplologies.constants.TopologyConstants.*;
 
@@ -16,17 +15,14 @@ import static eu.europeana.cloud.service.dps.examples.toplologies.constants.Topo
  * Created by Tarek on 10/2/2017.
  */
 public class StaticHttpTopology {
+    public static final String TOPOLOGY_NAME = "http_topology";
     public static void main(String[] args) {
-        StaticDpsTaskSpout staticDpsTaskSpout = new StaticDpsTaskSpout(DpsTaskUtil.generateDPSTaskForHTTP());
+        StaticDpsTaskSpout staticDpsTaskSpout = new StaticDpsTaskSpout(DpsTaskUtil.generateDPSTaskForHTTP(),TOPOLOGY_NAME);
         StormTopology stormTopology = SimpleStaticHTTPTopologyBuilder.buildTopology(staticDpsTaskSpout, UIS_URL, MCS_URL);
-        Config conf = new Config();
-        conf.setDebug(true);
-        conf.put(Config.TOPOLOGY_DEBUG, true);
-        conf.put(Config.TOPOLOGY_MESSAGE_TIMEOUT_SECS,36000);
         LocalCluster cluster = new LocalCluster();
-        cluster.submitTopology("test", conf, stormTopology);
+        cluster.submitTopology(TOPOLOGY_NAME, TopologyConfigBuilder.buildConfig(), stormTopology);
         Utils.sleep(60000);
-        cluster.killTopology("test");
+        cluster.killTopology(TOPOLOGY_NAME);
         cluster.shutdown();
 
     }
