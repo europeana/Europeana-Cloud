c5ba083aac0945397208863d70c940a606787ab8
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2018-04-10 16:21:40 +0200

    MET-837 , small changes

--- service/dps/utils/src/main/java/eu/europeana/cloud/service/dps/service/utils/validation/TargetIndexingDatabase.java
+++ service/dps/utils/src/main/java/eu/europeana/cloud/service/dps/service/utils/validation/TargetIndexingDatabase.java
@@ -1,6 +1,22 @@
 package eu.europeana.cloud.service.dps.service.utils.validation;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public enum TargetIndexingDatabase {
     PUBLISH,
-    PREVIEW
+    PREVIEW;
+    private static List<String> values = initializeTargetIndexingDatabaseValues();
+
+    public static List<String> getTargetIndexingDatabaseValues() {
+        return values;
+    }
+
+    private static List<String> initializeTargetIndexingDatabaseValues() {
+        List<String> values = new ArrayList<>();
+        for (TargetIndexingDatabase targetIndexingDatabase : TargetIndexingDatabase.values())
+            values.add(targetIndexingDatabase.toString());
+        return values;
+    }
 }
+
