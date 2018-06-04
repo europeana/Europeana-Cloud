dc98578ce17f266380767048ef96448dda728e73
Tarek Alkhaeir <tarekkh@man.poznan.pl>
2018-06-04 15:17:51 +0200

    MET-1096 , implementing remarks

--- service/dps/storm/topologies/normalization/src/test/java/eu/europeana/cloud/normalization/NormalizationTopologyTest.java
+++ service/dps/storm/topologies/normalization/src/test/java/eu/europeana/cloud/normalization/NormalizationTopologyTest.java
@@ -27,6 +27,7 @@ import org.junit.Before;
 import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.mockito.MockitoAnnotations;
 import org.powermock.core.classloader.annotations.PowerMockIgnore;
 import org.powermock.core.classloader.annotations.PrepareForTest;
 import org.powermock.modules.junit4.PowerMockRunner;
@@ -65,6 +66,7 @@ public class NormalizationTopologyTest extends TopologyTestHelper {
 
     @Before
     public final void setUp() throws Exception {
+        MockitoAnnotations.initMocks(this);
         mockRecordSC();
         mockFileSC();
         mockCassandraInteraction();
