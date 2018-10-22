cddf98a91915e84d6e59e04c646c45e882db54ee
pWoz <pwozniak@man.poznan.pl>
2018-10-22 17:24:57 +0200

    backup script updated

--- utils/scripts/maintenance/backupCreator.sh
+++ utils/scripts/maintenance/backupCreator.sh
@@ -6,52 +6,105 @@
 #set -e
 
 keyspacesToBeBackuped=(
-    production_ecloud_mcs)
+    production_ecloud_mcs_v12)
 
 backupTime=`date +"%Y-%m-%d"`
-dataLocation=~/playground/sampleCass/
+dataLocation=~/nosql_filesystem/cassandra/data/data/
+backupLocation=~/mnt/ARCHIVE/test
+machine=`hostname`
 
 ################
 ##
 ################
 
+check_request_valid () {
+    echo "Checking request"
+    keyspacesExists
+
+}
+
+keyspacesExists () {
+    for i in "${keyspacesToBeBackuped[@]}"
+        do
+            if [ ! -d "$dataLocation$i" ]; then
+                echo -e "\t'$i' does not exist. Stoping execution."
+                exit
+            fi
+        done
+    echo -e "\tKeyspaces exists"
+}
+
+################
+##
+################
+
+check_request_valid
+
 echo "Preparing backup"
 
-echo "Flushing dbs"
+while getopts ":s:" opt; do
+case $opt in
+    s)
+      snapshotToBeUsed=$OPTARG
+      echo "Snapshot name was provided. We will use '$snapshotToBeUsed' for backup."
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" >&2
+      exit 1
+      ;;
+    :)
+      echo "Option -$OPTARG requires an argument." >&2
+      exit 1
+      ;;
+ esac
+done
 
-for i in "${keyspacesToBeBackuped[@]}"
-    do
-        echo -e "\tFlushing: $i"
-    done
+if [ -z ${snapshotToBeUsed+x} ];
+    then
+        echo "Snapshot name is not specified. New snapshot will be created.";
+        echo "Flushing dbs"
 
-echo "Creating snapshots"
+        for i in "${keyspacesToBeBackuped[@]}"
+            do
+                echo -e "\tFlushing: $i"
+            done
 
-snapshotDirName="backup_$backupTime"
+        echo "Creating snapshots"
 
-for i in "${keyspacesToBeBackuped[@]}"
-    do
-        if [ -d "$snapshotDirName" ]; then
-            echo "Directory was already created. Script will be stopped."
-            exit
-        fi
-        echo -e "\tCreating snapshots for: $i in '$snapshotDirName' directory."
-    done
+        snapshotDirName="backup_$backupTime"
+        snapshotToBeUsed=$snapshotDirName
+
+        for i in "${keyspacesToBeBackuped[@]}"
+            do
+                echo -e "\tCreating snapshots for: $i in '$snapshotDirName' directory."
+                #nodetool snapshot -t $snapshotDirName $i
+            done
+    else
+        echo "Snapshot '$snapshotToBeUsed' will be used."; fi
 
 echo "About to start backups generation"
 
 for i in "${keyspacesToBeBackuped[@]}"
         do
             echo -e "\tWill backup: $i"
+            for table in $dataLocation$i/*
+            do
+                echo -e "\t\tGenerating tarball for ${table}"
 
-            for keyspace in $dataLocation$i/* ; do
-                for table in $keyspace ; do
-                    echo -e "\t\tGenerating tarball for $table"
-                    tar --exclude=sample.tar -cvf $table/snapshot/$snapshotDirName/sample.tar $table/snapshot/$snapshotDirName/*
+                if [ ! -d "$table/snapshots/$snapshotToBeUsed/" ]; then
+                    echo -e "\t\t[WARN]Directory '$table/snapshots/$snapshotToBeUsed/' does not exist. Skipping."
+                    continue
+                fi
 
-                    echo -e "\t\tCopying created tarball to backup directory"
-                    #rsync
-                    echo -e "\t\tRemoving created tarball from local directory"
-                    #rm
-                done
+                cd $table/snapshots/$snapshotToBeUsed/
+                tar --exclude=${table##*/}.tar -cf $table/snapshots/$snapshotToBeUsed/${table##*/}.tar $table/snapshots/$snapshotToBeUsed/*
+
+                backupPath=$backupLocation/$backupTime/$i/${table##*/}/$machine/
+                echo -e "\t\tCopying created tarball ${table##*/}.tar to backup directory $backupPath"
+                mkdir -p $backupPath
+                #rsync --progress -vh ${table##*/}.tar $backupPath
+                echo -e "\t\tRemoving created tarball from local directory"
+                #rm
+                echo -e " "
             done
         done
