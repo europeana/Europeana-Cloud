fb933b79e599867ac9489600d8c36273881e8a08
pWoz <pwozniak@man.poznan.pl>
2019-01-31 11:38:18 +0100

    Missing text colors added

--- utils/scripts/maintenance/fullBackupCreator.sh
+++ utils/scripts/maintenance/fullBackupCreator.sh
@@ -34,7 +34,7 @@ NC='\033[0m' # No Color
 ################
 
 check_request_valid () {
-    echo "${GREEN}Checking request${NC}"
+    echo -e "${GREEN}Checking request${NC}"
     keyspacesExists
 
 }
@@ -47,7 +47,7 @@ keyspacesExists () {
                 exit
             fi
         done
-    echo -e "\tKeyspaces exists"
+    echo -e "${GREEN}\tKeyspaces exists${NC}"
 }
 
 ################
@@ -56,20 +56,20 @@ keyspacesExists () {
 
 check_request_valid
 
-echo "Preparing backup"
+echo  -e "${GREEN}Preparing backup${NC}"
 
 while getopts ":s:" opt; do
 case $opt in
     s)
       snapshotToBeUsed=$OPTARG
-      echo "${GREEN}Snapshot name was provided. We will use '$snapshotToBeUsed' for backup.${NC}"
+      echo -e "${GREEN}Snapshot name was provided. We will use '$snapshotToBeUsed' for backup.${NC}"
       ;;
     \?)
-      echo "${GREEN}Invalid option: -$OPTARG${NC}" >&2
+      echo -e "${GREEN}Invalid option: -$OPTARG${NC}" >&2
       exit 1
       ;;
     :)
-      echo "${GREEN}Option -$OPTARG requires an argument.${NC}" >&2
+      echo -e "${GREEN}Option -$OPTARG requires an argument.${NC}" >&2
       exit 1
       ;;
  esac
@@ -77,8 +77,8 @@ done
 
 if [ -z ${snapshotToBeUsed+x} ];
     then
-        echo "${GREEN}Snapshot name is not specified. New snapshot will be created.${NC}";
-        echo "${GREEN}Flushing dbs${NC}"
+        echo -e "${GREEN}Snapshot name is not specified. New snapshot will be created.${NC}";
+        echo -e "${GREEN}Flushing dbs${NC}"
 
         for i in "${keyspacesToBeBackuped[@]}"
             do
@@ -86,7 +86,7 @@ if [ -z ${snapshotToBeUsed+x} ];
 		        nodetool flush $i
             done
 
-        echo "Creating snapshots"
+        echo -e "${GREEN}Creating snapshots${NC}"
 
         snapshotDirName="backup_$backupTimeInDays"
         snapshotToBeUsed=$snapshotDirName
@@ -97,30 +97,30 @@ if [ -z ${snapshotToBeUsed+x} ];
                 nodetool snapshot -t $snapshotDirName $i
             done
     else
-        echo "${GREEN}Snapshot '$snapshotToBeUsed' will be used.${NC}"; fi
+        echo -e "${GREEN}Snapshot '$snapshotToBeUsed' will be used.${NC}"; fi
 
-echo "About to start backups generation"
+echo -e "${GREEN}About to start backups generation${NC}"
 
 for i in "${keyspacesToBeBackuped[@]}"
-        do
-            echo -e "${GREEN}\tWill backup: $i${NC}"
-            for table in $dataLocation$i/*
-            do
-                echo -e "${GREEN}\t\tGenerating tarball for ${table}${NC}"
+do
+    echo -e "${GREEN}\tWill backup: $i${NC}"
+    for table in $dataLocation$i/*
+    do
+        echo -e "${GREEN}\t\tGenerating tarball for ${table}${NC}"
 
-            if [ ! -d "$table/snapshots/$snapshotToBeUsed/" ]; then
-                    echo -e "${GREEN}\t\t[WARN]Directory '$table/snapshots/$snapshotToBeUsed/' does not exist. Skipping.${NC}"
-                    continue
-                fi
-		    cd $table/snapshots/$snapshotToBeUsed/
-            tar --warning=none --exclude=${table##*/}.tar -cvf ${table##*/}.tar *
+        if [ ! -d "$table/snapshots/$snapshotToBeUsed/" ]; then
+            echo -e "${GREEN}\t\t[WARN]Directory '$table/snapshots/$snapshotToBeUsed/' does not exist. Skipping.${NC}"
+            continue
+        fi
+        cd $table/snapshots/$snapshotToBeUsed/
+        tar --warning=none --exclude=${table##*/}.tar -cvf ${table##*/}.tar *
 
-		    backupPath=$backupLocation/$backupTimeInMonths/$i/${table##*/}
-            echo -e "${GREEN}\t\tCopying created tarball ${table##*/}.tar to backup directory $backupPath${NC}"
-		    mkdir -p $backupPath
-		    rsync --progress -vh ${table##*/}.tar $backupPath
-            echo -e "${GREEN}\t\tRemoving created tarball from local directory${NC}"
-            rm ${table##*/}.tar
-            echo -e " "
-            done
-        done
+        backupPath=$backupLocation/$backupTimeInMonths/$i/${table##*/}
+        echo -e "${GREEN}\t\tCopying created tarball ${table##*/}.tar to backup directory $backupPath${NC}"
+        mkdir -p $backupPath
+        rsync --progress -vh ${table##*/}.tar $backupPath
+        echo -e "${GREEN}\t\tRemoving created tarball from local directory${NC}"
+        rm ${table##*/}.tar
+        echo -e " "
+    done
+done
