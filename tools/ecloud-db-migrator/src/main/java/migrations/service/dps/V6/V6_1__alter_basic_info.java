ad76ba11cf1d47611f2454508486b48acc16d971
Arek Margraf <margraf@man.poznan.pl>
2019-11-21 11:12:09 +0100

    MET-2314 Changes in 'basic_info' table (new column 'task_informations') + move creating 'tasks_by_task_state' from dps_setup.cql to migration mechanism

--- tools/ecloud-db-migrator/src/main/java/migrations/service/dps/V6/V6_1__alter_basic_info.java
+++ tools/ecloud-db-migrator/src/main/java/migrations/service/dps/V6/V6_1__alter_basic_info.java
@@ -6,6 +6,16 @@ import com.datastax.driver.core.Session;
 public class V6_1__alter_basic_info implements JavaMigration {
     @Override
     public void migrate(Session session) throws Exception {
+        session.execute(
+                "CREATE TABLE tasks_by_task_state(" +
+                        "        state varchar," +
+                        "        topology_name varchar," +
+                        "        task_id bigint," +
+                        "        application_id varchar," +
+                        "        start_time timestamp," +
+                        "        PRIMARY KEY(state,topology_name,task_id)" +
+                        ");"
+        );
         session.execute("ALTER TABLE basic_info ADD task_informations text;");
     }
 }
