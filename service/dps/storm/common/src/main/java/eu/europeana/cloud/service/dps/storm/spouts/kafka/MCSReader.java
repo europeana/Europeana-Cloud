bcd982e79b5d679654691a2d1c64e097a4039725
pWoz <pwozniak@man.poznan.pl>
2020-05-13 14:09:05 +0200

    Refactorization for MCSReader

--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/spouts/kafka/MCSReader.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/spouts/kafka/MCSReader.java
@@ -11,7 +11,6 @@ import eu.europeana.cloud.mcs.driver.RepresentationIterator;
 import eu.europeana.cloud.mcs.driver.exception.DriverException;
 import eu.europeana.cloud.service.commons.urls.UrlParser;
 import eu.europeana.cloud.service.commons.urls.UrlPart;
-import eu.europeana.cloud.service.mcs.exception.MCSException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -20,25 +19,21 @@ import java.util.List;
 import static eu.europeana.cloud.service.dps.storm.utils.Retriever.*;
 
 public class MCSReader implements AutoCloseable {
-    private final static Logger LOGGER = LoggerFactory.getLogger(MCSReader.class);
-    private final String authorizationHeader;
-    private String mcsClientURL;
+    private static final Logger LOGGER = LoggerFactory.getLogger(MCSReader.class);
 
-    private DataSetServiceClient dataSetServiceClient;
+    private final DataSetServiceClient dataSetServiceClient;
 
-    private FileServiceClient fileServiceClient;
+    private final FileServiceClient fileServiceClient;
 
-    private RecordServiceClient recordServiceClient;
+    private final RecordServiceClient recordServiceClient;
 
     public MCSReader(String mcsClientURL, String authorizationHeader) {
-        this.mcsClientURL = mcsClientURL;
-        this.authorizationHeader = authorizationHeader;
         dataSetServiceClient = new DataSetServiceClient(mcsClientURL, authorizationHeader);
         recordServiceClient = new RecordServiceClient(mcsClientURL, authorizationHeader);
         fileServiceClient = new FileServiceClient(mcsClientURL, authorizationHeader);
     }
 
-    public ResultSlice<CloudIdAndTimestampResponse> getLatestDataSetCloudIdByRepresentationAndRevisionChunk(String representationName, String revisionName, String revisionProvider, String datasetName, String datasetProvider, String startFrom) throws MCSException, DriverException {
+    public ResultSlice<CloudIdAndTimestampResponse> getLatestDataSetCloudIdByRepresentationAndRevisionChunk(String representationName, String revisionName, String revisionProvider, String datasetName, String datasetProvider, String startFrom) throws DriverException {
         return retryOnError3Times("Error while getting slice of latest cloud Id from data set.", () -> {
             ResultSlice<CloudIdAndTimestampResponse> resultSlice = dataSetServiceClient.getLatestDataSetCloudIdByRepresentationAndRevisionChunk(datasetName, datasetProvider, revisionProvider, revisionName, representationName, false, startFrom);
             if (resultSlice == null || resultSlice.getResults() == null) {
@@ -49,7 +44,7 @@ public class MCSReader implements AutoCloseable {
     }
 
 
-    public ResultSlice<CloudTagsResponse> getDataSetRevisionsChunk(String representationName, String revisionName, String revisionProvider, String revisionTimestamp, String datasetProvider, String datasetName, String startFrom) throws MCSException, DriverException {
+    public ResultSlice<CloudTagsResponse> getDataSetRevisionsChunk(String representationName, String revisionName, String revisionProvider, String revisionTimestamp, String datasetProvider, String datasetName, String startFrom) throws DriverException {
         return retryOnError3Times("Error while getting Revisions from data set.", () -> {
             ResultSlice<CloudTagsResponse> resultSlice = dataSetServiceClient.getDataSetRevisionsChunk(datasetProvider, datasetName, representationName,
                     revisionName, revisionProvider, revisionTimestamp, startFrom, null);
@@ -62,15 +57,9 @@ public class MCSReader implements AutoCloseable {
         });
     }
 
-    public List<Representation> getRepresentationsByRevision(String representationName, String revisionName, String revisionProvider, String revisionTimestamp, String responseCloudId) throws MCSException {
+    public List<Representation> getRepresentationsByRevision(String representationName, String revisionName, String revisionProvider, String revisionTimestamp, String responseCloudId) {
         return retryOnError3Times("Error while getting representation revision.", () ->
                 recordServiceClient.getRepresentationsByRevision(responseCloudId, representationName, revisionName, revisionProvider, revisionTimestamp));
-    }
-
-    public String getFileUri(Representation representation, String fileName) {
-        return retryOnError3Times("Error while getting File URI from MCS", () ->
-                fileServiceClient.getFileUri(representation.getCloudId(), representation.getRepresentationName(), representation.getVersion(), fileName).toString()
-        );
     }
 
     public RepresentationIterator getRepresentationsOfEntireDataset(UrlParser urlParser) {
@@ -79,16 +68,8 @@ public class MCSReader implements AutoCloseable {
 
 
     public void close() {
-        if (dataSetServiceClient != null) {
-            dataSetServiceClient.close();
-        }
-
-        if (recordServiceClient != null) {
-            recordServiceClient.close();
-        }
-        if (fileServiceClient != null) {
-            fileServiceClient.close();
-        }
+        dataSetServiceClient.close();
+        recordServiceClient.close();
+        fileServiceClient.close();
     }
-
 }
