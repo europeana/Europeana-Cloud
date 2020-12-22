2fc921cc648264dfe5ff47f6b5d4d8921561a801
marcin-rp <mratajczak@man.poznan.pl>
2020-12-22 12:37:53 +0100

    Corrected EnrichmentBolt after Metis library changes

--- service/dps/storm/topologies/dereference-enrichment/src/main/java/eu/europeana/cloud/enrichment/bolts/EnrichmentBolt.java
+++ service/dps/storm/topologies/dereference-enrichment/src/main/java/eu/europeana/cloud/enrichment/bolts/EnrichmentBolt.java
@@ -7,6 +7,8 @@ import eu.europeana.enrichment.rest.client.EnrichmentWorker;
 import eu.europeana.enrichment.rest.client.EnrichmentWorkerImpl;
 import eu.europeana.enrichment.rest.client.dereference.DereferencerProvider;
 import eu.europeana.enrichment.rest.client.enrichment.EnricherProvider;
+import eu.europeana.enrichment.rest.client.exceptions.DereferenceException;
+import eu.europeana.enrichment.rest.client.exceptions.EnrichmentException;
 import org.apache.commons.lang.exception.ExceptionUtils;
 import org.apache.storm.tuple.Tuple;
 import org.slf4j.Logger;
@@ -64,7 +66,11 @@ public class EnrichmentBolt extends AbstractDpsBolt {
         final DereferencerProvider dereferencerProvider = new DereferencerProvider();
         dereferencerProvider.setDereferenceUrl(dereferenceURL);
         dereferencerProvider.setEnrichmentUrl(enrichmentURL);
-        enrichmentWorker = new EnrichmentWorkerImpl(dereferencerProvider.create(),
-                enricherProvider.create());
+        try {
+            enrichmentWorker = new EnrichmentWorkerImpl(dereferencerProvider.create(),
+                    enricherProvider.create());
+        } catch (DereferenceException | EnrichmentException e) {
+            throw new RuntimeException("Could not instantiate EnrichmentBolt due Exception in enrich worker creating",e);
+        }
     }
 }
