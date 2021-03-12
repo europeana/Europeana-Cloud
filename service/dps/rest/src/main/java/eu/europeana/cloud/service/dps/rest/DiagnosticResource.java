f8c2e302cbb28caf61d76ce14872119d419b0546
marcin-rp <mratajczak@man.poznan.pl>
2021-03-12 12:02:42 +0100

    MET-3461 Implemented HarvestedRecordDAO

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/DiagnosticResource.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/rest/DiagnosticResource.java
@@ -1,14 +1,21 @@
 package eu.europeana.cloud.service.dps.rest;
 
 import eu.europeana.cloud.common.model.dps.TaskInfo;
+import eu.europeana.cloud.service.dps.storm.utils.HarvestedRecord;
+import eu.europeana.cloud.service.dps.storm.utils.HarvestedRecordDAO;
 import eu.europeana.cloud.service.dps.utils.GhostTaskService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Scope;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Iterator;
 import java.util.List;
 
 @RestController
@@ -19,10 +26,33 @@ public class DiagnosticResource {
     @Autowired
     private GhostTaskService ghostTaskService;
 
+    @Autowired
+    private HarvestedRecordDAO harvestedRecordDAO;
+
     @GetMapping("/ghostTasks")
     @PreAuthorize("hasRole('ROLE_ADMIN')")
     public List<TaskInfo> ghostTasks() {
         return ghostTaskService.findGhostTasks();
     }
+
+
+    @GetMapping("/harvestedRecords/{providerId}/{datasetId}")
+    @PreAuthorize("hasRole('ROLE_ADMIN')")
+    public List<HarvestedRecord> harvestedRecords(@PathVariable String providerId, @PathVariable String datasetId
+            , @RequestParam(defaultValue = "10") int count, @RequestParam(required = false) String oaiId)    {
+        if(oaiId!=null) {
+            return Collections.singletonList(harvestedRecordDAO.findRecord(providerId, datasetId,oaiId).orElse(null));
+        }else {
+            List<HarvestedRecord> result = new ArrayList<>();
+
+            Iterator<HarvestedRecord> it = harvestedRecordDAO.findDatasetRecords(providerId, datasetId);
+            for (int i = 0; i < count && it.hasNext(); i++) {
+                HarvestedRecord record = it.next();
+                result.add(record);
+            }
+            return result;
+        }
+    }
+
 
 }
--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/HarvestedRecordDAO.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/HarvestedRecordDAO.java
@@ -1,14 +1,19 @@
 package eu.europeana.cloud.service.dps.storm.utils;
 
 import com.datastax.driver.core.PreparedStatement;
+import com.datastax.driver.core.Row;
 import eu.europeana.cloud.cassandra.CassandraConnectionProvider;
+
+import java.util.Iterator;
+import java.util.Optional;
 
 public class HarvestedRecordDAO extends CassandraDAO {
 
     private static final int OAI_ID_BUCKET_COUNT = 32;
     private static HarvestedRecordDAO instance;
     private PreparedStatement insertHarvestedRecord;
-    private PreparedStatement findHarvestedRecord;
+    private PreparedStatement findRecord;
+    private PreparedStatement findAllRecordInDataset;
 
     public static synchronized HarvestedRecordDAO getInstance(CassandraConnectionProvider cassandra) {
         if (instance == null) {
@@ -17,9 +22,6 @@ public class HarvestedRecordDAO extends CassandraDAO {
         return instance;
     }
 
-    /**
-     * @param dbService The service exposing the connection and session
-     */
     public HarvestedRecordDAO(CassandraConnectionProvider dbService) {
         super(dbService);
     }
@@ -29,32 +31,110 @@ public class HarvestedRecordDAO extends CassandraDAO {
         insertHarvestedRecord = dbService.getSession().prepare("INSERT INTO "
                 + CassandraTablesAndColumnsNames.HARVESTED_RECORD_TABLE + "("
                 + CassandraTablesAndColumnsNames.HARVESTED_RECORD_PROVIDER_ID
-                +","+ CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID
-                +","+ CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID_BUCKET_NO
-                +","+  CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID
-                +","+CassandraTablesAndColumnsNames.HARVESTED_RECORD_HARVEST_DATE
-                +") VALUES(?,?,?,?,?);"
-                );
+                + "," + CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID
+                + "," + CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID_BUCKET_NO
+                + "," + CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID
+                + "," + CassandraTablesAndColumnsNames.HARVESTED_RECORD_HARVEST_DATE
+                + ") VALUES(?,?,?,?,?);"
+        );
 
         insertHarvestedRecord.setConsistencyLevel(dbService.getConsistencyLevel());
 
-        findHarvestedRecord = dbService.getSession().prepare(
+        findRecord = dbService.getSession().prepare(
                 "SELECT * FROM " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_TABLE
                         + " WHERE " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_PROVIDER_ID + " = ? "
-                        +                        " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID + " = ? "
-                        +                        " AND "+ CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID_BUCKET_NO + " = ? "
-                        +                        " AND "+ CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID + " = ? ");
+                        + " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID + " = ? "
+                        + " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID_BUCKET_NO + " = ? "
+                        + " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID + " = ? "
+        );
 
-        findHarvestedRecord.setConsistencyLevel(dbService.getConsistencyLevel());
+        findRecord.setConsistencyLevel(dbService.getConsistencyLevel());
+
+        findAllRecordInDataset = dbService.getSession().prepare(
+                "SELECT * FROM " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_TABLE
+                        + " WHERE " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_PROVIDER_ID + " = ? "
+                        + " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID + " = ? "
+                        + " AND " + CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID_BUCKET_NO + " = ? "
+        );
+
+        findAllRecordInDataset.setConsistencyLevel(dbService.getConsistencyLevel());
+
+
     }
 
     public void insertHarvestedRecord(HarvestedRecord record) {
-        dbService.getSession().execute(insertHarvestedRecord.bind( record.getProviderId(), record.getDatasetId(),
-                oaiIdBucketNo(record.getOaiId()), record.getOaiId(),record.getHarvestDate()));
+        dbService.getSession().execute(insertHarvestedRecord.bind(record.getProviderId(), record.getDatasetId(),
+                oaiIdBucketNo(record.getOaiId()), record.getOaiId(), record.getHarvestDate()));
+    }
+
+    public Optional<HarvestedRecord> findRecord(String providerId, String datasetId, String oaiId) {
+        return Optional.ofNullable(dbService.getSession().execute(
+                findRecord.bind(providerId, datasetId, oaiIdBucketNo(oaiId), oaiId))
+                .one()
+        ).map(this::readRecord);
+    }
+
+    public Iterator<HarvestedRecord> findDatasetRecords(String providerId, String datasetId) {
+        return new HarvestedRecordIterator(providerId, datasetId);
     }
 
 
+    private HarvestedRecord readRecord(Row row) {
+        return HarvestedRecord.builder()
+                .providerId(row.getString(CassandraTablesAndColumnsNames.HARVESTED_RECORD_PROVIDER_ID))
+                .datasetId(row.getString(CassandraTablesAndColumnsNames.HARVESTED_RECORD_DATASET_ID))
+                .oaiId(row.getString(CassandraTablesAndColumnsNames.HARVESTED_RECORD_OAI_ID))
+                .harvestDate(row.getTimestamp(CassandraTablesAndColumnsNames.HARVESTED_RECORD_HARVEST_DATE))
+                .indexingDate(row.getTimestamp(CassandraTablesAndColumnsNames.HARVESTED_RECORD_INDEXING_DATE))
+                .md5(row.getUUID(CassandraTablesAndColumnsNames.HARVESTED_RECORD_MD5))
+                .ignored(row.getBool(CassandraTablesAndColumnsNames.HARVESTED_RECORD_IGNORED))
+                .build();
+    }
+
     private int oaiIdBucketNo(String oaiId) {
         return (OAI_ID_BUCKET_COUNT - 1) & oaiId.hashCode();
+    }
+
+    private class HarvestedRecordIterator implements Iterator<HarvestedRecord> {
+        private final String providerId;
+        private final String datasetId;
+        private int bucketNo = 0;
+        private Iterator<Row> currentBucketIterator;
+
+        public HarvestedRecordIterator(String providerId, String datasetId) {
+            this.providerId = providerId;
+            this.datasetId = datasetId;
+            queryBucket();
+        }
+
+        @Override
+        public boolean hasNext() {
+            goToNextBucketIfNeeded();
+            return currentBucketIterator.hasNext();
+        }
+
+        @Override
+        public HarvestedRecord next() {
+            goToNextBucketIfNeeded();
+            return readRecord(currentBucketIterator.next());
+        }
+
+        private void goToNextBucketIfNeeded() {
+            while (!currentBucketIterator.hasNext() && notLastBucket()) {
+                bucketNo++;
+                queryBucket();
+            }
+        }
+
+        private boolean notLastBucket() {
+            return bucketNo < (OAI_ID_BUCKET_COUNT - 1);
+        }
+
+        private void queryBucket() {
+            currentBucketIterator = dbService.getSession().execute(
+                    findAllRecordInDataset.bind(providerId, datasetId, bucketNo))
+                    .iterator();
+        }
+
     }
 }
