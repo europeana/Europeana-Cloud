8403dc9e547e12cc8a9f4f2c7ec8961c3e97a3be
pWoz <pwozniak@man.poznan.pl>
2021-03-15 13:24:06 +0100

    HarvestingWriteRecordBolt processes now list of local identifiers

--- service/dps/storm/io/src/main/java/eu/europeana/cloud/service/dps/storm/io/HarvestingWriteRecordBolt.java
+++ service/dps/storm/io/src/main/java/eu/europeana/cloud/service/dps/storm/io/HarvestingWriteRecordBolt.java
@@ -1,17 +1,19 @@
 package eu.europeana.cloud.service.dps.storm.io;
 
-
 import eu.europeana.cloud.client.uis.rest.CloudException;
 import eu.europeana.cloud.client.uis.rest.UISClient;
 import eu.europeana.cloud.common.model.CloudId;
+import eu.europeana.cloud.common.response.ErrorInfo;
 import eu.europeana.cloud.service.dps.PluginParameterKeys;
 import eu.europeana.cloud.service.dps.storm.StormTaskTuple;
 import eu.europeana.cloud.service.dps.storm.utils.RetryableMethodExecutor;
 import eu.europeana.cloud.service.uis.exception.IdHasBeenMappedException;
 import eu.europeana.cloud.service.uis.exception.RecordDoesNotExistException;
+import eu.europeana.cloud.service.uis.exception.RecordIdDoesNotExistException;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import java.util.List;
 
 /**
  * Stores a Record on the cloud for the harvesting topology.
@@ -25,7 +27,7 @@ public class HarvestingWriteRecordBolt extends WriteRecordBolt {
     public static final String ERROR_MSG_WHILE_GETTING_CLOUD_ID = "Error while getting CloudId";
     private static final long serialVersionUID = 1L;
     private static final Logger LOGGER = LoggerFactory.getLogger(HarvestingWriteRecordBolt.class);
-    private String ecloudUisAddress;
+    private final String ecloudUisAddress;
     private transient UISClient uisClient;
 
     public HarvestingWriteRecordBolt(String ecloudMcsAddress, String ecloudUisAddress) {
@@ -35,6 +37,7 @@ public class HarvestingWriteRecordBolt extends WriteRecordBolt {
 
     @Override
     public void prepare() {
+        LOGGER.info("Preparing bolt for UIS address: {}", ecloudUisAddress);
         uisClient = new UISClient(ecloudUisAddress);
         super.prepare();
     }
@@ -47,10 +50,8 @@ public class HarvestingWriteRecordBolt extends WriteRecordBolt {
     @Override
     protected RecordWriteParams prepareWriteParameters(StormTaskTuple stormTaskTuple) throws CloudException {
         String providerId = stormTaskTuple.getParameter(PluginParameterKeys.PROVIDER_ID);
-        String localId = stormTaskTuple.getParameter(PluginParameterKeys.CLOUD_LOCAL_IDENTIFIER);
-        String additionalLocalIdentifier = stormTaskTuple.getParameter(PluginParameterKeys.ADDITIONAL_LOCAL_IDENTIFIER);
         String authenticationHeader = stormTaskTuple.getParameter(PluginParameterKeys.AUTHORIZATION_HEADER);
-        String cloudId = getCloudId(authenticationHeader, providerId, localId, additionalLocalIdentifier);
+        String cloudId = getCloudId(authenticationHeader, providerId, stormTaskTuple.getIdentifiersToUse());
         String representationName = stormTaskTuple.getParameter(PluginParameterKeys.NEW_REPRESENTATION_NAME);
         if ((representationName == null || representationName.isEmpty()) && stormTaskTuple.getSourceDetails() != null) {
             representationName = stormTaskTuple.getParameter(PluginParameterKeys.SCHEMA_NAME);
@@ -64,19 +65,21 @@ public class HarvestingWriteRecordBolt extends WriteRecordBolt {
         return writeParams;
     }
 
-    private String getCloudId(String authorizationHeader, String providerId, String localId, String additionalLocalIdentifier) throws CloudException {
+    private String getCloudId(String authorizationHeader, String providerId, List<String> identifiersToUse) throws CloudException {
+        if (identifiersToUse.isEmpty())
+            throw new CloudException("List of local identifiers has to be longer than 1", new RecordIdDoesNotExistException(new ErrorInfo()));
         String result;
         CloudId cloudId;
-        cloudId = getCloudId(providerId, localId, authorizationHeader);
+        cloudId = getCloudId(providerId, identifiersToUse.get(0), authorizationHeader);
         if (cloudId != null) {
             result = cloudId.getId();
         } else {
-            result = createCloudId(providerId, localId, authorizationHeader);
+            result = createCloudId(providerId, identifiersToUse.get(0), authorizationHeader);
         }
-        if (additionalLocalIdentifier != null)
-            attachAdditionalLocalIdentifier(additionalLocalIdentifier, result, providerId, authorizationHeader);
+        for (int i = 1; i < identifiersToUse.size(); i++) {
+            attachAdditionalLocalIdentifier(identifiersToUse.get(i), result, providerId, authorizationHeader);
+        }
         return result;
-
     }
 
     private CloudId getCloudId(String providerId, String localId, String authenticationHeader) throws CloudException {
