ed2fff03ae914ebd3c2d62bfb937135d4f45c9e3
marcin-rp <mratajczak@man.poznan.pl>
2021-03-17 14:01:27 +0100

    MET-3461 Review remarks part 2, hash function

--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/BucketUtils.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/BucketUtils.java
@@ -1,12 +1,29 @@
 package eu.europeana.cloud.service.dps.storm.utils;
 
-import com.google.common.hash.Hashing;
-import org.apache.commons.io.Charsets;
+import org.apache.commons.codec.digest.MurmurHash3;
+
+import java.nio.charset.StandardCharsets;
 
 public class BucketUtils {
 
+    static {
+        String libVersion = MurmurHash3.class.getPackage().getImplementationVersion();
+        if (!"1.15".equals(libVersion)) {
+            //This exception is thrown to prevent unexpected change of hashCode implementation in apache library
+            //which we use in hashFunction
+            //This hashcode is used as partition key in Cassandra so, after its chage data would be corrupted.
+            //After changing library version the method we should execute test method to check its behaviour
+            // eu.europeana.cloud.service.dps.storm.utils.BucketUtilsTest.testHashFuctionUnchanged()
+            throw new IllegalStateException("Detected chagned commons-coded library version: " +
+                    libVersion + ". Hash fuction should be verified!");
+        }
+    }
+
     public static int bucketNumber(String key, int bucketCount) {
-        return (bucketCount - 1) &
-                Hashing.murmur3_32().hashString(key, Charsets.UTF_8).asInt();
+        return (bucketCount - 1) & hash(key);
+    }
+
+    public static int hash(String key) {
+        return MurmurHash3.hash32x86(key.getBytes(StandardCharsets.UTF_8));
     }
 }
