f18a84eb5db0d31f6010b799efdf0addd206588d
marcin-rp <mratajczak@man.poznan.pl>
2021-03-26 16:15:34 +0100

    MET-3456 Repaired class BucketUtils - removed checking of commons-codec library version, it did not worked on application build to deploy on storm cluster.

--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/BucketUtils.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/utils/BucketUtils.java
@@ -6,19 +6,6 @@ import java.nio.charset.StandardCharsets;
 
 public class BucketUtils {
 
-    static {
-        String libVersion = MurmurHash3.class.getPackage().getImplementationVersion();
-        if (!"1.15".equals(libVersion)) {
-            //This exception is thrown to prevent unexpected change of hashCode implementation in apache library
-            //which we use in hashFunction
-            //This hashcode is used as partition key in Cassandra so, after its chage data would be corrupted.
-            //After changing library version the method we should execute test method to check its behaviour
-            // eu.europeana.cloud.service.dps.storm.utils.BucketUtilsTest.testHashFuctionUnchanged()
-            throw new IllegalStateException("Detected chagned commons-coded library version: " +
-                    libVersion + ". Hash fuction should be verified!");
-        }
-    }
-
     private BucketUtils() {
     }
 
@@ -26,6 +13,11 @@ public class BucketUtils {
         return (bucketCount - 1) & hash(key);
     }
 
+    //WARNING: This hashcode is used as partition key in Cassandra, so after any change in method behaviour data in
+    // Cassandra would be corrupted!
+    // It also could be in case of updating library apache commons-codecs. So change log of library should be verified.
+    // In any case test: eu.europeana.cloud.service.dps.storm.utils.BucketUtilsTest.testHashFuctionUnchanged()
+    // could be used to check if function works properly.
     public static int hash(String key) {
         return MurmurHash3.hash32x86(key.getBytes(StandardCharsets.UTF_8));
     }
---
+++ service/dps/storm/common/src/test/java/eu/europeana/cloud/service/dps/storm/utils/BucketUtilsTest.java
@@ -0,0 +1,40 @@
+package eu.europeana.cloud.service.dps.storm.utils;
+
+import com.google.common.io.Resources;
+import org.apache.commons.io.Charsets;
+import org.junit.Test;
+
+import java.io.FileWriter;
+import java.io.IOException;
+import java.nio.charset.StandardCharsets;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+
+public class BucketUtilsTest {
+
+    @Test
+    //WARNING This hash function is used as partition key in Cassandra, so after any change in method behaviour data in
+    // Cassandra would be corrupted.
+    public void shouldHashFunctionBeUnchanged() throws IOException {
+        List<String> strings = Resources.readLines(Resources.getResource("random_strings.txt"), Charsets.UTF_8);
+        List<String> hashes = Resources.readLines(Resources.getResource("string_hashcodes.txt"), Charsets.UTF_8);
+
+        assertEquals(strings.size(), hashes.size());
+        assertFalse(strings.isEmpty());
+        for (int i = 0; i < strings.size(); i++) {
+            assertEquals("Bad hash for string in line number " + i, Integer.parseInt(hashes.get(i)), BucketUtils.hash(strings.get(i)));
+        }
+    }
+
+    //method to generate file with hash code - could be executed by hand
+    public void regenerateHashes() throws IOException {
+        try (FileWriter out = new FileWriter("/home/user/dir/string_hashcodes.txt", StandardCharsets.UTF_8)) {
+            for (String string : Resources.readLines(Resources.getResource("random_strings.txt"), Charsets.UTF_8)) {
+                out.write("" + BucketUtils.hash(string));
+                out.write("\n");
+            }
+        }
+    }
+}
