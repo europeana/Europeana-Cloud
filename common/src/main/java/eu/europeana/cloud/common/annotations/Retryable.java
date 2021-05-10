146d13a4b9c9bf736dd4218b8a20bf19ef63944d
Arek Margraf <margraf@man.poznan.pl>
2021-05-10 09:20:29 +0200

    MET-3459 Annotation for re-try mechanism

---
+++ common/src/main/java/eu/europeana/cloud/common/annotations/Retryable.java
@@ -0,0 +1,24 @@
+package eu.europeana.cloud.common.annotations;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+import java.lang.annotation.Target;
+
+@Retention(RetentionPolicy.RUNTIME)
+@Target(ElementType.METHOD)
+public @interface Retryable {
+    /**
+     * Maximum number of attempts while retry mechanism works
+     * Default value is set to 3
+     * @return Number of attempt
+     */
+    int maxAttempts() default 3;
+
+    /**
+     * Number of milliseconds between fails attempt and next try
+     * Default value is set to 10[s]= 10*1000[ms]
+     * @return Number of milliseconds
+     */
+    int delay() default 10*1000;
+}
