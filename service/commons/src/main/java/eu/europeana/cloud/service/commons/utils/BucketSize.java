b1dfd450c035505a9903234eae68fc97f21fa2c3
pWoz <pwozniak@man.poznan.pl>
2021-05-31 15:08:53 +0200

    Bucket size reduced for provided_record_id table

--- service/commons/src/main/java/eu/europeana/cloud/service/commons/utils/BucketSize.java
+++ service/commons/src/main/java/eu/europeana/cloud/service/commons/utils/BucketSize.java
@@ -2,7 +2,7 @@ package eu.europeana.cloud.service.commons.utils;
 
 public class BucketSize {
 
-    public static final int PROVIDER_RECORD_ID_TABLE = 950000;
+    public static final int PROVIDER_RECORD_ID_TABLE = 200000;
 
     private BucketSize(){}
 }
