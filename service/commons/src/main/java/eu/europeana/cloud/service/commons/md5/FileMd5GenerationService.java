a7126ed383c1db29e830dbc6a36c550d28344844
Arek Margraf <margraf@man.poznan.pl>
2021-06-10 15:20:45 +0200

    MET-2276 MD5 features for UUID

--- service/commons/src/main/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationService.java
+++ service/commons/src/main/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationService.java
@@ -1,12 +1,15 @@
 package eu.europeana.cloud.service.commons.md5;
 
+import org.apache.commons.io.FileUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.ByteArrayInputStream;
+import java.io.File;
 import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.util.UUID;
 
 /**
  * Generates md5 hash from the provided file.
@@ -19,22 +22,36 @@ public class FileMd5GenerationService {
     private FileMd5GenerationService() {
     }
 
-    public static String generate(Path file) throws IOException {
-        try (var is = Files.newInputStream(file)) {
-            return org.apache.commons.codec.digest.DigestUtils.md5Hex(is);
-        }
+    public static UUID generate(Path filePath) throws IOException {
+        return generate(Files.readAllBytes(filePath));
     }
 
-    public static String generate(byte[] fileBytes) {
-        try (var is = new ByteArrayInputStream(fileBytes)) {
-            return org.apache.commons.codec.digest.DigestUtils.md5Hex(is);
-        } catch (IOException e) {
-            LOGGER.error("Unable to generate md5", e);
-            return "";
-        }
-
-
+    public static UUID generate(byte[] fileBytes) {
+        return UUID.nameUUIDFromBytes(fileBytes);
     }
 
+    public static boolean areEquals(byte[] md5digest, UUID uuid) {
+        long msb = 0L;
+        long lsb = 0L;
+
+        if(md5digest == null && uuid == null) {
+            return true;
+        } else if(md5digest == null || uuid == null) {
+            return false;
+        }
+
+        int i;
+        for(i = 0; i < 8; ++i) {
+            msb = msb << 8 | (long)(md5digest[i] & 255);
+        }
+
+        for(i = 8; i < 16; ++i) {
+            lsb = lsb << 8 | (long)(md5digest[i] & 255);
+        }
+
+        UUID md5uuid = new UUID(msb, lsb);
+
+        return md5uuid.compareTo(uuid) == 0;
+    }
 
 }
--- service/commons/src/test/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationServiceTest.java
+++ service/commons/src/test/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationServiceTest.java
@@ -3,10 +3,12 @@ package eu.europeana.cloud.service.commons.md5;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
+import java.io.ByteArrayInputStream;
 import java.io.File;
 import java.io.IOException;
 import java.net.URISyntaxException;
 import java.nio.file.Paths;
+import java.util.UUID;
 
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertNotSame;
@@ -15,39 +17,39 @@ public class FileMd5GenerationServiceTest {
 
     @Test
     public void shouldGenerateDifferentMd5ForSlightlyDifferentFiles() throws URISyntaxException, IOException {
-        String record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
-        String record2 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1_1.xml").toURI()));
+        UUID record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
+        UUID record2 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1_1.xml").toURI()));
         assertNotSame(record1, record2);
     }
 
     @Test
     public void shouldGenerateDifferentMd5ForSlightlyDifferentFilesProvidedAsByteArrays() throws IOException {
-        String record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
-        String record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1_1.xml").getFile())));
+        UUID record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
+        UUID record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1_1.xml").getFile())));
         assertNotSame(record1, record2);
     }
 
     @Test
     public void shouldGenerateTheSameMd5ForSameFile() throws URISyntaxException, IOException {
-        String record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
-        String record2 = FileMd5GenerationService.generate(Paths.get(getClass().getClassLoader().getResource("Lithuania_1.xml").toURI()));
+        UUID record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
+        UUID record2 = FileMd5GenerationService.generate(Paths.get(getClass().getClassLoader().getResource("Lithuania_1.xml").toURI()));
         assertEquals(record1, record2);
     }
 
     @Test
     public void shouldGenerateTheSameMd5ForSameFileProvidedAsByteArray() throws IOException {
-        String record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
-        String record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
+        UUID record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
+        UUID record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
         assertEquals(record1, record2);
     }
 
     @Test
     public void shouldGenerateDifferentMd5ForDifferentFiles() throws URISyntaxException, IOException {
-        String record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
-        String record1_1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1_1.xml").toURI()));
-        String record2 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_2.xml").toURI()));
-        String record3 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_3.xml").toURI()));
-        String record4 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_4.xml").toURI()));
+        UUID record1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1.xml").toURI()));
+        UUID record1_1 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_1_1.xml").toURI()));
+        UUID record2 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_2.xml").toURI()));
+        UUID record3 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_3.xml").toURI()));
+        UUID record4 = FileMd5GenerationService.generate(Paths.get(ClassLoader.getSystemResource("Lithuania_4.xml").toURI()));
         assertNotSame(record1, record2);
         assertNotSame(record1, record1_1);
         assertNotSame(record1, record3);
@@ -62,11 +64,11 @@ public class FileMd5GenerationServiceTest {
 
     @Test
     public void shouldGenerateDifferentMd5ForDifferentFilesProvidedAsByteArrays() throws IOException {
-        String record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
-        String record1_1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1_1.xml").getFile())));
-        String record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_2.xml").getFile())));
-        String record3 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_3.xml").getFile())));
-        String record4 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_4.xml").getFile())));
+        UUID record1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile())));
+        UUID record1_1 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1_1.xml").getFile())));
+        UUID record2 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_2.xml").getFile())));
+        UUID record3 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_3.xml").getFile())));
+        UUID record4 = FileMd5GenerationService.generate(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_4.xml").getFile())));
 
         assertNotSame(record1, record2);
         assertNotSame(record1, record1_1);
