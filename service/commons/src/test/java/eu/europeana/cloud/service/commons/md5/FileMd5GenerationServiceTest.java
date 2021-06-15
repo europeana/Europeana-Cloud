81cdbc7eafa918c500e9cfeb98bfcb074553d772
marcin-rp <mratajczak@man.poznan.pl>
2021-06-15 17:41:53 +0200

    MET-3703 Review remarks

--- service/commons/src/test/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationServiceTest.java
+++ service/commons/src/test/java/eu/europeana/cloud/service/commons/md5/FileMd5GenerationServiceTest.java
@@ -1,5 +1,6 @@
 package eu.europeana.cloud.service.commons.md5;
 
+import com.google.common.collect.Sets;
 import org.apache.commons.io.FileUtils;
 import org.junit.Test;
 
@@ -51,16 +52,8 @@ public class FileMd5GenerationServiceTest {
         UUID record2 = FileMd5GenerationService.generateUUID(Paths.get(ClassLoader.getSystemResource("Lithuania_2.xml").toURI()));
         UUID record3 = FileMd5GenerationService.generateUUID(Paths.get(ClassLoader.getSystemResource("Lithuania_3.xml").toURI()));
         UUID record4 = FileMd5GenerationService.generateUUID(Paths.get(ClassLoader.getSystemResource("Lithuania_4.xml").toURI()));
-        assertNotSame(record1, record2);
-        assertNotSame(record1, record1_1);
-        assertNotSame(record1, record3);
-        assertNotSame(record1, record4);
-        assertNotSame(record1_1, record2);
-        assertNotSame(record1_1, record3);
-        assertNotSame(record1_1, record4);
-        assertNotSame(record2, record3);
-        assertNotSame(record2, record4);
-        assertNotSame(record3, record4);
+
+        assertNotEquals(record1, record1_1, record2, record3, record4);
     }
 
     @Test
@@ -71,16 +64,19 @@ public class FileMd5GenerationServiceTest {
         UUID record3 = FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_3.xml").getFile())));
         UUID record4 = FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_4.xml").getFile())));
 
-        assertNotSame(record1, record2);
-        assertNotSame(record1, record1_1);
-        assertNotSame(record1, record3);
-        assertNotSame(record1, record4);
-        assertNotSame(record1_1, record2);
-        assertNotSame(record1_1, record3);
-        assertNotSame(record1_1, record4);
-        assertNotSame(record2, record3);
-        assertNotSame(record2, record4);
-        assertNotSame(record3, record4);
+        assertNotEquals(record1, record1_1, record2, record3, record4);
+    }
+
+
+
+    @Test
+    public void shouldGenerateTheSameUUIDsAsEarlier() throws URISyntaxException, IOException {
+        assertEquals(UUID.fromString("de709ba0-6d52-31e5-a7c1-24793dca071d"), FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1.xml").getFile()))));
+        assertEquals(UUID.fromString("7d13c967-fcb9-3dbf-8243-0ae4c03bb299"), FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_1_1.xml").getFile()))));
+        assertEquals(UUID.fromString("75b85fde-42be-3cd0-87d6-693485ec3124"), FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_2.xml").getFile()))));
+        assertEquals(UUID.fromString("79071711-1865-3d60-b872-665be230000d"), FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_3.xml").getFile()))));
+        assertEquals(UUID.fromString("e3d4dd38-9b62-39a6-b03d-a86e54ef64f2"), FileMd5GenerationService.generateUUID(FileUtils.readFileToByteArray(new File(ClassLoader.getSystemResource("Lithuania_4.xml").getFile()))));
+
     }
 
     @Test
@@ -173,7 +169,7 @@ public class FileMd5GenerationServiceTest {
         byte[] md5FromApacheCommons = org.apache.commons.codec.digest.DigestUtils.md5(Files.newInputStream(pathToFile));
         byte[] ms5FromDefaultJava = FileMd5GenerationService.generate(Files.readAllBytes(pathToFile));
 
-        assertEquals(0, Arrays.compare(md5FromApacheCommons, ms5FromDefaultJava));
+        assertArrayEquals(md5FromApacheCommons, ms5FromDefaultJava);
     }
 
     private void testMD5GeneratedAsHexAndDirectlyAsUUIDIsEquals(String filename) throws URISyntaxException, IOException {
@@ -183,6 +179,10 @@ public class FileMd5GenerationServiceTest {
         UUID md5UUIDFromDefaultJava = FileMd5GenerationService.generateUUID(Files.readAllBytes(pathToFile));
 
         assertEquals(FileMd5GenerationService.md5ToUUID(md5Hex), md5UUIDFromDefaultJava);
+    }
+
+    private void assertNotEquals(UUID... uuids) {
+        assertEquals(uuids.length, Sets.newHashSet(uuids).size());
     }
 
 }
