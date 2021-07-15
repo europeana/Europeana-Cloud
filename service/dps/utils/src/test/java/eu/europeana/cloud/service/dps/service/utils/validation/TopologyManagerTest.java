45d20c5b76e8bd8c19ed6ce28944b416fdc18381
marcin-rp <mratajczak@man.poznan.pl>
2021-07-15 09:14:58 +0200

    MET-3130/MET-3749 Review - corrected method name.

--- service/dps/utils/src/test/java/eu/europeana/cloud/service/dps/service/utils/validation/TopologyManagerTest.java
+++ service/dps/utils/src/test/java/eu/europeana/cloud/service/dps/service/utils/validation/TopologyManagerTest.java
@@ -51,7 +51,7 @@ public class TopologyManagerTest {
     }
 
     @Test
-    public void shouldContainTopologyC() {
+    public void shouldNotContainTopologyC() {
         baseContainsTopologyTest("topologyC", false);
     }
 
