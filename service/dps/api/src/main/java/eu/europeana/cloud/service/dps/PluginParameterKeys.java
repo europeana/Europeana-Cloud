6ca8b881d028fa5ed60349d724463140870a84a6
marcin-rp <mratajczak@man.poznan.pl>
2021-07-21 09:50:31 +0200

    MET-3130 Added INCREMENTAL_INDEXING parameter constant.

--- service/dps/api/src/main/java/eu/europeana/cloud/service/dps/PluginParameterKeys.java
+++ service/dps/api/src/main/java/eu/europeana/cloud/service/dps/PluginParameterKeys.java
@@ -80,6 +80,7 @@ public final class PluginParameterKeys {
     public static final String ADDITIONAL_LOCAL_IDENTIFIER = "ADDITIONAL_LOCAL_IDENTIFIER";
     public static final String USE_DEFAULT_IDENTIFIERS = "USE_DEFAULT_IDENTIFIERS";
     public static final String INCREMENTAL_HARVEST = "INCREMENTAL_HARVEST";
+    public static final String INCREMENTAL_INDEXING = "INCREMENTAL_INDEXING";
     public static final String HARVEST_DATE = "HARVEST_DATE";
     public static final String RECORD_DATESTAMP = "RECORD_DATESTAMP";
 
