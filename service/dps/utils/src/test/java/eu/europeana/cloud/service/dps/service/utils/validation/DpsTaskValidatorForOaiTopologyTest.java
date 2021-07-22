16b7d64682c89be41605cf013abea4fd8e41ca05
Arek Margraf <margraf@man.poznan.pl>
2021-07-22 12:09:55 +0200

    MET-3130/MET-3726 - Changes in post-processing. - validateing DpsTask for OAI/HTTP topologies. New parameter HARVEST_DATE added

--- service/dps/utils/src/test/java/eu/europeana/cloud/service/dps/service/utils/validation/DpsTaskValidatorForOaiTopologyTest.java
+++ service/dps/utils/src/test/java/eu/europeana/cloud/service/dps/service/utils/validation/DpsTaskValidatorForOaiTopologyTest.java
@@ -17,6 +17,7 @@ public class DpsTaskValidatorForOaiTopologyTest {
     private DpsTask oaiTopologyTaskWithoutOutputDataset;
     private DpsTask oaiTopologyTaskWithTwoRepositories;
     private DpsTask oaiTopologyTaskWithTwoOutputDatasets;
+    private DpsTask oaiTopologyTaskWithoutHarvestDate;
     private DpsTask oaiTopologyIncrementalTaskWithSampleSize;
     private DpsTask oaiTopologyIncrementalTaskWithoutSampleSize;
 
@@ -26,23 +27,33 @@ public class DpsTaskValidatorForOaiTopologyTest {
         oaiTopologyTask = new DpsTask();
         oaiTopologyTask.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
         oaiTopologyTask.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
+        oaiTopologyTask.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyTask.addParameter(PluginParameterKeys.OUTPUT_DATA_SETS, "http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt");
         //
         oaiTopologyTaskWithoutOutputDataset = new DpsTask();
         oaiTopologyTaskWithoutOutputDataset.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
+        oaiTopologyTaskWithoutOutputDataset.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyTaskWithoutOutputDataset.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
         //
         oaiTopologyTaskWithTwoRepositories = new DpsTask();
         oaiTopologyTaskWithTwoRepositories.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://iks-kbase.synat.pcss.pl:9090/mcs/records/JP46FLZLVI2UYV4JNHTPPAB4DGPESPY4SY4N5IUQK4SFWMQ3NUQQ/representations/tiff/versions/74c56880-7733-11e5-b38f-525400ea6731/files/f59753a5-6d75-4d48-9f4d-4690b671240c", "http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
+        oaiTopologyTaskWithTwoRepositories.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyTaskWithTwoRepositories.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
         //
         oaiTopologyTaskWithTwoOutputDatasets = new DpsTask();
         oaiTopologyTaskWithTwoOutputDatasets.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://iks-kbase.synat.pcss.pl:9090/mcs/records/JP46FLZLVI2UYV4JNHTPPAB4DGPESPY4SY4N5IUQK4SFWMQ3NUQQ/representations/tiff/versions/74c56880-7733-11e5-b38f-525400ea6731/files/f59753a5-6d75-4d48-9f4d-4690b671240c"));
         oaiTopologyTaskWithTwoOutputDatasets.addParameter(PluginParameterKeys.OUTPUT_DATA_SETS, "http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt,http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt");
+        oaiTopologyTaskWithTwoOutputDatasets.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyTaskWithTwoOutputDatasets.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
+        //
+        oaiTopologyTaskWithoutHarvestDate = new DpsTask();
+        oaiTopologyTaskWithoutHarvestDate.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
+        oaiTopologyTaskWithoutHarvestDate.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
+        oaiTopologyTaskWithoutHarvestDate.addParameter(PluginParameterKeys.OUTPUT_DATA_SETS, "http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt");
         //
         oaiTopologyIncrementalTaskWithSampleSize = new DpsTask();
         oaiTopologyIncrementalTaskWithSampleSize.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
+        oaiTopologyIncrementalTaskWithSampleSize.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyIncrementalTaskWithSampleSize.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
         oaiTopologyIncrementalTaskWithSampleSize.addParameter(PluginParameterKeys.INCREMENTAL_HARVEST, "true");
         oaiTopologyIncrementalTaskWithSampleSize.addParameter(PluginParameterKeys.SAMPLE_SIZE, "10");
@@ -50,6 +61,7 @@ public class DpsTaskValidatorForOaiTopologyTest {
         //
         oaiTopologyIncrementalTaskWithoutSampleSize = new DpsTask();
         oaiTopologyIncrementalTaskWithoutSampleSize.addDataEntry(REPOSITORY_URLS, Arrays.asList("http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt"));
+        oaiTopologyIncrementalTaskWithoutSampleSize.addParameter(PluginParameterKeys.HARVEST_DATE, "harvestDate");
         oaiTopologyIncrementalTaskWithoutSampleSize.addParameter(PluginParameterKeys.PROVIDER_ID, "providerID");
         oaiTopologyIncrementalTaskWithoutSampleSize.addParameter(PluginParameterKeys.INCREMENTAL_HARVEST, "true");
         oaiTopologyIncrementalTaskWithoutSampleSize.addParameter(PluginParameterKeys.OUTPUT_DATA_SETS, "http://127.0.0.1:8080/mcs/records/FUWQ4WMUGIGEHVA3X7FY5PA3DR5Q4B2C4TWKNILLS6EM4SJNTVEQ/representations/TIFF/versions/86318b00-6377-11e5-a1c6-90e6ba2d09ef/files/sampleFileName.txt");
@@ -95,6 +107,13 @@ public class DpsTaskValidatorForOaiTopologyTest {
 
         }
     }
+
+    @Test(expected = DpsTaskValidationException.class)
+    public void shouldValidateTasksWithoutHarvestDate() throws DpsTaskValidationException {
+        DpsTaskValidator validator = DpsTaskValidatorFactory.createValidatorForTaskType("oai_topology_repository_urls");
+        validator.validate(oaiTopologyTaskWithoutHarvestDate);
+    }
+
 
     @Test
     public void shouldValidateIncrementalTaskForOAITopologyWithSampleSize() {
