1adda25675b225272fe755b1cd9098393ba3cb41
pWoz <pwozniak@man.poznan.pl>
2021-12-20 17:15:03 +0100

    Refactorization: UniqueIdentifierResource.java changed

--- service/uis/api/src/main/java/eu/europeana/cloud/service/uis/RestInterfaceConstants.java
+++ service/uis/api/src/main/java/eu/europeana/cloud/service/uis/RestInterfaceConstants.java
@@ -6,6 +6,7 @@ public class RestInterfaceConstants {
     public static final String DATA_PROVIDERS = "/data-providers";
     public static final String DATA_PROVIDER = "/data-providers/{provider}";
     public static final String CLOUD_IDS = "/cloudIds";
+    public static final String CLOUD_ID = "/cloudIds/{cloudId}";
     public static final String DATA_PROVIDER_ACTIVATION = "/data-providers/{provider}/active";
 
 
