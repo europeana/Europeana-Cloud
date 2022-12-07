450454dfe12edac758f776799a929ca68e120840
Ruufuus <rufusikx3@gmail.com>
2022-12-07 18:29:35 +0100

    MET-4991 Add ReportDAO tests Extract ReportTestUtils methods from ReportServiceTest class

---
+++ service/dps/storm/common/src/test/java/eu/europeana/cloud/service/dps/storm/dao/ReportDAOTest.java
@@ -0,0 +1,95 @@
+package eu.europeana.cloud.service.dps.storm.dao;
+
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.ERROR_TYPE;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.TASK_ID_LONG;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreErrorType;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreNotification;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertNull;
+
+import eu.europeana.cloud.cassandra.CassandraConnectionProvider;
+import eu.europeana.cloud.common.model.dps.TaskInfo;
+import eu.europeana.cloud.service.dps.storm.utils.CassandraTestBase;
+import eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils;
+import eu.europeana.cloud.test.CassandraTestInstance;
+import java.util.UUID;
+import org.junit.Before;
+import org.junit.Test;
+
+public class ReportDAOTest extends CassandraTestBase {
+
+  private static final String ERROR_TYPE1 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b1";
+  private static final String ERROR_TYPE2 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b2";
+  private static final String ERROR_TYPE3 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b3";
+  private static final String ERROR_TYPE4 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b4";
+  private static final String ERROR_TYPE5 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b5";
+  private ReportDAO reportDAO;
+  private NotificationsDAO notificationsDAO;
+  private CassandraTaskInfoDAO taskInfoDAO;
+  private CassandraTaskErrorsDAO errorsDAO;
+
+  @Before
+  public void setup() {
+    CassandraConnectionProvider db = new CassandraConnectionProvider(HOST, CassandraTestInstance.getPort(), KEYSPACE, USER,
+        PASSWORD);
+    reportDAO = ReportDAO.getInstance(db);
+    notificationsDAO = NotificationsDAO.getInstance(db);
+    taskInfoDAO = CassandraTaskInfoDAO.getInstance(db);
+    errorsDAO = CassandraTaskErrorsDAO.getInstance(db);
+  }
+
+
+  @Test
+  public void getTaskInfoRecordTest() {
+    assertNull(reportDAO.getTaskInfoRecord(TASK_ID_LONG));
+    ReportTestUtils.createAndStoreTaskInfo(taskInfoDAO);
+    TaskInfo taskInfo = reportDAO.getTaskInfoRecord(TASK_ID_LONG);
+    assertEquals(TASK_ID_LONG, taskInfo.getId());
+  }
+
+  @Test
+  public void getNotificationsTest() {
+    assertEquals(0, reportDAO.getNotifications(TASK_ID_LONG, 1, 5, 0).size());
+    createAndStoreNotification(1, notificationsDAO);
+    createAndStoreNotification(2, notificationsDAO);
+    createAndStoreNotification(3, notificationsDAO);
+    createAndStoreNotification(4, notificationsDAO);
+    createAndStoreNotification(5, notificationsDAO);
+    assertEquals(5, reportDAO.getNotifications(TASK_ID_LONG, 1, 5, 0).size());
+    assertEquals(0, reportDAO.getNotifications(TASK_ID_LONG, 6, 10, 0).size());
+    createAndStoreNotification(6, notificationsDAO);
+    createAndStoreNotification(6, notificationsDAO);
+    assertEquals(1, reportDAO.getNotifications(TASK_ID_LONG, 6, 10, 0).size());
+  }
+
+  @Test
+  public void getErrorTypesTest() {
+    assertEquals(0, reportDAO.getErrorTypes(TASK_ID_LONG).size());
+    createAndStoreErrorType(ERROR_TYPE1, errorsDAO);
+    assertEquals(1, reportDAO.getErrorTypes(TASK_ID_LONG).size());
+    createAndStoreErrorType(ERROR_TYPE2, errorsDAO);
+    createAndStoreErrorType(ERROR_TYPE3, errorsDAO);
+    createAndStoreErrorType(ERROR_TYPE4, errorsDAO);
+    createAndStoreErrorType(ERROR_TYPE5, errorsDAO);
+    assertEquals(5, reportDAO.getErrorTypes(TASK_ID_LONG).size());
+    createAndStoreErrorType(ERROR_TYPE5, errorsDAO);
+    assertEquals(5, reportDAO.getErrorTypes(TASK_ID_LONG).size());
+  }
+
+  @Test
+  public void getErrorNotificationsTest() {
+    assertEquals(0, reportDAO.getErrorNotifications(TASK_ID_LONG, UUID.fromString(ERROR_TYPE), 1).size());
+    ReportTestUtils.createAndStoreErrorNotification(ERROR_TYPE, errorsDAO);
+    assertEquals(1, reportDAO.getErrorNotifications(TASK_ID_LONG, UUID.fromString(ERROR_TYPE), 1).size());
+    ReportTestUtils.createAndStoreErrorNotification(ERROR_TYPE, errorsDAO);
+    assertEquals(1, reportDAO.getErrorNotifications(TASK_ID_LONG, UUID.fromString(ERROR_TYPE), 5).size());
+  }
+
+  @Test
+  public void getErrorTypeTest() {
+    assertNull(reportDAO.getErrorType(TASK_ID_LONG, UUID.fromString(ERROR_TYPE)));
+    ReportTestUtils.createAndStoreErrorType(ERROR_TYPE, errorsDAO);
+    assertNotNull(reportDAO.getErrorType(TASK_ID_LONG, UUID.fromString(ERROR_TYPE)));
+  }
+}
--- service/dps/storm/common/src/test/java/eu/europeana/cloud/service/dps/storm/service/ReportServiceTest.java
+++ service/dps/storm/common/src/test/java/eu/europeana/cloud/service/dps/storm/service/ReportServiceTest.java
@@ -1,5 +1,14 @@
 package eu.europeana.cloud.service.dps.storm.service;
 
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.ERROR_TYPE;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.TASK_ID;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.TASK_ID_LONG;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.TOPOLOGY_NAME;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreErrorNotification;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreErrorType;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreNotification;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreSubtaskInfoRange;
+import static eu.europeana.cloud.service.dps.storm.utils.ReportTestUtils.createAndStoreTaskInfo;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.hasSize;
 import static org.junit.Assert.assertEquals;
@@ -9,7 +18,6 @@ import static org.junit.Assert.assertTrue;
 
 import com.google.common.collect.Lists;
 import eu.europeana.cloud.cassandra.CassandraConnectionProvider;
-import eu.europeana.cloud.common.model.dps.RecordState;
 import eu.europeana.cloud.common.model.dps.SubTaskInfo;
 import eu.europeana.cloud.common.model.dps.TaskErrorInfo;
 import eu.europeana.cloud.common.model.dps.TaskErrorsInfo;
@@ -21,23 +29,16 @@ import eu.europeana.cloud.service.dps.storm.dao.CassandraTaskInfoDAO;
 import eu.europeana.cloud.service.dps.storm.dao.NotificationsDAO;
 import eu.europeana.cloud.service.dps.storm.utils.CassandraTestBase;
 import eu.europeana.cloud.test.CassandraTestInstance;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 import org.junit.Before;
 import org.junit.Test;
 
 
 public class ReportServiceTest extends CassandraTestBase {
-
-  private static final long TASK_ID_LONG = 111;
-  private static final String TASK_ID = String.valueOf(TASK_ID_LONG);
-  private static final String TOPOLOGY_NAME = "some_topology";
-  private static final String ERROR_TYPE = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b2";
   private static final String ERROR_TYPE1 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b3";
   private static final String ERROR_TYPE2 = "1c71e7b0-7633-11ed-b1fe-a7fdf50126b4";
   private ReportService service;
-  private NotificationsDAO subtaskInfoDao;
+  private NotificationsDAO notificationsDAO;
   private CassandraTaskInfoDAO taskInfoDAO;
   private CassandraTaskErrorsDAO errorsDAO;
 
@@ -46,14 +47,14 @@ public class ReportServiceTest extends CassandraTestBase {
     CassandraConnectionProvider db = new CassandraConnectionProvider(HOST, CassandraTestInstance.getPort(), KEYSPACE, USER,
         PASSWORD);
     service = new ReportService(new CassandraConnectionProvider(HOST, CassandraTestInstance.getPort(), KEYSPACE, USER, PASSWORD));
-    subtaskInfoDao = NotificationsDAO.getInstance(db);
+    notificationsDAO = NotificationsDAO.getInstance(db);
     taskInfoDAO = CassandraTaskInfoDAO.getInstance(db);
     errorsDAO = CassandraTaskErrorsDAO.getInstance(db);
   }
 
   @Test
   public void shouldReturnTaskProgressWhenTaskExists() throws AccessDeniedOrObjectDoesNotExistException {
-    createAndStoreTaskInfo();
+    createAndStoreTaskInfo(taskInfoDAO);
     TaskInfo taskInfo = service.getTaskProgress(TASK_ID);
     assertEquals(TaskState.QUEUED, taskInfo.getState());
     assertEquals(TASK_ID_LONG, taskInfo.getId());
@@ -63,21 +64,21 @@ public class ReportServiceTest extends CassandraTestBase {
   public void shouldThrowExceptionWhenTaskDoesntExistOrIsFromDifferentTopology() {
     assertThrows(AccessDeniedOrObjectDoesNotExistException.class, () -> service.getTaskProgress(TASK_ID));
     assertThrows(AccessDeniedOrObjectDoesNotExistException.class, () -> service.checkIfTaskExists(TASK_ID, TOPOLOGY_NAME));
-    createAndStoreTaskInfo();
+    createAndStoreTaskInfo(taskInfoDAO);
     assertThrows(AccessDeniedOrObjectDoesNotExistException.class, () -> service.checkIfTaskExists(TASK_ID, "any_topology"));
   }
 
   @Test
   public void shouldCheckIfReportExists() {
     assertFalse(service.checkIfReportExists(TASK_ID));
-    createAndStoreErrorType();
+    createAndStoreErrorType(errorsDAO);
     assertTrue(service.checkIfReportExists(TASK_ID));
   }
 
   @Test
   public void shouldReturnSpecificTaskErrorReturn() throws AccessDeniedOrObjectDoesNotExistException {
-    createAndStoreErrorType();
-    createAndStoreErrorNotification();
+    createAndStoreErrorType(errorsDAO);
+    createAndStoreErrorNotification(errorsDAO);
     TaskErrorsInfo errorsInfo = service.getSpecificTaskErrorReport(TASK_ID, ERROR_TYPE, 5);
     assertEquals(1, errorsInfo.getErrors().size());
   }
@@ -86,7 +87,7 @@ public class ReportServiceTest extends CassandraTestBase {
   public void shouldThrowExceptionWhenSpecificTaskErrorDoesntExistOrErrorNotificationDoesntExist() {
     assertThrows(AccessDeniedOrObjectDoesNotExistException.class,
         () -> service.getSpecificTaskErrorReport(TASK_ID, ERROR_TYPE, 5));
-    createAndStoreErrorType();
+    createAndStoreErrorType(errorsDAO);
     assertThrows(AccessDeniedOrObjectDoesNotExistException.class,
         () -> service.getSpecificTaskErrorReport(TASK_ID, ERROR_TYPE, 5));
   }
@@ -94,12 +95,12 @@ public class ReportServiceTest extends CassandraTestBase {
   @Test
   public void shouldReturnGeneralTaskErrorReport() throws AccessDeniedOrObjectDoesNotExistException {
     assertEquals(0, service.getGeneralTaskErrorReport(TASK_ID, 5).getErrors().size());
-    createAndStoreErrorType();
-    createAndStoreErrorNotification();
-    createAndStoreErrorType(ERROR_TYPE1);
-    createAndStoreErrorNotification(ERROR_TYPE1);
-    createAndStoreErrorType(ERROR_TYPE2);
-    createAndStoreErrorNotification(ERROR_TYPE2);
+    createAndStoreErrorType(errorsDAO);
+    createAndStoreErrorNotification(errorsDAO);
+    createAndStoreErrorType(ERROR_TYPE1, errorsDAO);
+    createAndStoreErrorNotification(ERROR_TYPE1, errorsDAO);
+    createAndStoreErrorType(ERROR_TYPE2, errorsDAO);
+    createAndStoreErrorNotification(ERROR_TYPE2, errorsDAO);
     List<TaskErrorInfo> errorInfos = service.getGeneralTaskErrorReport(TASK_ID, 5).getErrors();
     assertEquals(3, errorInfos.size());
     assertTrue(errorInfos.stream().anyMatch(errorInfo -> ERROR_TYPE.equals(errorInfo.getErrorType())));
@@ -131,7 +132,7 @@ public class ReportServiceTest extends CassandraTestBase {
 
   @Test
   public void shouldReturnValidReportWhenQueryingOneRecord() {
-    SubTaskInfo info = createAndStoreSubtaskInfo(1);
+    SubTaskInfo info = createAndStoreNotification(1, notificationsDAO);
 
     List<SubTaskInfo> report = service.getDetailedTaskReport(TASK_ID, 1, 1);
 
@@ -162,7 +163,7 @@ public class ReportServiceTest extends CassandraTestBase {
   @Test
   public void shouldReturnValidReportWhenQueryingPartOfDataInOneBucketAtBorderOfData() {
     List<SubTaskInfo> infoList =
-        createAndStoreSubtaskInfoRange(300).subList(100 - 1, 200);
+        createAndStoreSubtaskInfoRange(300, notificationsDAO).subList(100 - 1, 200);
 
     List<SubTaskInfo> report = service.getDetailedTaskReport(TASK_ID, 100, 200);
 
@@ -173,7 +174,7 @@ public class ReportServiceTest extends CassandraTestBase {
   @Test
   public void shouldReturnValidReportWhenQueryingPartOfDataInFurtherBucket() {
     List<SubTaskInfo> infoList =
-        createAndStoreSubtaskInfoRange(40000).subList(21000 - 1, 22000);
+        createAndStoreSubtaskInfoRange(40000, notificationsDAO).subList(21000 - 1, 22000);
 
     List<SubTaskInfo> report = service.getDetailedTaskReport(TASK_ID, 21000, 22000);
 
@@ -181,57 +182,10 @@ public class ReportServiceTest extends CassandraTestBase {
   }
 
   private void shouldReturnValidReportWhenQueryingBucketTemplate(int preparedReportSize, int requestTo) {
-    List<SubTaskInfo> infoList = createAndStoreSubtaskInfoRange(preparedReportSize);
+    List<SubTaskInfo> infoList = createAndStoreSubtaskInfoRange(preparedReportSize, notificationsDAO);
 
     List<SubTaskInfo> report = service.getDetailedTaskReport(TASK_ID, 1, requestTo);
 
     assertEquals(Lists.reverse(infoList), report);
   }
-
-  private List<SubTaskInfo> createAndStoreSubtaskInfoRange(int to) {
-    List<SubTaskInfo> result = new ArrayList<>();
-    for (int i = 1; i <= to; i++) {
-      result.add(createAndStoreSubtaskInfo(i));
-    }
-    return result;
-  }
-
-  private void createAndStoreErrorType(String errorType) {
-    errorsDAO.insertErrorCounter(TASK_ID_LONG, errorType, 5);
-  }
-
-  private void createAndStoreErrorType() {
-    createAndStoreErrorType(ERROR_TYPE);
-  }
-
-  private void createAndStoreErrorNotification() {
-    createAndStoreErrorNotification(ERROR_TYPE);
-  }
-
-  private void createAndStoreErrorNotification(String errorType) {
-    errorsDAO.insertError(TASK_ID_LONG, errorType, "some_error_message", "some_resource", "some_additional_information");
-  }
-
-  private void createAndStoreTaskInfo() {
-    TaskInfo exampleTaskInfo = new TaskInfo();
-    exampleTaskInfo.setId(TASK_ID_LONG);
-    exampleTaskInfo.setState(TaskState.QUEUED);
-    exampleTaskInfo.setTopologyName(TOPOLOGY_NAME);
-    taskInfoDAO.insert(exampleTaskInfo);
-  }
-
-  private SubTaskInfo createAndStoreSubtaskInfo(int resourceNum) {
-    SubTaskInfo info = new SubTaskInfo(resourceNum, "resource" + resourceNum, RecordState.QUEUED, "info", "additionalInformation",
-        "europeanaId", 0L, "resultResource" + resourceNum);
-    subtaskInfoDao.insert(info.getResourceNum(), TASK_ID_LONG, TOPOLOGY_NAME,
-        info.getResource(), info.getRecordState().toString(), info.getInfo(),
-        Map.of(
-            NotificationsDAO.STATE_DESCRIPTION_KEY, info.getAdditionalInformations(),
-            NotificationsDAO.EUROPEANA_ID_KEY, info.getEuropeanaId(),
-            NotificationsDAO.PROCESSING_TIME_KEY, String.valueOf(info.getProcessingTime())
-        ),
-        info.getResultResource());
-    return info;
-  }
-
 }
