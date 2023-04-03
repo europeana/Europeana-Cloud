108b8be466274fdfd27fefa04537ce6785feb269
Ruufuus <rufusikx3@gmail.com>
2023-04-03 13:45:02 +0200

    MET-5124 Delete unused field

--- service/dps/rest/src/test/java/eu/europeana/cloud/service/dps/utils/GhostTaskServiceTest.java
+++ service/dps/rest/src/test/java/eu/europeana/cloud/service/dps/utils/GhostTaskServiceTest.java
@@ -54,8 +54,6 @@ public class GhostTaskServiceTest {
                         .lastRecordFinishedOnStormTime(Instant.now().minus(3, ChronoUnit.DAYS))
                         .build();
 
-  @Autowired
-  private static KafkaProperties kafkaProperties;
 
   @Autowired
   private GhostTaskService service;
