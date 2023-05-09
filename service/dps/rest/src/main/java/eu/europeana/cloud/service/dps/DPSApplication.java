bc3f6123f1a9c2058744d38016c4edcf2518f2fc
Ruufuus <rufusikx3@gmail.com>
2023-05-09 10:31:48 +0200

    MET-5202 Remove redundant import annotation

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/DPSApplication.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/DPSApplication.java
@@ -1,16 +1,11 @@
 package eu.europeana.cloud.service.dps;
 
-import eu.europeana.cloud.service.dps.config.AuthenticationConfiguration;
-import eu.europeana.cloud.service.dps.config.AuthorizationConfiguration;
-import eu.europeana.cloud.service.dps.config.ServiceConfiguration;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Import;
 
 @SpringBootApplication
-@Import({ServiceConfiguration.class, AuthenticationConfiguration.class, AuthorizationConfiguration.class})
 public class DPSApplication {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(DPSApplication.class);
