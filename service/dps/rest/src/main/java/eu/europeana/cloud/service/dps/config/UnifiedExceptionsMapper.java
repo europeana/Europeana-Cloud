54f91e0e9afa9ed38f6dbb3702f663554fb18e94
woz <pwozniak@man.poznan.pl>
2024-03-13 13:54:33 +0100

    MET-5845 migration to Spring 6: removed javax dependencies

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/config/UnifiedExceptionsMapper.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/config/UnifiedExceptionsMapper.java
@@ -11,7 +11,7 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
-import javax.validation.ConstraintViolationException;
+import jakarta.validation.ConstraintViolationException;
 
 import jakarta.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
