05034e348e54c5be04c8db8217e7886b456a49c4
Ruufuus <rufusikx3@gmail.com>
2024-10-31 18:10:01 +0100

    MET-5678 Add missing import due to IDE issues

--- service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/services/submitters/MCSTaskSubmitter.java
+++ service/dps/rest/src/main/java/eu/europeana/cloud/service/dps/services/submitters/MCSTaskSubmitter.java
@@ -29,6 +29,7 @@ import java.util.concurrent.ExecutionException;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.Future;
+import java.util.concurrent.TimeUnit;
 
 import static eu.europeana.cloud.service.dps.InputDataType.FILE_URLS;
 
