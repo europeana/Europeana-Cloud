797bab6b4d4b78bdd2598f0ea4de572e8b3dfef9
Ruufuus <rufusikx3@gmail.com>
2025-07-04 11:12:55 +0200

    Revert "MET-6540 Add temporary forced choose of new storage type"
    This reverts commit 0c3e68bfb115c6bfb4a920c7cd4bf11cc0292e35.

--- service/mcs/persistent/src/main/java/eu/europeana/cloud/service/mcs/persistent/DynamicContentProxy.java
+++ service/mcs/persistent/src/main/java/eu/europeana/cloud/service/mcs/persistent/DynamicContentProxy.java
@@ -30,39 +30,21 @@ public class DynamicContentProxy {
   }
 
   public void deleteContent(String md5, String fileName, Storage stored) throws FileNotExistsException {
-    if (Storage.DATA_BASE.equals(stored)) {
-      getContentDAO(Storage.DB_STORAGE).deleteContent(md5, fileName);
-    } else {
-      getContentDAO(stored).deleteContent(md5, fileName);
-    }
+    getContentDAO(stored).deleteContent(md5, fileName);
   }
 
   public void getContent(String md5, String fileName, long start, long end, OutputStream os, Storage stored)
       throws IOException, FileNotExistsException {
 
-    if (Storage.DATA_BASE.equals(stored)) {
-      getContentDAO(Storage.DB_STORAGE).getContent(md5, fileName, start, end, os);;
-    } else {
-      getContentDAO(stored).getContent(md5, fileName, start, end, os);
-    }
+    getContentDAO(stored).getContent(md5, fileName, start, end, os);
   }
 
   public PutResult putContent(String fileName, InputStream data, Storage stored) throws IOException {
-
-    if (Storage.DATA_BASE.equals(stored)) {
-      return getContentDAO(Storage.DB_STORAGE).putContent(fileName, data);
-    } else {
-      return getContentDAO(stored).putContent(fileName, data);
-    }
+    return getContentDAO(stored).putContent(fileName, data);
   }
 
   private ContentDAO getContentDAO(final Storage storage) {
-    ContentDAO dao;
-    if (Storage.DATA_BASE.equals(storage)) {
-      dao = contentDAOs.get(Storage.DB_STORAGE);
-    } else {
-      dao = contentDAOs.get(storage);
-    }
+    ContentDAO dao = contentDAOs.get(storage);
     if (dao == null) {
       throw new ContentDaoNotFoundException("Specified storage \"" + storage + "\" has not been defined in " + this + "!");
     }
