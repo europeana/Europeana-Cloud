f22595b050d9492de66038b7975410f42665e7ed
marcin-rp <mratajczak@man.poznan.pl>
2025-12-18 15:53:58 +0100

    MET-6967 Review remarks

--- common/src/main/java/eu/europeana/cloud/service/mcs/Storage.java
+++ common/src/main/java/eu/europeana/cloud/service/mcs/Storage.java
@@ -3,11 +3,11 @@ package eu.europeana.cloud.service.mcs;
 /**
  * Indicate storage.
  *
- * Enum values are used as key in Cassandra, so coudl not be changed easily
+ * Enum values are used as key in Cassandra, so could not be changed easily
  *
  */
 public enum Storage {
   OBJECT_STORAGE, // S3 Storage
-  DATA_BASE, // old files_content Cassandra table - used by default
-  DB_STORAGE // new files_content_v2 Cassandra table with optimization
+  DATA_BASE, // old files_content Cassandra table - used by default, reverted in ticket: MET-6967
+  DB_STORAGE // new files_content_v2 Cassandra table with optimization developed based on static column
 }
