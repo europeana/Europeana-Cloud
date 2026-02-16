c846993e809b254c204ad349227d6c49223929f1
Ruufuus <rufusikx3@gmail.com>
2026-02-16 15:42:22 +0100

    MET-6688 Improve logging message

--- service/mcs/persistent/src/main/java/eu/europeana/cloud/service/mcs/persistent/CassandraRecordService.java
+++ service/mcs/persistent/src/main/java/eu/europeana/cloud/service/mcs/persistent/CassandraRecordService.java
@@ -201,7 +201,7 @@ public class CassandraRecordService implements RecordService {
       if (!currentDataset.equals(newVersionDataset)) {
         throw new DataSetAssignmentException("ProviderId and/or datasetId: " + newVersionDataset
                 + " doesn't match the current assignments of the representation: " + currentDataset
-                + ". It is not allowed to assign version of same record to more than one dataset.");
+                + ". It is not allowed to assign the same representation version of the record to more than one dataset.");
       }
     }
   }
