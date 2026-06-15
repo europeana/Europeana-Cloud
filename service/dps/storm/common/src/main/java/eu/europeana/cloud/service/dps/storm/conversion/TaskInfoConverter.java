863399287ccfcf9d3fac3aa8669274dc5256aac3
Ruufuus <rufusikx3@gmail.com>
2026-06-15 12:04:40 +0200

    MET-7075 Fix counter converter from old to new table

--- service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/conversion/TaskInfoConverter.java
+++ service/dps/storm/common/src/main/java/eu/europeana/cloud/service/dps/storm/conversion/TaskInfoConverter.java
@@ -62,28 +62,29 @@ public final class TaskInfoConverter {
                 .startTimestamp(row.getTimestamp(CassandraTablesAndColumnsNames.TASK_INFO_START_TIMESTAMP))
                 .finishTimestamp(row.getTimestamp(CassandraTablesAndColumnsNames.TASK_INFO_FINISH_TIMESTAMP))
                 .expectedRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_EXPECTED_RECORDS_NUMBER)
-                        - row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_POST_PROCESSED_RECORDS_COUNT)
-                        - row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
+                        - getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_POST_PROCESSED_RECORDS_COUNT)
+                        - getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
                 .successRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_RECORDS_COUNT)
-                        - row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_IGNORED_RECORDS_COUNT)
-                        - row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_ERRORS_COUNT))
+                        - getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_ERRORS_COUNT))
                 .warningRecords(-1)
                 .failRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_ERRORS_COUNT))
                 .duplicateRecords(-1)
                 .unchangedRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_IGNORED_RECORDS_COUNT))
                 .processedRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_RECORDS_COUNT)
-                        + row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT)
-                        + row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_IGNORED_RECORDS_COUNT)
-                        + row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_PROCESSED_ERRORS_COUNT))
+                        + getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_IGNORED_RECORDS_COUNT))
                 .postProcessedRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_POST_PROCESSED_RECORDS_COUNT))
                 .expectedPostProcessedRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_EXPECTED_POST_PROCESSED_RECORDS_NUMBER))
                 .expectedDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_POST_PROCESSED_RECORDS_COUNT)
-                        + row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
-                .successDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
+                        + getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
+                .successDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT)
+                        - getValue(row, CassandraTablesAndColumnsNames.TASK_INFO_DELETED_ERRORS_COUNT))
                 .failDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_ERRORS_COUNT))
-                .processedDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_ERRORS_COUNT)
-                        + row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
+                .processedDepublishRecords(row.getInt(CassandraTablesAndColumnsNames.TASK_INFO_DELETED_RECORDS_COUNT))
                 .definition(row.getString(CassandraTablesAndColumnsNames.TASK_INFO_DEFINITION))
                 .build();
     }
+
+  private static int getValue(Row row, String columnName) {
+    return Math.max(0, row.getInt(columnName));
+  }
 }
